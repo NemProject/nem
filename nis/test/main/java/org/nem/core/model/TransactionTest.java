8db4dbb36608168d53e777df2e75ed49026ac00d
nem <nem@bob.nem.ninja>
2014-02-20 22:54:00 -0500

    ...

--- nis/test/main/java/org/nem/core/model/TransactionTest.java
+++ nis/test/main/java/org/nem/core/model/TransactionTest.java
@@ -4,8 +4,7 @@ import org.hamcrest.core.*;
 import org.junit.*;
 import org.nem.core.crypto.*;
 import org.nem.core.serialization.*;
-import org.nem.core.test.MockAccount;
-import org.nem.core.test.MockAccountLookup;
+import org.nem.core.test.*;
 
 import java.security.InvalidParameterException;
 
@@ -179,45 +178,5 @@ public class TransactionTest {
             new JsonDeserializer(jsonSerializer.getObject()),
             accountLookup);
         return new MockTransaction(deserializer);
-    }
-
-    private class MockTransaction extends Transaction {
-
-        private int customField;
-        private long minimumFee;
-
-        public MockTransaction(final Account sender) throws Exception {
-            this(sender, 0);
-        }
-
-        public MockTransaction(final Account sender, final int customField) throws Exception {
-            super(123, 759, sender);
-            this.customField = customField;
-        }
-
-        public MockTransaction(final ObjectDeserializer deserializer) throws Exception {
-            super(deserializer.readInt("type"), deserializer);
-            this.customField = deserializer.readInt("customField");
-        }
-
-        public int getCustomField() { return this.customField; }
-        public void setCustomField(final int customField) { this.customField = customField; }
-
-        public void setMinimumFee(final long minimumFee) { this.minimumFee = minimumFee; }
-
-        @Override
-        public boolean isValid() {
-            return true;
-        }
-
-        @Override
-        protected long getMinimumFee() {
-            return this.minimumFee;
-        }
-
-        @Override
-        protected void serializeImpl(ObjectSerializer serializer) throws Exception {
-            serializer.writeInt("customField", this.customField);
-        }
     }
 }
