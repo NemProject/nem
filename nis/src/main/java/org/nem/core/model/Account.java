c4f457ac5dc89d225e0e942c7bd10aaa59f127c4
nem <nem@bob.nem.ninja>
2014-02-28 19:59:16 -0500

    ...

--- nis/src/main/java/org/nem/core/model/Account.java
+++ nis/src/main/java/org/nem/core/model/Account.java
@@ -14,6 +14,11 @@ public class Account {
     private long balance;
     private String label;
 
+    /**
+     * Creates an account around a key pair.
+     *
+     * @param keyPair The key pair.
+     */
     public Account(final KeyPair keyPair) {
         this.keyPair = keyPair;
         this.address = Address.fromPublicKey(keyPair.getPublicKey());
@@ -21,6 +26,12 @@ public class Account {
         this.balance = 0;
     }
 
+    /**
+     * Creates an account around an address. This constructor should only
+     * be used if the account's public key is not known.
+     *
+     * @param address The address.
+     */
 	public Account(final Address address) {
 		this.keyPair = null;
 		this.address = address;
@@ -28,20 +39,63 @@ public class Account {
 		this.balance = 0;
 	}
 
+    /**
+     * Gets the account's key pair.
+     *
+     * @return The account's key pair.
+     */
     public KeyPair getKeyPair() { return this.keyPair; }
 
+    /**
+     * Gets the account's address.
+     *
+     * @return The account's address.
+     */
     public Address getAddress() { return this.address; }
 
+    /**
+     * Gets the account's balance.
+     *
+     * @return The account's balance.
+     */
     public long getBalance() { return this.balance; }
-    public void incrementBalance(final long balance) { this.balance += balance; }
 
+    /**
+     * Increments the account's balance.
+     *
+     * @param balance The amount by which to increment the balance.
+     */
+    public void incrementBalance(final long balance) {
+        this.balance += balance;
+    }
+
+    /**
+     * Gets the account's label.
+     *
+     * @return The account's label.
+     */
     public String getLabel() { return this.label; }
+
+    /**
+     * Sets the account's label.
+     *
+     * @param label The desired label.
+     */
     public void setLabel(final String label) { this.label = label; }
 
-     // TODO: the messaging API still needs to be cleaned up a little bit
-
+    /**
+     * Gets all messages associated with the account.
+     *
+     * @return All messages associated with the account.
+     */
     public List<Message> getMessages() { return this.messages; }
-    public void addMessage(Message message) {
+
+    /**
+     * Associates message with the account.
+     *
+     * @param message The message to associate with this account.
+     */
+    public void addMessage(final Message message) {
         this.messages.add(message);
     }
 
