8e4ae2e38aa43d2826c5aec867e46b6e2bee7f15
nem <nem@bob.nem.ninja>
2014-03-05 20:27:59 -0500

    ...

--- nis/src/main/java/org/nem/peer/v2/NodeStatusDemux.java
+++ nis/src/main/java/org/nem/peer/v2/NodeStatusDemux.java
@@ -1,58 +1,83 @@
 package org.nem.peer.v2;
 
 import org.nem.core.serialization.*;
-import org.nem.peer.*;
-import org.nem.peer.v2.NodeStatus;
 
 import java.util.*;
 
 /**
- * Demultiplexes a node collection into separate collections based on node state.
+ * Demultiplexes a node collection into separate node info collections based on node status.
  */
-public class NodeStateDemux implements SerializableEntity {
+public class NodeStatusDemux implements SerializableEntity {
 
-    final List<Node> activeNodes;
-    final List<Node> inactiveNodes;
+    private static ObjectDeserializer<NodeInfo> NODE_INFO_DESERIALIZER = new ObjectDeserializer<NodeInfo>() {
+        @Override
+        public NodeInfo deserialize(final Deserializer deserializer) {
+            return new NodeInfo(deserializer);
+        }
+    };
+
+    final List<NodeInfo> activeNodes;
+    final List<NodeInfo> inactiveNodes;
 
     /**
      * Demultiplexes an collection of nodes.
      *
      * @param nodes The collection to demux.
      */
-    public NodeStateDemux(final Iterable<Node> nodes) {
+    public NodeStatusDemux(final Iterable<Node> nodes) {
         this.activeNodes = new ArrayList<>();
         this.inactiveNodes = new ArrayList<>();
 
         for (final Node node : nodes) {
-            final List<Node> nodeList = NodeStatus.ACTIVE == node.getStatus() ? this.activeNodes : this.inactiveNodes;
-            nodeList.add(node);
+            final List<NodeInfo> nodeList;
+            switch (node.getStatus()) {
+                case ACTIVE:
+                    nodeList = this.activeNodes;
+                    break;
+
+                case INACTIVE:
+                    nodeList = this.inactiveNodes;
+                    break;
+
+                default:
+                    nodeList = null;
+                    break;
+            }
+
+            if (null == nodeList)
+                continue;
+
+            nodeList.add(node.getInfo());
         }
     }
 
     /**
      * Deserializes demultiplexed nodes.
      *
-     * @param nodes The collection to demux.
+     * @param deserializer The deserializer.
      */
-    public NodeStateDemux(final Iterable<org.nem.peer.Node> nodes) {
+    public NodeStatusDemux(final Deserializer deserializer) {
+        this.activeNodes = deserializer.readObjectArray("active", NODE_INFO_DESERIALIZER);
+        this.inactiveNodes = deserializer.readObjectArray("inactive", NODE_INFO_DESERIALIZER);
+    }
 
     /**
      * Gets a collection of active nodes.
      *
      * @return A collection of active nodes.
      */
-    public Iterable<org.nem.peer.Node> getActiveNodes() { return this.activeNodes; }
+    public Collection<NodeInfo> getActiveNodes() { return this.activeNodes; }
 
     /**
      * Gets a collection of inactive nodes.
      *
      * @return A collection of active nodes.
      */
-    public Iterable<org.nem.peer.Node> getInactiveNodes() { return this.activeNodes; }
-
+    public Collection<NodeInfo> getInactiveNodes() { return this.inactiveNodes; }
 
     @Override
-    public void serialize(Serializer serializer) {
-
+    public void serialize(final Serializer serializer) {
+        serializer.writeObjectArray("active", this.activeNodes);
+        serializer.writeObjectArray("inactive", this.inactiveNodes);
     }
 }
--- nis/src/test/java/org/nem/peer/v2/NodeStatusDemuxTest.java
+++ nis/src/test/java/org/nem/peer/v2/NodeStatusDemuxTest.java
@@ -1,4 +1,111 @@
 package org.nem.peer.v2;
 
+import org.hamcrest.core.*;
+import org.junit.*;
+import org.nem.core.test.Utils;
+
+import java.util.*;
+
 public class NodeStatusDemuxTest {
+
+    //region basic partitioning
+
+    @Test
+    public void activeNodeIsPlacedInActiveList() {
+        // Arrange:
+        assertStatusListAssignment(NodeStatus.ACTIVE, 1, 0);
+    }
+
+    @Test
+    public void inactiveNodeIsPlacedInInactiveList() {
+        // Arrange:
+        assertStatusListAssignment(NodeStatus.INACTIVE, 0, 1);
+    }
+
+    @Test
+    public void failedNodeIsNotPlacedInAnyList() {
+        // Assert:
+        assertStatusListAssignment(NodeStatus.FAILURE, 0, 0);
+    }
+
+    @Test
+    public void inputListWithMultipleNodesIsPartitionedCorrectly() {
+        // Arrange:
+        final List<Node> nodes = new ArrayList<>();
+        nodes.add(createNode(NodeStatus.ACTIVE, "A"));
+        nodes.add(createNode(NodeStatus.INACTIVE, "B"));
+        nodes.add(createNode(NodeStatus.INACTIVE, "C"));
+        nodes.add(createNode(NodeStatus.ACTIVE, "D"));
+        nodes.add(createNode(NodeStatus.FAILURE, "E"));
+        nodes.add(createNode(NodeStatus.ACTIVE, "F"));
+
+        // Assert:
+        assertStatusListAssignment(nodes, 3, 2);
+
+        //TODO: validate specific nodes in addition to counts
+    }
+
+    private static void assertStatusListAssignment(
+        final List<Node> nodes,
+        final int expectedNumActiveNodes,
+        final int expectedNumInactiveNodes) {
+        // Act:
+        final NodeStatusDemux demux = new NodeStatusDemux(nodes);
+
+        // Assert:
+        Assert.assertThat(demux.getActiveNodes().size(), IsEqual.equalTo(expectedNumActiveNodes));
+        Assert.assertThat(demux.getInactiveNodes().size(), IsEqual.equalTo(expectedNumInactiveNodes));
+    }
+
+    private static void assertStatusListAssignment(
+        final NodeStatus status,
+        final int expectedNumActiveNodes,
+        final int expectedNumInactiveNodes) {
+        // Arrange:
+        final List<Node> nodes = createSingleItemNodeList(status, "Alpha");
+
+        // Assert:
+        assertStatusListAssignment(nodes, expectedNumActiveNodes, expectedNumInactiveNodes);
+    }
+
+    //endregion
+
+    //region serialization
+
+    @Test
+    public void canRoundTripNodeStatusDemux() {
+        // Arrange:
+        final List<Node> nodes = new ArrayList<>();
+        nodes.add(createNode(NodeStatus.ACTIVE, "A"));
+        nodes.add(createNode(NodeStatus.INACTIVE, "B"));
+        nodes.add(createNode(NodeStatus.INACTIVE, "C"));
+        nodes.add(createNode(NodeStatus.ACTIVE, "D"));
+        nodes.add(createNode(NodeStatus.FAILURE, "E"));
+        nodes.add(createNode(NodeStatus.ACTIVE, "F"));
+        final NodeStatusDemux originalDemux = new NodeStatusDemux(nodes);
+
+        // Assert:
+        NodeStatusDemux demux = new NodeStatusDemux(Utils.roundtripSerializableEntity(originalDemux, null));
+
+        // Assert:
+        Assert.assertThat(demux.getActiveNodes().size(), IsEqual.equalTo(3));
+        Assert.assertThat(demux.getInactiveNodes().size(), IsEqual.equalTo(2));
+    }
+
+    //endregion
+
+    private static List<Node> createSingleItemNodeList(final NodeStatus status, final String platform) {
+        // Arrange:
+        final List<Node> nodes = new ArrayList<>();
+        nodes.add(createNode(status, platform));
+        return nodes;
+    }
+
+    private static Node createNode(final NodeStatus status, final String platform) {
+        // Arrange:
+        final NodeInfo info = new NodeInfo(new NodeEndpoint("http", "localhost", 80), platform, "FooBar");
+        final Node node = new Node(info);
+        node.setStatus(status);
+        return node;
+    }
 }
