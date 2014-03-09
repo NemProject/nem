f75835d2446a82f1d6ee6f4cb55b83cf3944e30b
nem <nem@bob.nem.ninja>
2014-03-09 22:30:47 +0100

    ...

--- nis/src/main/java/org/nem/nis/BlockChain.java
+++ nis/src/main/java/org/nem/nis/BlockChain.java
@@ -14,7 +14,7 @@ import java.util.concurrent.ConcurrentMap;
 public class BlockChain {
 	private ConcurrentMap<byte[], Transaction> unconfirmedTransactions;
 
-	BlockChain() {
+	public BlockChain() {
 		unconfirmedTransactions = new ConcurrentHashMap<>();
 	}
 
---
+++ nis/src/test/java/org/nem/core/nis/BlockChainTest.java
@@ -0,0 +1,46 @@
+package org.nem.core.nis;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Test;
+import org.nem.core.model.Account;
+import org.nem.core.test.Utils;
+import org.nem.core.transactions.TransferTransaction;
+import org.nem.nis.BlockChain;
+
+public class BlockChainTest {
+	@Test
+	public void canProcessTransaction() {
+		final Account signer = Utils.generateRandomAccount();
+		final Account recipient = Utils.generateRandomAccount();
+		final BlockChain blockChain = new BlockChain();
+
+		// Act:
+		TransferTransaction transaction = new TransferTransaction(signer, recipient, 123, null);
+		transaction.sign();
+		boolean result = blockChain.processTransaction(transaction);
+
+		// Assert:
+		Assert.assertThat(transaction.verify(), IsEqual.equalTo(true));
+		Assert.assertThat(result, IsEqual.equalTo(true));
+	}
+
+	@Test
+	public void cannotProcessSameTransaction() {
+		final Account signer = Utils.generateRandomAccount();
+		final Account recipient = Utils.generateRandomAccount();
+		final BlockChain blockChain = new BlockChain();
+
+		// Act:
+		TransferTransaction transaction = new TransferTransaction(signer, recipient, 123, null);
+		transaction.sign();
+
+		boolean result1 = blockChain.processTransaction(transaction);
+		boolean result2 = blockChain.processTransaction(transaction);
+
+		// Assert:
+		Assert.assertThat(transaction.verify(), IsEqual.equalTo(true));
+		Assert.assertThat(result1, IsEqual.equalTo(false));
+		Assert.assertThat(result2, IsEqual.equalTo(false));
+	}
+}
