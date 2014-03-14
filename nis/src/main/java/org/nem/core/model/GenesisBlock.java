7924f52363f55405a48cd0247d1ad49ba6c62c02
nem <nem@bob.nem.ninja>
2014-03-13 21:55:20 -0400

    ...

---
+++ nis/src/main/java/org/nem/core/model/GenesisBlock.java
@@ -0,0 +1,64 @@
+package org.nem.core.model;
+
+import org.nem.core.crypto.KeyPair;
+import org.nem.core.transactions.TransferTransaction;
+import org.nem.core.utils.HexEncoder;
+
+import java.math.BigInteger;
+
+/**
+ * Represents the genesis block.
+ */
+public class GenesisBlock extends Block {
+
+    // this will be removed later, only public key will be present in the code
+    // all signatures will be pre-generated and placed in-code
+    private final static BigInteger CREATOR_PRIVATE_KEY = new BigInteger(
+        HexEncoder.getBytesSilent("aa761e0715669beb77f71de0ce3c29b792e8eb3130d21f697f59070665100c04"));
+
+    private final static int GENESIS_HEIGHT = 1;
+    private final static int HASH_LENGTH = 32;
+
+    private final static Account GENESIS_ACCOUNT;
+
+    private final static String[] GENESIS_RECIPIENT_ACCOUNT_IDS = new String[] {
+        "NBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI",
+        "NCBWD3TSIMFRHV67PQUQPRL5SZ5CEE6MUL2ANOON",
+        "NBI5SUNZOYBHM3D6Q7BOHP6K327EIJ6EETIIRTS2",
+        "NAUULYJ4MSHXON3GDQVUN4WFRTAQNADYL5KYTTX7",
+        "NBT7M43C4X25VDNSL34IRQO5IRKO6WXSMSJ4PCFP",
+        "NAXNGGK5JEU7EXXFLV4L2NCGNJAWBGEOPEI4XHUN",
+        "NCVRRAC4GIGMY5BIHDQZO3K6HLAJIDKYZDF7RO5H",
+        "NBMSVDI52MR3KSO7RGIJEGGMGZAGSKV4A3ZNJJSM",
+        "NBZUVLKB7THC5QH5IJUJVEF66QJZUCQLMVTIFXUC"
+    };
+
+    static {
+        final KeyPair genesisKeyPair = new KeyPair(CREATOR_PRIVATE_KEY);
+        GENESIS_ACCOUNT = new Account(genesisKeyPair);
+    }
+
+    private final static long GENESIS_AMOUNT = 4000000000L;
+
+    /**
+     * Creates a genesis block.
+     *
+     * @param timestamp The block timestamp.
+     */
+    public GenesisBlock(final int timestamp) {
+        super(GENESIS_ACCOUNT, new byte[HASH_LENGTH], timestamp, GENESIS_HEIGHT);
+
+        // TODO: as a placeholder distribute amounts equally
+        final long shareAmount = GENESIS_AMOUNT / GENESIS_RECIPIENT_ACCOUNT_IDS.length;
+        for (final String id : GENESIS_RECIPIENT_ACCOUNT_IDS) {
+            final Address address = Address.fromEncoded(id);
+            final Account account = new Account(address);
+            final TransferTransaction transaction = new TransferTransaction(0, GENESIS_ACCOUNT, account, shareAmount, null);
+            transaction.sign();
+            transaction.setFee(0); // TODO: this won't work because of minimum fee enforcement
+            this.addTransaction(transaction);
+        }
+
+        this.sign();
+    }
+}
---
+++ nis/src/test/java/org/nem/core/model/GenesisBlockTest.java
@@ -0,0 +1,45 @@
+package org.nem.core.model;
+
+import org.hamcrest.core.*;
+import org.junit.*;
+
+public class GenesisBlockTest {
+
+    private final static String GENESIS_ACCOUNT = "NBERUJIKSAPW54YISFOJZ2PLG3E7CACCNN2Z6SOW";
+
+    @Test
+    public void genesisBlockCanBeCreated() {
+        // Act:
+        final Block block = new GenesisBlock(12);
+
+        // Assert:
+        Assert.assertThat(block.getSigner().getAddress().getEncoded(), IsEqual.equalTo(GENESIS_ACCOUNT));
+        Assert.assertThat(block.getType(), IsEqual.equalTo(1));
+        Assert.assertThat(block.getVersion(), IsEqual.equalTo(1));
+        Assert.assertThat(block.getTimeStamp(), IsEqual.equalTo(12));
+
+//        Assert.assertThat(block.getTotalFee(), IsEqual.equalTo(0L)); TODO: verify
+        Assert.assertThat(block.getPreviousBlockHash(), IsEqual.equalTo(new byte[32]));
+        Assert.assertThat(block.getHeight(), IsEqual.equalTo(1L));
+//        Assert.assertThat(block.getTransactions().size(), IsEqual.equalTo(0)); TODO: verify
+    }
+
+    @Test
+    public void genesisBlockIsVerifiable() {
+        // Arrange:
+        final Block block = new GenesisBlock(0);
+
+        // Assert:
+        Assert.assertThat(block.verify(), IsEqual.equalTo(true));
+    }
+
+    @Test
+    public void genesisTransactionsAreVerifiable() {
+        // Arrange:
+        final Block block = new GenesisBlock(0);
+
+        // Assert:
+        for (final Transaction transaction : block.getTransactions())
+            Assert.assertThat(transaction.verify(), IsEqual.equalTo(true));
+    }
+}
