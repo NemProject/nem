7b90311b38d909e3ecb5135acfe65b8b3e31b041
nem <nem@bob.nem.ninja>
2014-03-16 14:48:57 +0100

    ...

--- nis/src/main/java/org/nem/core/time/SystemTimeProvider.java
+++ nis/src/main/java/org/nem/core/time/SystemTimeProvider.java
@@ -15,8 +15,8 @@ public class SystemTimeProvider implements TimeProvider {
 		calendar.set(Calendar.ERA, GregorianCalendar.AD);
         calendar.set(Calendar.YEAR, 2014);
         calendar.set(Calendar.MONTH, Calendar.MARCH);
-        calendar.set(Calendar.DAY_OF_MONTH, 9);
-        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.DAY_OF_MONTH, 16);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
