67f9fa3432e78b994f31a049af67571fb94ce8f0
nem <nem@bob.nem.ninja>
2014-03-23 11:55:07 -0400

    ...

---
+++ nis/src/main/java/org/nem/peer/trust/TrustUtils.java
@@ -0,0 +1,31 @@
+package org.nem.peer.trust;
+
+import org.nem.peer.*;
+
+/**
+ * Static class containing trust utility functions.
+ */
+public class TrustUtils {
+
+    /**
+     * Flattens all nodes in a NodeCollection and a local node into a node array.
+     *
+     * @param nodes The nodes.
+     * @param localNode The local node.
+     * @return An array containing all nodes in the network.
+     */
+    public static Node[] toNodeArray(final NodeCollection nodes, final Node localNode) {
+        final int numNodes = nodes.getActiveNodes().size() + nodes.getInactiveNodes().size() + 1;
+        final Node[] nodeArray = new Node[numNodes];
+
+        int index = 0;
+        for (final Node node : nodes.getActiveNodes())
+            nodeArray[index++] = node;
+
+        for (final Node node : nodes.getInactiveNodes())
+            nodeArray[index++] = node;
+
+        nodeArray[index] = localNode;
+        return nodeArray;
+    }
+}
