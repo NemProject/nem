5e5b8ea4e8abb8558e918b00f96aad7dadf0c065
nem <nem@bob.nem.ninja>
2014-03-25 20:58:17 -0400

    ...

--- nis/src/test/java/org/nem/nis/controller/TransferControllerTest.java
+++ nis/src/test/java/org/nem/nis/controller/TransferControllerTest.java
@@ -5,12 +5,10 @@ import org.apache.commons.codec.DecoderException;
 import org.hamcrest.core.IsEqual;
 import org.junit.Assert;
 import org.junit.Test;
-import org.nem.core.model.MessageTypes;
-import org.nem.core.model.TransactionTypes;
+import org.nem.core.model.*;
 import org.nem.core.serialization.JsonDeserializer;
 import org.nem.core.test.MockPeerConnector;
-import org.nem.core.utils.Base64Encoder;
-import org.nem.core.utils.HexEncoder;
+import org.nem.core.utils.*;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
@@ -31,11 +29,11 @@ public class TransferControllerTest {
 		JSONObject obj = new JSONObject();
 		obj.put("type", 123456);
 		obj.put("version", 1);
-		obj.put("recipient", "NBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI");
+		obj.put("recipient", getRecipientAccountId());
 		byte[] signersKey = HexEncoder.getBytes("02a7ac11bd1163850985f9db69ea23d536f568670bf55ba2e7c9e596260e6dbdfb");
 		obj.put("signer", Base64Encoder.getString(signersKey));
-		obj.put("amount", Long.valueOf(42));
-		obj.put("fee", Long.valueOf(1));
+		obj.put("amount", 42L);
+		obj.put("fee", 1L);
 		obj.put("timestamp", 0);
 		obj.put("deadline", 1);
 		JSONObject message = new JSONObject();
@@ -61,8 +59,8 @@ public class TransferControllerTest {
 		obj.put("recipient", "AAAANBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI");
 		byte[] signersKey = HexEncoder.getBytes("02a7ac11bd1163850985f9db69ea23d536f568670bf55ba2e7c9e596260e6dbdfb");
 		obj.put("signer", Base64Encoder.getString(signersKey));
-		obj.put("amount", Long.valueOf(42));
-		obj.put("fee", Long.valueOf(1));
+		obj.put("amount", 42L);
+		obj.put("fee", 1L);
 		obj.put("timestamp", 0);
 		obj.put("deadline", 1);
 		JSONObject message = new JSONObject();
@@ -85,11 +83,11 @@ public class TransferControllerTest {
 		JSONObject obj = new JSONObject();
 		obj.put("type", TransactionTypes.TRANSFER);
 		obj.put("version", 1);
-		obj.put("recipient", "NBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI");
+		obj.put("recipient", getRecipientAccountId());
 		byte[] signersKey = HexEncoder.getBytes("02a7ac11bd1163850985f9db69ea23d536f568670bf55ba2e7c9e596260e6dbdfb");
 		obj.put("signer", Base64Encoder.getString(signersKey));
-		obj.put("amount", Long.valueOf(-13));
-		obj.put("fee", Long.valueOf(1));
+		obj.put("amount", -13L);
+		obj.put("fee", 1L);
 		obj.put("timestamp", 0);
 		obj.put("deadline", 1);
 		JSONObject message = new JSONObject();
@@ -112,11 +110,11 @@ public class TransferControllerTest {
 		JSONObject obj = new JSONObject();
 		obj.put("type", TransactionTypes.TRANSFER);
 		obj.put("version", 1);
-		obj.put("recipient", "NBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI");
+		obj.put("recipient", getRecipientAccountId());
 		byte[] signersKey = HexEncoder.getBytes("02a7ac11bd1163850985f9db69ea23d536f568670bf55ba2e7c9e596260e6dbdfb");
 		obj.put("signer", Base64Encoder.getString(signersKey));
-		obj.put("amount", Long.valueOf(42));
-		obj.put("fee", Long.valueOf(1));
+		obj.put("amount", 42L);
+		obj.put("fee", 1L);
 		obj.put("timestamp", 0);
 		obj.put("deadline", 1);
 		JSONObject message = new JSONObject();
@@ -139,11 +137,11 @@ public class TransferControllerTest {
 		JSONObject obj = new JSONObject();
 		obj.put("type", TransactionTypes.TRANSFER);
 		obj.put("version", 1);
-		obj.put("recipient", "NBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI");
+		obj.put("recipient", getRecipientAccountId());
 		byte[] signersKey = HexEncoder.getBytes("02a7ac11bd1163850985f9db69ea23d536f568670bf55ba2e7c9e596260e6dbdfb");
 		obj.put("signer", Base64Encoder.getString(signersKey));
-		obj.put("amount", Long.valueOf(42));
-		obj.put("fee", Long.valueOf(1));
+		obj.put("amount", 42L);
+		obj.put("fee", 1L);
 		obj.put("timestamp", 0);
 		obj.put("deadline", 1);
 		JSONObject message = new JSONObject();
@@ -166,11 +164,11 @@ public class TransferControllerTest {
 		JSONObject obj = new JSONObject();
 		obj.put("type", TransactionTypes.TRANSFER);
 		obj.put("version", 1);
-		obj.put("recipient", "NBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI");
+		obj.put("recipient", getRecipientAccountId());
 		byte[] signersKey = HexEncoder.getBytes("4202a7ac11bd1163850985f9db69ea23d536f568670bf55ba2e7c9e596260e6dbdfb");
 		obj.put("signer", Base64Encoder.getString(signersKey));
-		obj.put("amount", Long.valueOf(42));
-		obj.put("fee", Long.valueOf(1));
+		obj.put("amount", 42L);
+		obj.put("fee", 1L);
 		obj.put("timestamp", 0);
 		obj.put("deadline", 1);
 		JSONObject message = new JSONObject();
@@ -193,11 +191,11 @@ public class TransferControllerTest {
 		JSONObject obj = new JSONObject();
 		obj.put("type", TransactionTypes.TRANSFER);
 		obj.put("version", 1);
-		obj.put("recipient", "NBKLYTH6OWWQCQ6OI66HJOPBGLXWVQG6V2UTQEUI");
+		obj.put("recipient", getRecipientAccountId());
 		byte[] signersKey = HexEncoder.getBytes("02a7ac11bd1163850985f9db69ea23d536f568670bf55ba2e7c9e596260e6dbdfb");
 		obj.put("signer", Base64Encoder.getString(signersKey));
-		obj.put("amount", Long.valueOf(42));
-		obj.put("fee", Long.valueOf(1));
+		obj.put("amount", 42L);
+		obj.put("fee", 1L);
 		obj.put("timestamp", 0);
 		obj.put("deadline", 1);
 		JSONObject message = new JSONObject();
@@ -211,4 +209,8 @@ public class TransferControllerTest {
 		// Assert:
         Assert.assertThat(res.readInt("status"), IsEqual.equalTo(null));
 	}
+
+    private static String getRecipientAccountId() {
+        return NetworkInfo.getDefault().getGenesisRecipientAccountIds()[2];
+    }
 }
