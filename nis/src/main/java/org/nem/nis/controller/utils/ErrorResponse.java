27e9ac2fe7d3fad69b7fdebc62919e4f290a65ee
nem <nem@bob.nem.ninja>
2014-04-14 19:06:00 -0400

    ...

---
+++ nis/src/main/java/org/nem/nis/controller/utils/ErrorResponse.java
@@ -0,0 +1,107 @@
+package org.nem.nis.controller.utils;
+
+import org.nem.core.serialization.*;
+import org.nem.core.time.SystemTimeProvider;
+import org.nem.core.time.TimeProvider;
+import org.springframework.http.HttpStatus;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Response that is returned when an action fails.
+ */
+class ErrorResponse implements SerializableEntity {
+
+	private static final Logger LOGGER = Logger.getLogger(ErrorResponse.class.getName());
+	private static final TimeProvider TIME_PROVIDER = new SystemTimeProvider();
+
+	private final int timeStamp;
+	private final String error;
+	private final int status;
+	private final String message;
+
+	/**
+	 * Creates a new error response.
+	 *
+	 * @param e The original exception.
+	 * @param status The desired HttpStatus.
+	 */
+	public ErrorResponse(final Exception e, final HttpStatus status) {
+		this(e.getMessage(), status.value());
+	}
+
+	/**
+	 * Creates a new error response.
+	 *
+	 * @param message The error message.
+	 * @param status The desired HTTP status code.
+	 */
+	public ErrorResponse(final String message, final int status) {
+		this.status = status;
+		this.error = getStatusReason(status);
+		this.message = message;
+		this.timeStamp = TIME_PROVIDER.getCurrentTime().getRawTime();
+
+		final Level logLevel = this.status >= 500 ? Level.SEVERE : Level.INFO;
+		LOGGER.log(logLevel, this.message);
+	}
+
+	/**
+	 * Deserializes an error response.
+	 *
+	 * @param deserializer The deserializer.
+	 */
+	public ErrorResponse(final Deserializer deserializer) {
+		this.status = deserializer.readInt("status");
+		this.error = deserializer.readString("error");
+		this.message = deserializer.readString("message");
+		this.timeStamp = deserializer.readInt("timeStamp");
+	}
+
+	/**
+	 * Gets the timestamp.
+	 *
+	 * @return The timestamp.
+	 */
+	public int getTimeStamp() { return this.timeStamp; }
+
+	/**
+	 * Gets the error reason phrase.
+	 *
+	 * @return The error reason phrase.
+	 */
+	public String getError() { return this.error; }
+
+	/**
+	 * Gets the error message.
+	 *
+	 * @return The error message.
+	 */
+	public String getMessage() { return this.message; }
+
+	/**
+	 * Gets the raw HTTP status.
+	 *
+	 * @return The raw HTTP status.
+	 */
+	public int getStatus() { return this.status; }
+
+	@Override
+	public void serialize(final Serializer serializer) {
+		serializer.writeInt("status", this.status);
+		serializer.writeString("error", this.error);
+		serializer.writeString("message", this.message);
+		serializer.writeInt("timeStamp", this.timeStamp);
+	}
+
+	private static String getStatusReason(final int rawStatus) {
+		try {
+			final HttpStatus status = HttpStatus.valueOf(rawStatus);
+			return status.getReasonPhrase();
+		}
+		catch (IllegalArgumentException e) {
+			return null;
+		}
+	}
+}
