9f675a0fe12e3b3a998c89e2b3aa86f68dc334ff
nem <nem@bob.nem.ninja>
2014-04-15 15:39:32 +0200

    ...

--- nis/src/main/java/org/nem/nis/BlockScorer.java
+++ nis/src/main/java/org/nem/nis/BlockScorer.java
@@ -10,6 +10,7 @@ import org.nem.core.utils.ArrayUtils;
 import org.nem.core.utils.ByteUtils;
 
 import java.math.BigInteger;
+import java.security.InvalidParameterException;
 import java.util.Arrays;
 
 /**
@@ -20,7 +21,27 @@ public class BlockScorer {
 	/**
 	 * 500_000_000 NEMs have force to generate block every minute.
 	 */
-	public static final long MAGIC_MULTIPLIER = 614891469L;
+	public static final long INITIAL_MAGIC_MULTIPLIER = 614891469L;
+	
+	/**
+	 * Minimum value for magic multiplier.
+	 */
+	public static final long MIN_MAGIC_MULTIPLIER = INITIAL_MAGIC_MULTIPLIER/10L;
+	
+	/**
+	 * Maximum value for magic multiplier.
+	 */
+	public static final long Max_MAGIC_MULTIPLIER = INITIAL_MAGIC_MULTIPLIER*10L;
+	
+	/**
+	 * The target time between two blocks in seconds.
+	 */
+	public static final long TARGET_TIME_BETWEEN_BLOCKS = 60;
+
+	/**
+	 * Current magic multiplier used in target calculation.
+	 */
+	public static long magicMultiplier = INITIAL_MAGIC_MULTIPLIER;
 
 	/**
 	 * Calculates the hit score for block.
@@ -60,7 +81,7 @@ public class BlockScorer {
 		long forgerBalance = blockSigner.getBalance().getNumNem();
 		return BigInteger.valueOf(timeStampDifference)
 				.multiply(BigInteger.valueOf(forgerBalance))
-				.multiply(BigInteger.valueOf(MAGIC_MULTIPLIER));
+				.multiply(BigInteger.valueOf(magicMultiplier));
 	}
 
 	/**
@@ -92,5 +113,32 @@ public class BlockScorer {
 	private static long intToUlong(int value) {
 		//final long fix = Math.abs((long)Integer.MIN_VALUE);
 		return Math.abs((long)value);
+	}
+	
+	/**
+	 * Calculates the new magic multiplier.
+	 */
+	public void calculateMagicMultiplier(final Block lastBlock, final Block historicalBlock) {
+		if (lastBlock.getHeight() == 1) {
+			magicMultiplier = INITIAL_MAGIC_MULTIPLIER;
+		}
+		else {
+			if (lastBlock.getHeight() <= historicalBlock.getHeight()) {
+				throw new InvalidParameterException("historicalBlock must have lower height than lastBlock");
+			}
+			
+			final long timeDiff = lastBlock.getTimeStamp().subtract(historicalBlock.getTimeStamp());
+			final long heightDiff = lastBlock.getHeight() - historicalBlock.getHeight();
+			magicMultiplier = BigInteger.valueOf(magicMultiplier).multiply(BigInteger.valueOf(timeDiff))
+																 .divide(BigInteger.valueOf(heightDiff))
+																 .divide(BigInteger.valueOf(TARGET_TIME_BETWEEN_BLOCKS))
+																 .longValue();
+            if (magicMultiplier < MIN_MAGIC_MULTIPLIER) {
+            	magicMultiplier = MIN_MAGIC_MULTIPLIER;
+            }
+            if (magicMultiplier > Max_MAGIC_MULTIPLIER) {
+            	magicMultiplier = Max_MAGIC_MULTIPLIER;
+            }
+		}
 	}
 }
