e7ed439eae433618d869b972f375b8e8995ff3ea
nem <nem@bob.nem.ninja>
2014-04-18 23:10:29 -0400

    ...

---
+++ nis/src/main/java/org/nem/core/model/BlockDifficulty.java
@@ -0,0 +1,47 @@
+package org.nem.core.model;
+
+/**
+ * Represents a NEM block height.
+ */
+public class BlockDifficulty extends AbstractPrimitive<BlockDifficulty> {
+
+	/**
+	 * The initial block difficulty.
+	 * 1_000_000_000 NEMs have force to generate block every minute.
+	 */
+	public static final BlockDifficulty INITIAL_DIFFICULTY = new BlockDifficulty(120_000_000_000L, false);
+
+	/**
+	 * Minimum value for difficulty.
+	 */
+	private static final long MIN_DIFFICULTY = INITIAL_DIFFICULTY.getRaw() / 10L;
+
+	/**
+	 * Maximum value for difficulty.
+	 */
+	private static final long MAX_DIFFICULTY = INITIAL_DIFFICULTY.getRaw() * 10L;
+
+	/**
+	 * Creates a block difficulty and automatically clamps the raw value.
+	 *
+	 * @param difficulty The block difficulty.
+	 */
+	public BlockDifficulty(long difficulty) {
+		this(difficulty, true);
+	}
+
+	private BlockDifficulty(long difficulty, boolean clamp) {
+		super(clamp ? Clamp(difficulty) : difficulty, BlockDifficulty.class);
+	}
+
+	/**
+	 * Returns the underlying difficulty.
+	 *
+	 * @return The underlying difficulty.
+	 */
+	public long getRaw() { return this.getValue(); }
+
+	private static long Clamp(long difficulty) {
+		return Math.min(MAX_DIFFICULTY, Math.max(MIN_DIFFICULTY, difficulty));
+	}
+}
---
+++ nis/src/test/java/org/nem/core/model/BlockDifficultyTest.java
@@ -0,0 +1,50 @@
+package org.nem.core.model;
+
+import org.hamcrest.core.*;
+import org.junit.*;
+
+public class BlockDifficultyTest {
+
+	//region constants
+
+	@Test
+	public void constantsAreInitializedCorrectly() {
+		// Assert:
+		Assert.assertThat(
+				BlockDifficulty.INITIAL_DIFFICULTY,
+				IsEqual.equalTo(new BlockDifficulty(120_000_000_000L)));
+	}
+
+	//endregion
+
+	//region constructor
+
+	@Test
+	public void valueInDifficultyRangeIsNotChanged() {
+		// Arrange:
+		final BlockDifficulty difficulty = new BlockDifficulty(987_654_321_123L);
+
+		// Assert:
+		Assert.assertThat(difficulty.getRaw(), IsEqual.equalTo(987_654_321_123L));
+	}
+
+	@Test
+	public void valueBelowMinDifficultyIsIncreasedToMinDifficulty() {
+		// Arrange:
+		final BlockDifficulty difficulty = new BlockDifficulty(7_654_321_123L);
+
+		// Assert:
+		Assert.assertThat(difficulty.getRaw(), IsEqual.equalTo(12_000_000_000L));
+	}
+
+	@Test
+	public void valueAboveMaxDifficultyIsDecreasedToMaxDifficulty() {
+		// Arrange:
+		final BlockDifficulty difficulty = new BlockDifficulty(2_987_654_321_123L);
+
+		// Assert:
+		Assert.assertThat(difficulty.getRaw(), IsEqual.equalTo(1_200_000_000_000L));
+	}
+
+	//endregion
+}
