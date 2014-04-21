fd4848c860f970c344208f0347257016901f333e
nem <nem@bob.nem.ninja>
2014-04-21 18:36:43 -0400

    ...

--- nis/src/main/java/org/nem/nis/dao/BlockDao.java
+++ nis/src/main/java/org/nem/nis/dao/BlockDao.java
@@ -9,20 +9,7 @@ import java.util.List;
 /**
  * DAO for accessing db Block objects.
  */
-public interface BlockDao {
-	/**
-	 * Saves full block in the database, along with associated transactions, forgers, signers, etc.
-	 *
-	 * @param block
-	 */
-	public void save(Block block);
-
-	/**
-	 * Updates lastBlockId of this block using id of given block.
-	 *
-	 * @param block Block whose id will be used.
-	 */
-	public void updateLastBlockId(Block block);
+public interface BlockDao extends ReadOnlyBlockDao {
 
 	/**
 	 * Returns number of blocks in the database.
@@ -32,41 +19,18 @@ public interface BlockDao {
 	public Long count();
 
 	/**
-	 * Retrieves Block from db given it's id in the database.
+	 * Saves full block in the database, along with associated transactions, forgers, signers, etc.
 	 *
-	 * @param id id of a block.
-	 *
-	 * @return associated Block or null if there's no block with such id.
+	 * @param block Block to save.
 	 */
-	public Block findById(long id);
-
-    /**
-	 * Retrieves Block from db given it's hash.
-	 *
-	 * @param blockHash hash of a block to retrieve.
-	 *
-	 * @return Block having given hash or null.
-	 */
-	public Block findByHash(final Hash blockHash);
+	public void save(Block block);
 
 	/**
-	 * Retrieves Block from db at given height.
+	 * Updates lastBlockId of this block using id of given block.
 	 *
-	 * @param height height of a block to retrieve.
-	 *
-	 * @return Block at given height or null.
+	 * @param block Block whose id will be used.
 	 */
-	public Block findByHeight(final BlockHeight height);
-
-    /**
-     * Retrieves list of at most limit hashes for blocks starting at given height.
-     * This should be used, not to pull whole block from the db.
-     *
-	 * @param height height of a first block.
-	 * @param limit maximal number of elements to return.
-	 * @return HashChain.
-     */
-    public HashChain getHashesFrom(final BlockHeight height, int limit);
+	public void updateLastBlockId(Block block);
 
 	/**
 	 * Retrieves list of at most limit difficulties for blocks starting at given height.
