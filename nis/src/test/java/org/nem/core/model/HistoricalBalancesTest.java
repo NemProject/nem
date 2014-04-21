850d684ff002efc85016a21dfca1f0cd84504c21
nem <nem@bob.nem.ninja>
2014-04-21 12:54:53 +0200

    ...

--- nis/src/test/java/org/nem/core/model/HistoricalBalancesTest.java
+++ nis/src/test/java/org/nem/core/model/HistoricalBalancesTest.java
@@ -6,4 +6,39 @@ import org.junit.Test;
 
 public class HistoricalBalancesTest {
 
+	//region add/subtract/retrieve
+	@Test
+	public void historicalBalanceCanBeaddedAndRetrieved() {
+		// Arrange:
+		final HistoricalBalances balances = new HistoricalBalances();
+		
+		// Act:
+		balances.add(new BlockHeight(20L), new Amount(2L));
+		balances.add(new BlockHeight(30L), new Amount(8L));
+		balances.add(new BlockHeight(25L), new Amount(4L));
+		balances.add(new BlockHeight(10L), new Amount(1L));
+
+		// Assert:
+		Assert.assertThat(balances.getHistoricalBalance(new BlockHeight(1L)).getBalance().getNumMicroNem(), IsEqual.equalTo(0L));
+		Assert.assertThat(balances.getHistoricalBalance(new BlockHeight(10L)).getBalance().getNumMicroNem(), IsEqual.equalTo(1L));
+		Assert.assertThat(balances.getHistoricalBalance(new BlockHeight(11L)).getBalance().getNumMicroNem(), IsEqual.equalTo(1L));
+		Assert.assertThat(balances.getHistoricalBalance(new BlockHeight(20L)).getBalance().getNumMicroNem(), IsEqual.equalTo(3L));
+		Assert.assertThat(balances.getHistoricalBalance(new BlockHeight(25L)).getBalance().getNumMicroNem(), IsEqual.equalTo(7L));
+		Assert.assertThat(balances.getHistoricalBalance(new BlockHeight(30L)).getBalance().getNumMicroNem(), IsEqual.equalTo(15L));
+		Assert.assertThat(balances.getHistoricalBalance(new BlockHeight(35L)).getBalance().getNumMicroNem(), IsEqual.equalTo(15L));
+	}
+	//endregion
+
+
+	//region Retrieval
+	@Test
+	public void historicalBalanceCanBeRetrieved() {
+		// Arrange:
+		
+		// Act:
+
+		// Assert:
+	
+	}
+	//endregion
 }
