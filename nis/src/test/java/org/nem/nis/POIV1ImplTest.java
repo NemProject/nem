d93fdf217654e55a0591bae3340530cd666a0230
nem <nem@bob.nem.ninja>
2014-04-26 23:00:27 +0900

    ...

---
+++ nis/src/test/java/org/nem/nis/POIV1ImplTest.java
@@ -0,0 +1,13 @@
+package org.nem.nis;
+
+/**
+ * some tests we should consider:
+ * - Sybil attack (master node creates a ton of other nodes and transacts with them (and maybe some other nodes) to try to boost score)
+ * - infinite loop attack
+ * - closed loop attack
+ * - small transaction spam attack
+ * - 
+ */
+public class POIV1ImplTest {
+
+}
