78f616dbc1d94493ebd3703914e23f6321b84784
nem <nem@bob.nem.ninja>
2014-04-27 23:39:20 +0200

    ...

---
+++ nis/src/main/java/org/nem/nis/service/AccountIoAdapter.java
@@ -0,0 +1,47 @@
+package org.nem.nis.service;
+
+import org.nem.core.model.*;
+import org.nem.core.serialization.AccountLookup;
+import org.nem.nis.dao.TransferDao;
+import org.nem.nis.dbmodel.Transfer;
+import org.nem.nis.mappers.TransferMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.MissingResourceException;
+
+@Service
+public class AccountIoAdapter implements AccountIo {
+	private final TransferDao transferDao;
+	private final AccountLookup accountLookup;
+
+	@Autowired(required = true)
+	public AccountIoAdapter(final TransferDao transferDao, final AccountLookup accountLookup) {
+		this.transferDao = transferDao;
+		this.accountLookup = accountLookup;
+	}
+
+	@Override
+	public Account findByAddress(Address address) {
+		return this.accountLookup.findByAddress(address);
+	}
+
+	@Override
+	public SerializableList<Transaction> getAccountTransfers(Address address) {
+//		final Account account = this.accountLookup.findByAddress(address);
+//
+//		Collection<Transfer> transfers = transferDao.getTransactionsForAccount(account, 25);
+//
+//		final SerializableList<Transaction> transactionList = new SerializableList<>(0);
+//		transfers.stream().map(tr -> TransferMapper.toModel(tr, this.accountLookup)).forEach(
+//				transaction -> transactionList.add(transaction)
+//		);
+//		return transactionList;
+		throw createMissingResourceException(address.getEncoded());
+	}
+
+	private static MissingResourceException createMissingResourceException(final String key) {
+		return new MissingResourceException("account not found", AccountIoAdapter.class.getName(), key);
+	}
+}
