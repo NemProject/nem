d96f5e7b83b3d295a7f93ec4520a208eb35ba385
nem <nem@bob.nem.ninja>
2014-04-30 19:16:57 +0200

    ...

--- nis/src/main/java/org/nem/nis/service/BlockChainLastBlockLayer.java
+++ nis/src/main/java/org/nem/nis/service/BlockChainLastBlockLayer.java
@@ -1,5 +1,6 @@
 package org.nem.nis.service;
 
+import org.nem.core.model.Account;
 import org.nem.core.model.Block;
 import org.nem.core.model.BlockHeight;
 import org.nem.nis.BlockChain;
@@ -12,6 +13,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
+/**
+ * This is intermediate layer between blocchain or foraging and actual Dao
+ *
+ * TODO: not sure if other dau functions should be moved here, probably not
+ */
 @Service
 public class BlockChainLastBlockLayer {
 	private static final Logger LOGGER = Logger.getLogger(BlockChain.class.getName());
@@ -22,28 +28,46 @@ public class BlockChainLastBlockLayer {
 	private org.nem.nis.dbmodel.Block lastBlock;
 
 	@Autowired(required = true)
-	BlockChainLastBlockLayer(final AccountDao accountDao, final BlockDao blockDao) {
+	public BlockChainLastBlockLayer(final AccountDao accountDao, final BlockDao blockDao) {
 		this.accountDao = accountDao;
 		this.blockDao = blockDao;
 	}
 
+	/**
+	 * Returns last block in the db.
+	 * @return last block from db.
+	 */
 	public org.nem.nis.dbmodel.Block getLastDbBlock() {
 		return lastBlock;
 	}
 
+	/**
+	 * Returns height of last block in the db.
+	 * @return height of last block in the db.
+	 */
 	public Long getLastBlockHeight() {
 		return lastBlock.getHeight();
 	}
 
+	/**
+	 * Analyzes last block, used during initial initialization of blocks in blockchain.
+	 * @param curBlock lastBlock in db.
+	 */
 	public void analyzeLastBlock(org.nem.nis.dbmodel.Block curBlock) {
 		LOGGER.info("analyzing last block: " + Long.toString(curBlock.getShortId()));
 		lastBlock = curBlock;
 	}
 
-	public boolean addBlockToDb(Block bestBlock) {
+	/**
+	 * Adds new block into db
+	 *
+	 * @param block block to be added to db
+	 * @return always true
+	 */
+	public boolean addBlockToDb(Block block) {
 		synchronized (this) {
 
-			final org.nem.nis.dbmodel.Block dbBlock = BlockMapper.toDbModel(bestBlock, new AccountDaoLookupAdapter(this.accountDao));
+			final org.nem.nis.dbmodel.Block dbBlock = BlockMapper.toDbModel(block, new AccountDaoLookupAdapter(this.accountDao));
 
 			// hibernate will save both block AND transactions
 			// as there is cascade in Block
@@ -60,6 +84,9 @@ public class BlockChainLastBlockLayer {
 		return true;
 	}
 
+	/**
+	 * Removes block from the db after specified height
+	 */
 	public void dropDbBlocksAfter(final BlockHeight height) {
 		blockDao.deleteBlocksAfterHeight(height);
 		lastBlock = blockDao.findByHeight(height);
