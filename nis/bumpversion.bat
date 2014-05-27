a32b9e77cce509808623ea5590fb730f9eb4ee9a
nem <nem@bob.nem.ninja>
2014-05-27 19:16:55 +0200

    ...

---
+++ nis/bumpversion.bat
@@ -0,0 +1,19 @@
+rem batch files have funny escaping
+rem
+
+awk "{ if (match($0, /(.*)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom.xml > pom.out
+tr --delete "\r" < pom.out > pom.xml
+
+awk "{ if (match($0, /(.*)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom-core.xml > pom.out
+tr --delete "\r" < pom.out > pom-core.xml
+
+awk "{ if (match($0, /(.*)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom-mariadb.xml > pom.out
+tr --delete "\r" < pom.out > pom-mariadb.xml
+rm pom.out
+
+git add pom.xml
+git add pom-core.xml
+git add pom-mariadb.xml
+sed -n "/ALPHA/{ s/.*\([0-9]\+.[0-9]\+.[0-9]\+-ALPHA\).*/bump version to \1/; p }" pom.xml | xargs -IXX git commit -m XX
+sed -n "/ALPHA/{ s/.*\([0-9]\+.[0-9]\+.[0-9]\+\)-ALPHA.*/alpha_\1/; p }" pom.xml | xargs -IXX git tag XX
+echo "don't forget to push branch and tag"
