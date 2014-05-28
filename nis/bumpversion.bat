12ecc88387a32befa614087c317664a0f0e220d5
nem <nem@bob.nem.ninja>
2014-05-28 21:42:12 +0200

    ...

--- nis/bumpversion.bat
+++ nis/bumpversion.bat
@@ -1,13 +1,13 @@
 rem batch files have funny escaping
 rem
 
-awk "{ if (match($0, /(.*)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom.xml > pom.out
+awk "{ if (match($0, /(.*\.)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom.xml > pom.out
 tr --delete "\r" < pom.out > pom.xml
 
-awk "{ if (match($0, /(.*)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom-core.xml > pom.out
+awk "{ if (match($0, /(.*\.)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom-core.xml > pom.out
 tr --delete "\r" < pom.out > pom-core.xml
 
-awk "{ if (match($0, /(.*)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom-mariadb.xml > pom.out
+awk "{ if (match($0, /(.*\.)([0-9]+)(-ALPHA.*)/, arr)) { printf \"%%s%%d%%s\n\", arr[1], arr[2]+1, arr[3] } else { print } }" < pom-mariadb.xml > pom.out
 tr --delete "\r" < pom.out > pom-mariadb.xml
 rm pom.out
 
