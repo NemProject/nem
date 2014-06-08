72ee6a1da48bb650b72543d31ff10cc6dd6c0f83
nem <nem@bob.nem.ninja>
2014-06-08 11:13:07 +0200

    ...

--- nis/src/main/java/org/nem/core/model/Account.java
+++ nis/src/main/java/org/nem/core/model/Account.java
@@ -22,6 +22,7 @@ public class Account implements SerializableEntity {
 	private final AccountImportance importance;
 
 	private BlockHeight height;
+	private ReferenceCounter refCount = ReferenceCounter.ZERO;
 
 	/**
 	 * Creates an account around a key pair.
@@ -280,6 +281,35 @@ public class Account implements SerializableEntity {
 		    this.height = height;
 	}
 
+	/**
+	 * Returns the reference counter.
+	 * 
+	 * @return The  reference counter.
+	 */
+	public ReferenceCounter getReferenceCounter() {
+		return this.refCount;
+	}
+	
+	/**
+	 * Increments the reference counter.
+	 * 
+	 * @return The new value of the reference counter.
+	 */
+	public ReferenceCounter incrementReferenceCounter() {
+		this.refCount = this.refCount.increment();
+		return this.refCount;
+	}
+	
+	/**
+	 * Decrements the reference counter.
+	 * 
+	 * @return The new value of the reference counter.
+	 */
+	public ReferenceCounter decrementReferenceCounter() {
+		this.refCount = this.refCount.decrement();
+		return this.refCount;
+	}
+	
 	/**
 	 * Gets the weighted balances associated with this account.
 	 *
---
+++ nis/src/main/java/org/nem/core/model/ReferenceCounter.java
@@ -0,0 +1,42 @@
+package org.nem.core.model;
+
+/**
+ * Represents a referencer counter.
+ */
+public class ReferenceCounter extends AbstractPrimitive<ReferenceCounter> {
+
+	/**
+	 * Value representing initial referenceCount.
+	 */
+	public static final ReferenceCounter ZERO = new ReferenceCounter(0);
+
+	protected ReferenceCounter(final long refCount) {
+		super(refCount, ReferenceCounter.class);
+		
+		if (this.getRaw() < 0)
+			throw new IllegalArgumentException("reference counter can't be negative");
+	}
+
+	/**
+	 * Returns the underlying reference counter.
+	 *
+	 * @return The underlying reference counter.
+	 */
+	public long getRaw() { 
+		return this.getValue(); 
+	}
+
+	/**
+	 * Increment the reference counter
+	 * 
+	 * @return The new value of the reference counter
+	 */
+	public ReferenceCounter increment() {
+		return new ReferenceCounter(this.getRaw() + 1);
+	}
+	
+	public ReferenceCounter decrement() {
+		return new ReferenceCounter(this.getRaw() - 1);
+	}
+	
+}
