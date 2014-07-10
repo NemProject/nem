77bf2c4d55d2ff81fb573ad07b018eb7096e2665
nem <nem@bob.nem.ninja>
2014-07-09 23:33:21 -0400

    ...

--- nis/src/main/java/org/nem/peer/connect/HttpCommunicator.java
+++ nis/src/main/java/org/nem/peer/connect/HttpCommunicator.java
@@ -9,8 +9,9 @@ import java.util.concurrent.CompletableFuture;
 /**
  * HTTP JSON-base communicator implementation.
  */
-public class HttpJsonCommunicator implements Communicator {
+public class HttpCommunicator implements Communicator {
 	private final HttpMethodClient<Deserializer> httpMethodClient;
+	private final boolean isBinaryMode;
 	private final HttpResponseStrategy<Deserializer> responseStrategy;
 	private final HttpResponseStrategy<Deserializer> voidResponseStrategy;
 
@@ -18,23 +19,34 @@ public class HttpJsonCommunicator implements Communicator {
 	 * Creates a new HTTP communicator.
 	 *
 	 * @param httpMethodClient The HTTP client to use.
+	 * @param communicationMode The communication mode.
 	 * @param context The deserialization context.
 	 */
-	public HttpJsonCommunicator(
+	public HttpCommunicator(
 			final HttpMethodClient<Deserializer> httpMethodClient,
+			final CommunicationMode communicationMode,
 			final DeserializationContext context) {
 		this.httpMethodClient = httpMethodClient;
-		this.responseStrategy = new HttpJsonResponseStrategy(context);
+		this.isBinaryMode = CommunicationMode.BINARY == communicationMode;
+		this.responseStrategy = this.createResponseStrategy(context);
 		this.voidResponseStrategy = new HttpVoidResponseStrategy();
 	}
 
 	@Override
 	public CompletableFuture<Deserializer> post(final URL url, final SerializableEntity entity) {
-		return this.httpMethodClient.post(url, new HttpJsonPostRequest(entity), this.responseStrategy).getFuture();
+		return this.httpMethodClient.post(url, this.createPostRequest(entity), this.responseStrategy).getFuture();
 	}
 
 	@Override
 	public CompletableFuture<Deserializer> postVoid(final URL url, final SerializableEntity entity) {
-		return this.httpMethodClient.post(url, new HttpJsonPostRequest(entity), this.voidResponseStrategy).getFuture();
+		return this.httpMethodClient.post(url, this.createPostRequest(entity), this.voidResponseStrategy).getFuture();
+	}
+
+	private HttpResponseStrategy<Deserializer> createResponseStrategy(final DeserializationContext context) {
+		return this.isBinaryMode ? new HttpBinaryResponseStrategy(context) : new HttpJsonResponseStrategy(context);
+	}
+
+	private HttpPostRequest createPostRequest(final SerializableEntity entity) {
+		return this.isBinaryMode ? new HttpBinaryPostRequest(entity) : new HttpJsonPostRequest(entity);
 	}
 }
