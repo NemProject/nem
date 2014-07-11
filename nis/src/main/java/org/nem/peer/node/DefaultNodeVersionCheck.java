e8a75544c2c166384c48936f1a0bbe62f8be596e
nem <nem@bob.nem.ninja>
2014-07-10 22:31:32 -0400

    ...

--- nis/src/main/java/org/nem/peer/node/DefaultNodeVersionCheck.java
+++ nis/src/main/java/org/nem/peer/node/DefaultNodeVersionCheck.java
@@ -4,6 +4,16 @@ public class DefaultNodeVersionCheck implements NodeVersionCheck {
 
 	@Override
 	public boolean check(final NodeVersion local, final NodeVersion remote) {
-		return true;
+		// always communicate with 0-version builds (likely developer builds) to facilitate testing
+		// otherwise, require matching major and minor versions
+		return isZero(local) || isZero(remote) || majorMinorMatches(local, remote);
+	}
+
+	private static boolean isZero(final NodeVersion version) {
+		return new NodeVersion(0, 0, 0, version.getTag()).equals(version);
+	}
+
+	private static boolean majorMinorMatches(final NodeVersion lhs, final NodeVersion rhs) {
+		return lhs.getMajorVersion() == rhs.getMajorVersion() && lhs.getMinorVersion() == rhs.getMinorVersion();
 	}
 }
