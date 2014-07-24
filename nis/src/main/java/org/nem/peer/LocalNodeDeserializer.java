0d9f5a576b2813ee3d9332f80851e9e43bce142c
nem <nem@bob.nem.ninja>
2014-07-23 23:35:40 -0400

    ...

--- nis/src/main/java/org/nem/peer/LocalNodeDeserializer.java
+++ nis/src/main/java/org/nem/peer/LocalNodeDeserializer.java
@@ -12,7 +12,13 @@ public class LocalNodeDeserializer implements ObjectDeserializer<Node> {
 	public Node deserialize(final Deserializer deserializer) {
 		final NodeIdentity identity = deserializer.readObject("identity", deser -> NodeIdentity.deserializeWithPrivateKey(deser));
 		final NodeEndpoint endpoint = deserializer.readObject("endpoint", obj -> new NodeEndpoint(obj));
-		final NodeMetaData metaData = deserializer.readObject("metaData", obj -> new NodeMetaData(obj));
+		final NodeMetaData metaData = deserializer.readObject("metaData", obj -> deserializeLocalNodeMetaData(obj));
 		return new Node(identity, endpoint, metaData);
+	}
+
+	private static NodeMetaData deserializeLocalNodeMetaData(final Deserializer deserializer) {
+		// the only property we care about is application; platform and version will get populated automatically
+		final String application = deserializer.readString("application");
+		return new NodeMetaData(null, application, null);
 	}
 }
--- nis/src/test/java/org/nem/peer/LocalNodeDeserializerTest.java
+++ nis/src/test/java/org/nem/peer/LocalNodeDeserializerTest.java
@@ -2,37 +2,121 @@ package org.nem.peer;
 
 import org.hamcrest.core.IsEqual;
 import org.junit.*;
-import org.nem.core.crypto.KeyPair;
+import org.nem.core.crypto.*;
 import org.nem.core.node.*;
 import org.nem.core.serialization.*;
+import org.nem.core.test.Utils;
 
 public class LocalNodeDeserializerTest {
 
 	@Test
-	public void localNodeDeserializerCanLoadNodeWithOwnedIdentity() {
+	public void canDeserializeNodeIdentityWithPrivateKey() {
 		// Arrange:
 		final KeyPair keyPair = new KeyPair();
 		final NodeIdentity identity = new NodeIdentity(keyPair);
-		final NodeEndpoint endpoint = new NodeEndpoint("http", "localhost", 8080);
-		final NodeMetaData metaData = new NodeMetaData("p", "a", NodeVersion.ZERO);
 
-		final JsonSerializer serializer = new JsonSerializer(true);
-		serializer.writeObject("identity", childSerializer -> {
-			childSerializer.writeBigInteger("private-key", keyPair.getPrivateKey().getRaw());
-			childSerializer.writeString("name", "trudy");
-		});
-		serializer.writeObject("endpoint", endpoint);
-		serializer.writeObject("metaData", metaData);
+		final TestDeserializerBuilder builder = new TestDeserializerBuilder();
+		builder.writeIdentity("trudy", keyPair);
+		builder.writeEndpoint();
+		builder.writeMetaData();
 
 		// Act:
-		final Deserializer deserializer = new JsonDeserializer(serializer.getObject(), null);
-		final Node node = new LocalNodeDeserializer().deserialize(deserializer);
+		final Node node = new LocalNodeDeserializer().deserialize(builder.getDeserializer());
 
 		// Assert:
 		Assert.assertThat(node.getIdentity(), IsEqual.equalTo(identity));
 		Assert.assertThat(node.getIdentity().getKeyPair().getPrivateKey(), IsEqual.equalTo(keyPair.getPrivateKey()));
+		Assert.assertThat(node.getIdentity().getName(), IsEqual.equalTo("trudy"));
 		Assert.assertThat(node.getIdentity().isOwned(), IsEqual.equalTo(true));
+	}
+
+	@Test
+	public void canDeserializeNodeEndpoint() {
+		// Arrange:
+		final NodeEndpoint endpoint = new NodeEndpoint("http", "localhost", 8080);
+
+		final TestDeserializerBuilder builder = new TestDeserializerBuilder();
+		builder.writeIdentity();
+		builder.writeEndpoint(endpoint);
+		builder.writeMetaData();
+
+		// Act:
+		final Node node = new LocalNodeDeserializer().deserialize(builder.getDeserializer());
+
+		// Assert:
 		Assert.assertThat(node.getEndpoint(), IsEqual.equalTo(endpoint));
-		Assert.assertThat(node.getMetaData().getPlatform(), IsEqual.equalTo("p"));
+	}
+
+	@Test
+	public void canDeserializeNodeMetaDataApplication() {
+		// Arrange:
+		final NodeMetaData metaData = new NodeMetaData("p", "a", new NodeVersion(3, 2, 1));
+
+		final TestDeserializerBuilder builder = new TestDeserializerBuilder();
+		builder.writeIdentity();
+		builder.writeEndpoint();
+		builder.writeMetaData(metaData);
+
+		// Act:
+		final Node node = new LocalNodeDeserializer().deserialize(builder.getDeserializer());
+
+		// Assert:
+		Assert.assertThat(node.getMetaData(), IsEqual.equalTo(new NodeMetaData(null, "a", null)));
+	}
+
+	@Test
+	public void localNodeDeserializerCanDeserializeNodeMetaDataWithOnlyApplication() {
+		// Arrange:
+		final TestDeserializerBuilder builder = new TestDeserializerBuilder();
+		builder.writeIdentity();
+		builder.writeEndpoint();
+		builder.writeMetaDataApplicationOnly("app");
+
+		// Act:
+		final Node node = new LocalNodeDeserializer().deserialize(builder.getDeserializer());
+
+		// Assert:
+		Assert.assertThat(node.getMetaData(), IsEqual.equalTo(new NodeMetaData(null, "app", null)));
+	}
+
+	private static class TestDeserializerBuilder {
+		private final JsonSerializer serializer = new JsonSerializer(true);
+
+		private void writeIdentity() {
+			this.writeIdentity("bob", new KeyPair());
+		}
+
+		private void writeIdentity(final String name, final KeyPair keyPair) {
+			this.serializer.writeObject("identity", childSerializer -> {
+				childSerializer.writeBigInteger("private-key", keyPair.getPrivateKey().getRaw());
+				childSerializer.writeString("name", name);
+			});
+		}
+
+		private void writeEndpoint() {
+			this.writeEndpoint(new NodeEndpoint("http", "localhost", 8080));
+		}
+
+		private void writeEndpoint(final NodeEndpoint endpoint) {
+			this.serializer.writeObject("endpoint", endpoint);
+		}
+
+		private void writeMetaData() {
+			this.writeMetaData(new NodeMetaData("p", "a", new NodeVersion(3, 2, 1)));
+		}
+
+		private void writeMetaData(final NodeMetaData metaData) {
+			this.serializer.writeObject("metaData", metaData);
+		}
+
+		private void writeMetaDataApplicationOnly(final String application) {
+			this.serializer.writeObject(
+					"metaData",
+					childSerializer -> childSerializer.writeString("application", application));
+		}
+
+		private Deserializer getDeserializer() {
+			return Utils.createDeserializer(this.serializer.getObject());
+		}
 	}
 }
