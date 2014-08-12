d9f624464e3dfe6a62cba53327698afeb1374af9
nem <nem@bob.nem.ninja>
2014-08-12 13:12:57 +0200

    ...

--- nis/src/main/java/org/nem/core/deploy/CommonStarter.java
+++ nis/src/main/java/org/nem/core/deploy/CommonStarter.java
@@ -32,15 +32,32 @@ import java.util.logging.*;
  * TODO-CR 20140811 - is spring able to inject dependencies into the CommonStarter (instance) constructor?
  * if so, we can inject a NIS / NCC policy class (that exposes the classes for instance) and avoid string -> class transformations
  * if not, what is here is fine
+ * BR: I can define a bean "CommonStarter" in the Application config (would have to rename Nis/NccAppConfig -> NemAppConfig for both Nis/Ncc),
+ *     and then auto wire a private field of type CommonStarterPolicy. But in the CommonStarterPolicy class, I would need to construct or auto wire
+ *     Ncc specific stuff like WebStartProxy, JarFileServlet, NccDefaultServlet, NisController, and NccWebAppInitializer. So how would I do that without
+ *     explicitly mention the class (e.g. a CommonStarterPolicy can't have a method public WebStartProxy getWebStartProxy();
+ *     because Nis doesn't know the class).
  */
 @WebListener
 public class CommonStarter implements ServletContextListener {
 	private static final Logger LOGGER = Logger.getLogger(CommonStarter.class.getName());
 
 	// TODO-CR 20140811: comment publics
+	// BR: Done, though i didn't know how to comment CommonStarter in a meaningful way..
+	/**
+	 * The publicly available system time provider.
+	 */
 	public static final TimeProvider TIME_PROVIDER = new SystemTimeProvider();
+
+	/**
+	 * The meta data (name, version,...) for this application.
+	 */
 	public static final ApplicationMetaData META_DATA = MetaDataFactory.loadApplicationMetaData(CommonStarter.class, TIME_PROVIDER);
-	public static final CommonStarter INSTANCE = new CommonStarter();
+
+	/**
+	 * The common starter.
+	 */
+	public static CommonStarter INSTANCE = new CommonStarter();
 
 	private static final int IDLE_TIMEOUT = 30000;
 	private static final int HTTPS_HEADER_SIZE = 8192;
@@ -52,14 +69,15 @@ public class CommonStarter implements ServletContextListener {
 	static {
 		// initialize logging before anything is logged; otherwise not all
 		// settings will take effect
-		loadConfigurationProperties();
+		loadCommonConfiguration();
 		initializeLogging();
 	}
 
-	private static void loadConfigurationProperties() {
+	private static void loadCommonConfiguration() {
 		ExceptionUtils.propagate(() -> {
 			// TODO-CR 20140811: don't access static member via instance
-			INSTANCE.configuration = new CommonConfiguration();
+			// BR: Done
+			configuration = new CommonConfiguration();
 			return configuration;
 		}, IllegalStateException::new);
 	}
@@ -76,20 +94,20 @@ public class CommonStarter implements ServletContextListener {
 			LOGGER.log(Level.SEVERE, "Stopping Jetty Server.", e);
 		} finally {
 			// Last chance to save configuration
-			LOGGER.info(String.format("%s %s shutdown...", CommonStarter.META_DATA.getAppName(), CommonStarter.META_DATA.getVersion()));
+			LOGGER.info(String.format("%s %s shutdown...", META_DATA.getAppName(), META_DATA.getVersion()));
 		}
 
 		System.exit(1);
 	}
 
 	private static void initializeLogging() {
-		try (final InputStream inputStream = CommonStarter.class.getClassLoader().getResourceAsStream("logalpha.properties")) {
+		try (final InputStream inputStream = CommonStarter.class.getClassLoader().getResourceAsStream("logalpha.properties");
+			 final InputStream inputStringStream = adaptFileLocation(inputStream)) {
 
 			// TODO-CR - should this stream be in a try block?
-			final InputStream inputStringStream = adaptFileLocation(inputStream);
+			// BR: Done. Unless you wanted it in a separate try block...
 			final LogManager logManager = LogManager.getLogManager();
 			logManager.readConfiguration(inputStringStream);
-
 			final File logFile = new File(logManager.getProperty("java.util.logging.FileHandler.pattern"));
 			final File logDirectory = new File(logFile.getParent());
 			if (!logDirectory.exists() && !logDirectory.mkdirs()) {
@@ -97,7 +115,7 @@ public class CommonStarter implements ServletContextListener {
 			}
 		} catch (final IOException e) {
 			LOGGER.severe("Could not load default logging properties file");
-			LOGGER.severe(e.getMessage());
+			LOGGER.severe(e.toString());
 		}
 	}
 
@@ -138,11 +156,11 @@ public class CommonStarter implements ServletContextListener {
 	private Server createServer() {
 		// Taken from Jetty doc
 		final QueuedThreadPool threadPool = new QueuedThreadPool();
-		threadPool.setMaxThreads(this.configuration.getMaxThreads());
+		threadPool.setMaxThreads(configuration.getMaxThreads());
 		final Server server = new Server(threadPool);
 		server.addBean(new ScheduledExecutorScheduler());
 
-		if (this.configuration.isNcc()) {
+		if (configuration.isNcc()) {
 			final Configuration.ClassList classList = Configuration.ClassList.setServerDefault(server);
 			classList.addAfter(
 					"org.eclipse.jetty.webapp.FragmentConfiguration",
@@ -159,7 +177,7 @@ public class CommonStarter implements ServletContextListener {
 	private Connector createConnector(Server server) {
 		final HttpConfiguration http_config = new HttpConfiguration();
 		http_config.setSecureScheme("https");
-		http_config.setSecurePort(this.configuration.getHttpsPort());
+		http_config.setSecurePort(configuration.getHttpsPort());
 		http_config.setOutputBufferSize(HTTPS_BUFFER_SIZE);
 		http_config.setRequestHeaderSize(HTTPS_HEADER_SIZE);
 		http_config.setResponseHeaderSize(HTTPS_HEADER_SIZE);
@@ -167,7 +185,7 @@ public class CommonStarter implements ServletContextListener {
 		http_config.setSendDateHeader(false);
 
 		final ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config));
-		http.setPort(this.configuration.getHttpPort());
+		http.setPort(configuration.getHttpPort());
 		http.setIdleTimeout(IDLE_TIMEOUT);
 		return http;
 	}
@@ -194,7 +212,7 @@ public class CommonStarter implements ServletContextListener {
 		}
 		LOGGER.info(String.format("%s is ready to serve. URL is \"%s\".",
 				CommonStarter.META_DATA.getAppName(),
-				this.configuration.getBaseUrl()));
+				configuration.getBaseUrl()));
 	}
 
 	public void stopServer() {
@@ -231,20 +249,20 @@ public class CommonStarter implements ServletContextListener {
 		this.server.setDumpBeforeStop(false);
 		this.server.setStopAtShutdown(true);
 
-		if (this.configuration.isNcc()) {
+		if (configuration.isNcc()) {
 			this.startWebApplication(this.server);
 		}
 
 		LOGGER.info("Calling start().");
-		startServer(this.server, new URL(this.configuration.getShutdownUrl()));
+		startServer(this.server, new URL(configuration.getShutdownUrl()));
 
-		if (this.configuration.isNcc()) {
+		if (configuration.isNcc()) {
 			this.getMethod("org.nem.deploy.WebStartProxy", "openWebBrowser", new Class[]{ String.class })
-					.invoke(null, this.configuration.getHomeUrl());
+					.invoke(null, configuration.getHomeUrl());
 
-			if (this.configuration.isWebStart()) {
+			if (configuration.isWebStart()) {
 				this.getMethod("org.nem.ncc.connector.NISController", "startNISViaWebStart", new Class[]{ String.class })
-						.invoke(null, this.configuration.getNisJnlpUrl());
+						.invoke(null, configuration.getNisJnlpUrl());
 			}
 		}
 	}
@@ -271,8 +289,8 @@ public class CommonStarter implements ServletContextListener {
 	public void contextInitialized(final ServletContextEvent event) {
 		// This is the replacement for the web.xml (new with Servlet 3.0)
 		try {
-			final String appConfigClassName = String.format("%s%s%s", "org.nem.deploy.", this.configuration.getShortServerName(), "AppConfig");
-			final String webAppInitializerClassName = String.format("%s%s%s", "org.nem.deploy.", this.configuration.getShortServerName(), "WebAppInitializer");
+			final String appConfigClassName = String.format("%s%s%s", "org.nem.deploy.", configuration.getShortServerName(), "AppConfig");
+			final String webAppInitializerClassName = String.format("%s%s%s", "org.nem.deploy.", configuration.getShortServerName(), "WebAppInitializer");
 			final Class appConfigClass = getClass(appConfigClassName);
 			final Class appWebAppInitializerClass = getClass(webAppInitializerClassName);
 			final AnnotationConfigApplicationContext appCtx = new AnnotationConfigApplicationContext(appConfigClass);
@@ -283,40 +301,51 @@ public class CommonStarter implements ServletContextListener {
 			final ServletContext context = event.getServletContext();
 			ServletRegistration.Dynamic dispatcher = context.addServlet("Spring MVC Dispatcher Servlet", new DispatcherServlet(webCtx));
 			dispatcher.setLoadOnStartup(1);
-			dispatcher.addMapping(String.format("%s%s", this.configuration.getApiContext(), "/*"));
+			dispatcher.addMapping(String.format("%s%s", configuration.getApiContext(), "/*"));
 
 			context.setInitParameter("contextClass", "org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
 
 			// TODO-CR - can you move this to separate function
-			if (this.configuration.isNcc()) {
-				ServletRegistration.Dynamic servlet = context.addServlet("FileServlet", "org.nem.ncc.web.servlet.JarFileServlet");
-				servlet.setInitParameter("maxCacheSize", "0");
-				servlet.addMapping(String.format("%s%s", this.configuration.getWebContext(), "/*"));
-				servlet.setLoadOnStartup(1);
-
-				servlet = context.addServlet("DefaultServlet", "org.nem.ncc.web.servlet.NccDefaultServlet");
-				servlet.addMapping("/");
-				servlet.setLoadOnStartup(1);
-			}
+			// BR: Done. Not sure about naming. Is it better to put "Optional" in the method name like createOptionalServlets?
+			createServlets(context);
 
 			// TODO-CR - can you move this to separate function
-			if (this.configuration.useDosFilter()) {
-				javax.servlet.FilterRegistration.Dynamic dosFilter = context.addFilter("DoSFilter", "org.eclipse.jetty.servlets.DoSFilter");
-				dosFilter.setInitParameter("delayMs", "1000");
-				dosFilter.setInitParameter("trackSessions", "false");
-				dosFilter.setInitParameter("maxRequestMs", "120000");
-				dosFilter.setInitParameter("ipWhitelist", "127.0.0.1");
-				dosFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
-
-				// GZIP filter
-				dosFilter = context.addFilter("GzipFilter", "org.eclipse.jetty.servlets.GzipFilter");
-				// Zipping following MimeTypes
-				dosFilter.setInitParameter("mimeTypes", MimeTypes.Type.APPLICATION_JSON.asString());
-				dosFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
-			}
+			// BR: Done.
+			createDosFilter(context);
 		} catch (final Exception e) {
 			//TODO-CR: pass e to runtime exception
-			throw new RuntimeException("Exception in contextInitialized");
+			// BR: Done.
+			throw new RuntimeException(String.format("Exception in contextInitialized: %s", e.toString()), e);
+		}
+	}
+
+	private void createServlets(final ServletContext context) {
+		if (configuration.isNcc()) {
+			ServletRegistration.Dynamic servlet = context.addServlet("FileServlet", "org.nem.ncc.web.servlet.JarFileServlet");
+			servlet.setInitParameter("maxCacheSize", "0");
+			servlet.addMapping(String.format("%s%s", configuration.getWebContext(), "/*"));
+			servlet.setLoadOnStartup(1);
+
+			servlet = context.addServlet("DefaultServlet", "org.nem.ncc.web.servlet.NccDefaultServlet");
+			servlet.addMapping("/");
+			servlet.setLoadOnStartup(1);
+		}
+	}
+
+	private void createDosFilter(final ServletContext context) {
+		if (configuration.useDosFilter()) {
+			javax.servlet.FilterRegistration.Dynamic dosFilter = context.addFilter("DoSFilter", "org.eclipse.jetty.servlets.DoSFilter");
+			dosFilter.setInitParameter("delayMs", "1000");
+			dosFilter.setInitParameter("trackSessions", "false");
+			dosFilter.setInitParameter("maxRequestMs", "120000");
+			dosFilter.setInitParameter("ipWhitelist", "127.0.0.1");
+			dosFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+
+			// GZIP filter
+			dosFilter = context.addFilter("GzipFilter", "org.eclipse.jetty.servlets.GzipFilter");
+			// Zipping following MimeTypes
+			dosFilter.setInitParameter("mimeTypes", MimeTypes.Type.APPLICATION_JSON.asString());
+			dosFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
 		}
 	}
 
