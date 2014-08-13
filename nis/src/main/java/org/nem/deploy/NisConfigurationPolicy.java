79f48a15f969797cf4a586567a7c514f0235398a
nem <nem@bob.nem.ninja>
2014-08-13 18:21:29 +0200

    ...

--- nis/src/main/java/org/nem/deploy/NisConfigurationPolicy.java
+++ nis/src/main/java/org/nem/deploy/NisConfigurationPolicy.java
@@ -35,7 +35,7 @@ public class NisConfigurationPolicy implements NemConfigurationPolicy {
 	}
 
 	@Override
-	public void startNISViaWebStart(final String nisJnlpUrl) {
-		throw new RuntimeException("startNISViaWebStart is not supposed to be called from NIS server.");
+	public void startNisViaWebStart(final String nisJnlpUrl) {
+		throw new RuntimeException("startNisViaWebStart is not supposed to be called from NIS server.");
 	}
 }
