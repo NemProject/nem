83689face168eb66247264b97dcee7ec9c53fb9d
nem <nem@bob.nem.ninja>
2014-08-18 22:41:50 -0400

    ...

--- nis/src/main/java/org/nem/core/serialization/TypeMismatchException.java
+++ nis/src/main/java/org/nem/core/serialization/TypeMismatchException.java
@@ -3,8 +3,7 @@ package org.nem.core.serialization;
 /**
  * Exception that is thrown to indicate a serialization failure caused by a property value having an incompatible type.
  */
-public class TypeMismatchException extends SerializationException {
-	private final String propertyName;
+public class TypeMismatchException extends InvalidPropertyException {
 
 	/**
 	 * Creates a new exception.
@@ -12,17 +11,6 @@ public class TypeMismatchException extends SerializationException {
 	 * @param propertyName The name of the property with the incompatible value.
 	 */
 	public TypeMismatchException(final String propertyName) {
-		super(String.format("property %s has an incompatible value", propertyName));
-
-		this.propertyName = propertyName;
-	}
-
-	/**
-	 * Gets the missing property name.
-	 *
-	 * @return The missing property name.
-	 */
-	public String getPropertyName() {
-		return this.propertyName;
+		super("property %s has an incompatible value", propertyName);
 	}
 }
