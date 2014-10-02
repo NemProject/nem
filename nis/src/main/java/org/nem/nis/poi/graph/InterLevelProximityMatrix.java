689f46477f0f4f06c6ed60f643925f5c79ec82d3
nem <nem@bob.nem.ninja>
2014-10-02 11:12:34 -0400

    ...

--- nis/src/main/java/org/nem/nis/poi/graph/InterLevelProximityMatrix.java
+++ nis/src/main/java/org/nem/nis/poi/graph/InterLevelProximityMatrix.java
@@ -19,14 +19,12 @@ public class InterLevelProximityMatrix {
 	 * 1) the size of the cluster containing a node.
 	 * 2) the number of clusters needed to cover the entire neighborhood of a node.
 	 */
-	private final SparseMatrix r;
+	private final Matrix r;
 
 	/**
 	 * Matrix that represents a binary mask denoting nodes in each cluster
 	 */
-	private final SparseMatrix a;
-
-	private final HashMap<ClusterId, Set<Integer>> clusterIdToNeighborhoodClusterIdsSetIndex;
+	private final Matrix a;
 
 	/**
 	 * Creates an inter-level proximity matrix.
@@ -36,92 +34,10 @@ public class InterLevelProximityMatrix {
 	 * @param outlinkMatrix The final outlink matrix (negative values removed), which represents the directed graph.
 	 */
 	public InterLevelProximityMatrix(final ClusteringResult clusters, final Neighborhood neighborhood, final Matrix outlinkMatrix) {
-		final int numNodes = clusters.numNodes();
-		final int numClusters = clusters.numClusters();
-
-		this.r = new SparseMatrix(numClusters, numNodes, 5);  //TODO: need to estimate good initial capacity.
-		this.a = new SparseMatrix(numNodes, numClusters, 5); // TODO: need to estimate good initial capacity.
-		this.clusterIdToNeighborhoodClusterIdsSetIndex = new HashMap<>();
-
-		computeMatrices(clusters, neighborhood, outlinkMatrix);
-	}
-
-	/**
-	 * Computes the two matrices needed for the NCD aware page rank calculation.
-	 *
-	 * @param clusteringResult The results from the clustering process.
-	 * @param neighborhood The neighborhoods of the nodes.
-	 * @param outlinkMatrix The final outlink matrix (negative values removed) which represents the directed graph.
-	 */
-	private void computeMatrices(final ClusteringResult clusteringResult, final Neighborhood neighborhood, final Matrix outlinkMatrix) {
-		int blockNum = 0;
-
-		// 0) For every node compute the set of cluster ids which cover the entire neighborhood of the node
-		final List<SparseBitmap> neighborhoodClusterIdsSet = computeNeighborhoodClusterIdsSet(clusteringResult, neighborhood, outlinkMatrix);
-
-		// 1) Regular clusters
-		handleClusters(clusteringResult.getClusters(), neighborhoodClusterIdsSet, blockNum);
-		blockNum += clusteringResult.getClusters().size();
-
-		// 2) Hubs
-		handleClusters(clusteringResult.getHubs(), neighborhoodClusterIdsSet, blockNum);
-		blockNum += clusteringResult.getHubs().size();
-
-		// 3) Outliers
-		handleClusters(clusteringResult.getOutliers(), neighborhoodClusterIdsSet, blockNum);
-	}
-
-	/**
-	 * Computes the entries in r and a for the given clusters.
-	 *
-	 * @param clusters Collection of clusters.
-	 * @param neighborhoodClusterIdsSet Set of clusters so that node u and all neighbors are contained in a cluster from xu.
-	 * @param row The row into which to put the values.
-	 */
-	private void handleClusters(final Collection<Cluster> clusters, final List<SparseBitmap> neighborhoodClusterIdsSet, int row) {
-		for (final Cluster cluster : clusters) {
-
-			for (final NodeId id : cluster.getMemberIds()) {
-				this.a.setAt(id.getRaw(), row, 1);
-			}
-
-			final int clusterSize = cluster.size();
-			final int currentRow = row;
-			final Set<Integer> indexSet = this.clusterIdToNeighborhoodClusterIdsSetIndex.getOrDefault(cluster.getId(),
-					Collections.newSetFromMap(new ConcurrentHashMap<>()));
-			indexSet.stream()
-					.forEach(i -> this.r.setAt(currentRow, i, 1.0 / (clusterSize * neighborhoodClusterIdsSet.get(i).cardinality())));
-
-			++row;
-		}
-	}
-
-	private List<SparseBitmap> computeNeighborhoodClusterIdsSet(
-			final ClusteringResult clusteringResult,
-			final Neighborhood neighborhood,
-			final Matrix outlinkMatrix) {
-		final List<SparseBitmap> neighborhoodClusterIdsSet = new ArrayList<>(outlinkMatrix.getColumnCount());
-
-		for (int i = 0; i < outlinkMatrix.getColumnCount(); ++i) {
-			final int id = i;
-			// TODO: does it make sense for getNeighboringCommunities to return sorted ids?
-			final SparseBitmap clusterIdsForNode = SparseBitmap.createFromUnsortedData(neighborhood.getNeighboringCommunities(new NodeId(id)).stream()
-					.filter(c -> c.getPivotId().getRaw() == id || outlinkMatrix.getAt(c.getPivotId().getRaw(), id) > 0.0)
-					.map(c -> {
-						ClusterId clusterId = clusteringResult.getIdForNode(c.getPivotId());
-						Set<Integer> indexSet = this.clusterIdToNeighborhoodClusterIdsSetIndex.get(clusterId);
-						if (indexSet == null) {
-							indexSet = Collections.newSetFromMap(new ConcurrentHashMap<>());
-							this.clusterIdToNeighborhoodClusterIdsSetIndex.put(clusterId, indexSet);
-						}
-						indexSet.add(id);
-						return clusterId;
-					})
-					.mapToInt(ClusterId::getRaw).toArray());
-			neighborhoodClusterIdsSet.add(clusterIdsForNode);
-		}
-
-		return neighborhoodClusterIdsSet;
+		final MatrixBuilder builder = new MatrixBuilder(clusters, neighborhood, outlinkMatrix);
+		builder.build();
+		this.r = builder.r;
+		this.a = builder.a;
 	}
 
 	/**
@@ -129,7 +45,7 @@ public class InterLevelProximityMatrix {
 	 *
 	 * @return The matrix r.
 	 */
-	public SparseMatrix getR() {
+	public Matrix getR() {
 		return this.r;
 	}
 
@@ -138,7 +54,100 @@ public class InterLevelProximityMatrix {
 	 *
 	 * @return the matrix a.
 	 */
-	public SparseMatrix getA() {
+	public Matrix getA() {
 		return this.a;
+	}
+
+	private class MatrixBuilder {
+		private final ClusteringResult clusteringResult;
+		private final Neighborhood neighborhood;
+		private final Matrix outlinkMatrix;
+		private final Matrix a;
+		private final Matrix r;
+		private final HashMap<ClusterId, Set<Integer>> clusterIdToNeighborhoodClusterIdsSetIndex = new HashMap<>();
+		private int blockNum; // block == cluster; where cluster is { cluster, hub, outlier }
+
+		public MatrixBuilder(
+				final ClusteringResult clusteringResult,
+				final Neighborhood neighborhood,
+				final Matrix outlinkMatrix) {
+			this.clusteringResult = clusteringResult;
+			this.neighborhood = neighborhood;
+			this.outlinkMatrix = outlinkMatrix;
+
+			final int numNodes = this.clusteringResult.numNodes();
+			final int numClusters = this.clusteringResult.numClusters();
+
+			final int initialCapacity = Math.max(5, numNodes / numClusters);
+			this.r = new SparseMatrix(numClusters, numNodes, initialCapacity);
+			this.a = new SparseMatrix(numNodes, numClusters, initialCapacity);
+		}
+
+		/**
+		 * Computes the two matrices needed for the NCD aware page rank calculation.
+		 */
+		private void build() {
+			// 0) For every node compute the set of cluster ids which cover the entire neighborhood of the node
+			final List<SparseBitmap> neighborhoodClusterIdsSet = computeNeighborhoodClusterIdsSet();
+
+			// 1) Regular clusters
+			this.processClusters(this.clusteringResult.getClusters(), neighborhoodClusterIdsSet);
+
+			// 2) Hubs
+			this.processClusters(this.clusteringResult.getHubs(), neighborhoodClusterIdsSet);
+
+			// 3) Outliers
+			this.processClusters(this.clusteringResult.getOutliers(), neighborhoodClusterIdsSet);
+		}
+
+		/**
+		 * Computes the entries in r and a for the given clusters.
+		 *
+		 * @param clusters Collection of clusters.
+		 * @param neighborhoodClusterIdsSet Set of clusters so that node u and all neighbors are contained in a cluster from xu.
+		 */
+		private void processClusters(final Collection<Cluster> clusters, final List<SparseBitmap> neighborhoodClusterIdsSet) {
+			for (final Cluster cluster : clusters) {
+
+				for (final NodeId id : cluster.getMemberIds()) {
+					this.a.setAt(id.getRaw(), this.blockNum, 1);
+				}
+
+				final int clusterSize = cluster.size();
+				final int currentRow = this.blockNum;
+				final Set<Integer> indexSet = this.clusterIdToNeighborhoodClusterIdsSetIndex.getOrDefault(cluster.getId(),
+						Collections.newSetFromMap(new ConcurrentHashMap<>()));
+				indexSet.stream()
+						.forEach(i -> this.r.setAt(currentRow, i, 1.0 / (clusterSize * neighborhoodClusterIdsSet.get(i).cardinality())));
+
+				++this.blockNum;
+			}
+		}
+
+		private List<SparseBitmap> computeNeighborhoodClusterIdsSet() {
+			final List<SparseBitmap> neighborhoodClusterIdsSet = new ArrayList<>(this.outlinkMatrix.getColumnCount());
+			for (int i = 0; i < this.outlinkMatrix.getColumnCount(); ++i) {
+				final int id = i;
+				final Collection<Community> neighboringCommunities = this.neighborhood.getNeighboringCommunities(new NodeId(id));
+				final int[] clusterIdsForNode = neighboringCommunities.stream()
+						.filter(c -> c.getPivotId().getRaw() == id || this.outlinkMatrix.getAt(c.getPivotId().getRaw(), id) > 0.0)
+						.map(c -> {
+							ClusterId clusterId = this.clusteringResult.getIdForNode(c.getPivotId());
+							Set<Integer> indexSet = this.clusterIdToNeighborhoodClusterIdsSetIndex.get(clusterId);
+							if (indexSet == null) {
+								indexSet = Collections.newSetFromMap(new ConcurrentHashMap<>());
+								this.clusterIdToNeighborhoodClusterIdsSetIndex.put(clusterId, indexSet);
+							}
+							indexSet.add(id);
+							return clusterId;
+						})
+						.mapToInt(ClusterId::getRaw)
+						.toArray();
+				// TODO: does it make sense for getNeighboringCommunities to return sorted ids?
+				neighborhoodClusterIdsSet.add(SparseBitmap.createFromUnsortedData(clusterIdsForNode));
+			}
+
+			return neighborhoodClusterIdsSet;
+		}
 	}
 }
