a0aa807a381d6996be96c17cd1786721192c99d1
nem <nem@bob.nem.ninja>
2014-10-04 12:54:36 +0200

    ...

--- nis/src/main/java/org/nem/nis/service/AccountInfoFactory.java
+++ nis/src/main/java/org/nem/nis/service/AccountInfoFactory.java
@@ -2,9 +2,10 @@ package org.nem.nis.service;
 
 import org.nem.core.model.*;
 import org.nem.core.model.ncc.AccountInfo;
+import org.nem.core.model.primitive.BlockHeight;
 import org.nem.core.serialization.AccountLookup;
-import org.nem.nis.poi.PoiFacade;
-import org.nem.nis.secret.AccountImportance;
+import org.nem.nis.poi.*;
+import org.nem.nis.secret.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,15 +34,34 @@ public class AccountInfoFactory {
 	 * Creates an account info for a specified account.
 	 *
 	 * @param address The account address.
-	 * @return The info.
+	 * @param height
+     * @return The info.
 	 */
-	public AccountInfo createInfo(final Address address) {
+	public AccountInfo createInfo(final Address address, final BlockHeight height) {
 		final Account account = this.accountLookup.findByAddress(address);
-		final AccountImportance ai = this.poiFacade.findStateByAddress(address).getImportanceInfo();
+        final PoiAccountState accountState = this.poiFacade.findStateByAddress(address);
+
+        final RemoteLinks remoteLinks = accountState.getRemoteLinks();
+        AccountRemoteStatus accountRemoteStatus = AccountRemoteStatus.REMOTE;
+        if (remoteLinks.isEmpty()) {
+            accountRemoteStatus = AccountRemoteStatus.INACTIVE;
+        } else  if (remoteLinks.isHarvestingRemotely()) {
+            final boolean isActivated = ImportanceTransferTransaction.Mode.Activate.value() == remoteLinks.getCurrent().getMode();
+            final long heightDiff = height.subtract(remoteLinks.getCurrent().getEffectiveHeight());
+            final boolean withinOneDay = heightDiff < BlockChainConstants.ESTIMATED_BLOCKS_PER_DAY;
+            if (isActivated) {
+                accountRemoteStatus = withinOneDay ? AccountRemoteStatus.ACTIVATED : AccountRemoteStatus.ACTIVE;
+            } else {
+                accountRemoteStatus = withinOneDay ? AccountRemoteStatus.DEACTIVATED : AccountRemoteStatus.INACTIVE;
+            }
+        }
+
+		final AccountImportance ai = accountState.getImportanceInfo();
 		return new AccountInfo(
 				account.getAddress(),
 				account.getBalance(),
 				account.getForagedBlocks(),
+                accountRemoteStatus,
 				account.getLabel(),
 				!ai.isSet() ? 0.0 : ai.getImportance(ai.getHeight()));
 	}
