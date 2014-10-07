aedcb594a8f768e9327ac28c94a91a56f60ff9c0
nem <nem@bob.nem.ninja>
2014-10-07 07:11:47 -0400

    ...

--- nis/src/main/java/org/nem/nis/secret/AccountImportance.java
+++ nis/src/main/java/org/nem/nis/secret/AccountImportance.java
@@ -101,12 +101,14 @@ public class AccountImportance implements SerializableEntity {
 	 * @param importance The importance.
 	 */
 	public void setImportance(final BlockHeight blockHeight, final double importance) {
-		if (null == this.importanceHeight || 0 != this.importanceHeight.compareTo(blockHeight)) {
-			this.importanceHeight = blockHeight;
-			this.importance = importance;
-		} else if (0 == this.importanceHeight.compareTo(blockHeight)) {
+		// don't allow importance to be reset at the same height
+		// TODO 20141007 J-G: do you remember why we have this check?
+		if (null != this.importanceHeight && 0 == this.importanceHeight.compareTo(blockHeight)) {
 			throw new IllegalArgumentException("importance already set at given height");
 		}
+
+		this.importanceHeight = blockHeight;
+		this.importance = importance;
 	}
 
 	/**
