63adeb641b65646888a155ea42a6318453e893ef
nem <nem@bob.nem.ninja>
2014-10-13 16:03:02 +0200

    ...

---
+++ nis/src/main/java/org/nem/nis/controller/AccountInfoController.java
@@ -0,0 +1,59 @@
+package org.nem.nis.controller;
+
+import org.nem.core.model.*;
+import org.nem.core.model.ncc.*;
+import org.nem.core.model.primitive.BlockHeight;
+import org.nem.nis.controller.annotations.ClientApi;
+import org.nem.nis.controller.requests.AccountIdBuilder;
+import org.nem.nis.harvesting.*;
+import org.nem.nis.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * REST API for retrieving account related information
+ */
+@RestController
+public class AccountInfoController {
+    private final UnlockedAccounts unlockedAccounts;
+    private final BlockChainLastBlockLayer blockChainLastBlockLayer;
+    private final AccountInfoFactory accountInfoFactory;
+
+    @Autowired(required = true)
+    AccountInfoController(
+            final UnlockedAccounts unlockedAccounts,
+            final BlockChainLastBlockLayer blockChainLastBlockLayer,
+            final AccountInfoFactory accountInfoFactory) {
+        this.unlockedAccounts = unlockedAccounts;
+        this.blockChainLastBlockLayer = blockChainLastBlockLayer;
+        this.accountInfoFactory = accountInfoFactory;
+    }
+    /**
+     * Gets information about an account.
+     *
+     * @param builder The account id builder.
+     * @return The account information.
+     */
+    @RequestMapping(value = "/account/get", method = RequestMethod.GET)
+    @ClientApi
+    public AccountMetaDataPair accountGet(final AccountIdBuilder builder) {
+        // TODO 20141005 J-G: yea, i think it should be easy to move remote status from info to metadata
+        // > everything we need is here ;)
+        final Address address = builder.build().getAddress();
+        final Long height = this.blockChainLastBlockLayer.getLastBlockHeight();
+        final AccountInfo account = this.accountInfoFactory.createInfo(address, new BlockHeight(height));
+        final AccountMetaData metaData = new AccountMetaData(this.getAccountStatus(address));
+        return new AccountMetaDataPair(account, metaData);
+    }
+
+    @RequestMapping(value = "/account/status", method = RequestMethod.GET)
+    @ClientApi
+    public AccountMetaData accountStatus(final AccountIdBuilder builder) {
+        final Address address = builder.build().getAddress();
+        return new AccountMetaData(this.getAccountStatus(address));
+    }
+
+    private AccountStatus getAccountStatus(final Address address) {
+        return this.unlockedAccounts.isAccountUnlocked(address) ? AccountStatus.UNLOCKED : AccountStatus.LOCKED;
+    }
+}
---
+++ nis/src/test/java/org/nem/nis/controller/AccountInfoControllerTest.java
@@ -0,0 +1,98 @@
+package org.nem.nis.controller;
+
+import org.hamcrest.core.*;
+import org.junit.*;
+import org.mockito.Mockito;
+import org.nem.core.model.*;
+import org.nem.core.model.ncc.*;
+import org.nem.core.model.primitive.BlockHeight;
+import org.nem.core.test.Utils;
+import org.nem.nis.controller.requests.AccountIdBuilder;
+import org.nem.nis.harvesting.*;
+import org.nem.nis.service.*;
+
+public class AccountInfoControllerTest {
+
+    //region accountGet
+
+    @Test
+    public void accountGetDelegatesToAccountInfoFactory() {
+        // Arrange:
+        final Address address = Utils.generateRandomAddressWithPublicKey();
+        final AccountIdBuilder builder = new AccountIdBuilder();
+        builder.setAddress(address.getEncoded());
+        final AccountInfo accountInfo = Mockito.mock(AccountInfo.class);
+
+        final TestContext context = new TestContext();
+        Mockito.when(context.blockChainLastBlockLayer.getLastBlockHeight()).thenReturn(1L);
+        Mockito.when(context.accountInfoFactory.createInfo(address, BlockHeight.ONE)).thenReturn(accountInfo);
+
+        // Act:
+        final AccountMetaDataPair metaDataPair = context.controller.accountGet(builder);
+
+        // Assert:
+        Mockito.verify(context.accountInfoFactory, Mockito.times(1)).createInfo(address, BlockHeight.ONE);
+        Assert.assertThat(metaDataPair.getAccount(), IsSame.sameInstance(accountInfo));
+    }
+
+    @Test
+    public void accountGetDelegatesToUnlockedAccounts() {
+        // Arrange:
+        final Address address = Utils.generateRandomAddressWithPublicKey();
+        final AccountIdBuilder builder = new AccountIdBuilder();
+        builder.setAddress(address.getEncoded());
+
+        final TestContext context = new TestContext();
+        Mockito.when(context.blockChainLastBlockLayer.getLastBlockHeight()).thenReturn(1L);
+        Mockito.when(context.accountInfoFactory.createInfo(address, BlockHeight.ONE)).thenReturn(Mockito.mock(AccountInfo.class));
+        Mockito.when(context.unlockedAccounts.isAccountUnlocked(address)).thenReturn(true);
+
+        // Act:
+        final AccountMetaDataPair metaDataPair = context.controller.accountGet(builder);
+
+        // Assert:
+        Mockito.verify(context.unlockedAccounts, Mockito.times(1)).isAccountUnlocked(address);
+        Assert.assertThat(metaDataPair.getMetaData().getStatus(), IsEqual.equalTo(AccountStatus.UNLOCKED));
+    }
+
+    //endregion
+
+    //region accountStatus
+    @Test
+    public void accountStatusDelegatesToUnlockedAccounts() {
+        this.assertAccountStatusDelegatesToUnlockedAccounts(true, AccountStatus.UNLOCKED);
+        this.assertAccountStatusDelegatesToUnlockedAccounts(false, AccountStatus.LOCKED);
+    }
+
+    private void assertAccountStatusDelegatesToUnlockedAccounts(final boolean returned, final AccountStatus expectedStatus) {
+        // Arrange:
+        final Address address = Utils.generateRandomAddressWithPublicKey();
+        final AccountIdBuilder builder = new AccountIdBuilder();
+        builder.setAddress(address.getEncoded());
+
+        final TestContext context = new TestContext();
+        Mockito.when(context.unlockedAccounts.isAccountUnlocked(address)).thenReturn(returned);
+
+        // Act:
+        final AccountMetaData accountMetaData = context.controller.accountStatus(builder);
+
+        // Assert:
+        Mockito.verify(context.unlockedAccounts, Mockito.times(1)).isAccountUnlocked(address);
+        Assert.assertThat(accountMetaData.getStatus(), IsEqual.equalTo(expectedStatus));
+    }
+    //endregion
+
+    private static class TestContext {
+        private final AccountInfoController controller;
+        private final UnlockedAccounts unlockedAccounts = Mockito.mock(UnlockedAccounts.class);
+        private final AccountInfoFactory accountInfoFactory = Mockito.mock(AccountInfoFactory.class);
+        private final BlockChainLastBlockLayer blockChainLastBlockLayer = Mockito.mock(BlockChainLastBlockLayer.class);
+
+        public TestContext() {
+            this.controller = new AccountInfoController(
+                    this.unlockedAccounts,
+                    this.blockChainLastBlockLayer,
+                    this.accountInfoFactory);
+        }
+    }
+}
