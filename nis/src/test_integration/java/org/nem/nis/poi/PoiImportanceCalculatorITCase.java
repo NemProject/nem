1e4faa4bc5fb9aa50dbc90e3c60cdd15bf75c00e
nem <nem@bob.nem.ninja>
2014-10-24 22:40:22 +0900

    ...

--- nis/src/test_integration/java/org/nem/nis/poi/PoiImportanceCalculatorITCase.java
+++ nis/src/test_integration/java/org/nem/nis/poi/PoiImportanceCalculatorITCase.java
@@ -147,6 +147,7 @@ public class PoiImportanceCalculatorITCase {
 		// Arrange 1 vs many:
 		// Splitting of one account into many small accounts should have no influence on the importance distribution.
 		// TODO-CR 20140916 BR: test fails because weight on page rank was increased from 5% to 13.37%.
+        // TODO: 20141024 M-M: see if there are any effects of clustering here
 		final List<PoiAccountState> accounts = new ArrayList<>();
 		for (int i = 2; i < 10; i++) {
 			accounts.clear();
