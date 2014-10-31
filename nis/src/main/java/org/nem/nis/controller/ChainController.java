5f2f82032af75e487ebca9e57d46c8f7b88e4295
nem <nem@bob.nem.ninja>
2014-10-31 23:50:03 +0100

    ...

--- nis/src/main/java/org/nem/nis/controller/ChainController.java
+++ nis/src/main/java/org/nem/nis/controller/ChainController.java
@@ -11,11 +11,12 @@ import org.nem.nis.controller.viewmodels.AuthenticatedBlockHeightRequest;
 import org.nem.nis.dao.ReadOnlyBlockDao;
 import org.nem.nis.mappers.BlockMapper;
 import org.nem.nis.secret.BlockChainConstants;
-import org.nem.nis.service.*;
+import org.nem.nis.service.BlockChainLastBlockLayer;
 import org.nem.peer.node.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.logging.Logger;
 
 @RestController
@@ -67,26 +68,34 @@ public class ChainController {
 	@AuthenticatedApi
 	public AuthenticatedResponse<SerializableList<Block>> blocksAfter(
 			@RequestBody final AuthenticatedBlockHeightRequest request) {
-		// TODO 20141030 J-B: i guess this is one of the functions to optimize
 		// TODO: add tests for this action
-		org.nem.nis.dbmodel.Block dbBlock = this.blockDao.findByHeight(request.getEntity());
+		// TODO 20141030 BR: added one test
+		final long start = System.currentTimeMillis();
 		final SerializableList<Block> blockList = new SerializableList<>(BlockChainConstants.BLOCKS_LIMIT);
-		for (int i = 0; i < BlockChainConstants.BLOCKS_LIMIT; ++i) {
-			final Long curBlockId = dbBlock.getNextBlockId();
-			if (null == curBlockId) {
-				break;
+		final Collection<org.nem.nis.dbmodel.Block> dbBlockList = this.blockDao.getBlocksAfter(request.getEntity().getRaw(), BlockChainConstants.BLOCKS_LIMIT);
+		org.nem.nis.dbmodel.Block previousDbBlock = null;
+		for (org.nem.nis.dbmodel.Block dbBlock : dbBlockList) {
+			// There should be only one block per height. Just to be sure everything is fine we make this check.
+			if (null != previousDbBlock && !previousDbBlock.getNextBlockId().equals(dbBlock.getId()))  {
+				throw new RuntimeException("Corrupt block list returned from db.");
 			}
 
-			dbBlock = this.blockDao.findById(curBlockId);
+			previousDbBlock = dbBlock;
 			blockList.add(BlockMapper.toModel(dbBlock, this.accountLookup));
 		}
 
+		final long stop = System.currentTimeMillis();
+		LOGGER.info(String.format("Pulling %d blocks from db starting at height %d needed %dms.",
+				BlockChainConstants.BLOCKS_LIMIT,
+				request.getEntity().getRaw(),
+				stop - start));
 		final Node localNode = this.host.getNetwork().getLocalNode();
 		return new AuthenticatedResponse<>(
 				blockList,
 				localNode.getIdentity(),
 				request.getChallenge());
 	}
+
 
 	@RequestMapping(value = "/chain/hashes-from", method = RequestMethod.POST)
 	@P2PApi
