cdc2d5dcbd86c05cd16c11d1f715aa0c69724dd2
nem <nem@bob.nem.ninja>
2014-11-01 19:54:16 -0400

    ...

---
+++ nis/src/main/java/org/nem/nis/validators/NoSelfSignedTransactionsBlockValidator.java
@@ -0,0 +1,34 @@
+package org.nem.nis.validators;
+
+import org.nem.core.model.*;
+import org.nem.nis.poi.*;
+
+/**
+ * Validator that checks the block signer and rejects a block if any transactions are self-signed.
+ */
+public class NoSelfSignedTransactionsBlockValidator implements BlockValidator {
+	private final PoiFacade poiFacade;
+
+	/**
+	 * Creates a validator.
+	 *
+	 * @param poiFacade The poi facade.
+	 */
+	public NoSelfSignedTransactionsBlockValidator(final PoiFacade poiFacade) {
+		this.poiFacade = poiFacade;
+	}
+
+	@Override
+	public ValidationResult validate(final Block block) {
+		final Address harvesterAddress = block.getSigner().getAddress();
+		final PoiAccountState ownerState = this.poiFacade.findForwardedStateByAddress(harvesterAddress, block.getHeight());
+		final boolean isSelfSigned = block.getTransactions().stream().anyMatch(transaction -> {
+			final Address signerAddress = transaction.getSigner().getAddress();
+			return signerAddress.equals(harvesterAddress) || signerAddress.equals(ownerState.getAddress());
+		});
+
+		return isSelfSigned
+				? ValidationResult.FAILURE_ENTITY_UNUSABLE
+				: ValidationResult.SUCCESS;
+	}
+}
