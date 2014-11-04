d3a1b84d168a26de2be1ca2f27a656e20c788b88
nem <nem@bob.nem.ninja>
2014-11-03 19:07:40 -0500

    ...

--- nis/src/main/java/org/nem/nis/BlockChainServices.java
+++ nis/src/main/java/org/nem/nis/BlockChainServices.java
@@ -10,8 +10,6 @@ import org.nem.nis.service.BlockExecutor;
 import org.nem.nis.sync.*;
 import org.nem.nis.validators.*;
 import org.nem.nis.visitors.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.*;
 
@@ -21,14 +19,12 @@ import java.util.*;
  * Facade that hides the details of wiring up a number of BlockChain dependencies.
  * This class is intended to hide BlockExecutor and BlockChainValidator from the BlockChain COMPLETELY.
  */
-@Service
 public class BlockChainServices {
 	private final BlockDao blockDao;
 	private final BlockTransactionObserverFactory observerFactory;
 	private final BlockValidatorFactory blockValidatorFactory;
 	private final TransactionValidatorFactory transactionValidatorFactory;
 
-	@Autowired(required = true)
 	public BlockChainServices(
 			final BlockDao blockDao,
 			final BlockTransactionObserverFactory observerFactory,
