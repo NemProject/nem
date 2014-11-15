98e26ecd923bb05f6234cab52a1b74755723a2d6
nem <nem@bob.nem.ninja>
2014-11-15 13:45:54 +0900

    ...

--- nis/src/test_integration/java/org/nem/nis/poi/PoiImportanceCalculatorITCase.java
+++ nis/src/test_integration/java/org/nem/nis/poi/PoiImportanceCalculatorITCase.java
@@ -499,8 +499,6 @@ public class PoiImportanceCalculatorITCase {
 		LOGGER.info("Testing memory usage of the poi calculation");
 
 		// Arrange:
-        UtilUnsafe sizeFetcher = new UtilUnsafe();
-
 		System.out.println("Setting up accounts.");
 		final int numAccounts = 50000;
 		final List<PoiAccountState> accounts = new ArrayList<>();
@@ -632,52 +630,4 @@ public class PoiImportanceCalculatorITCase {
 
 		return importancesVector;
 	}
-
-
-    class UtilUnsafe {
-        private final int NR_BITS = Integer.valueOf(System.getProperty("sun.arch.data.model"));
-        private final int BYTE = 8;
-        private final int WORD = NR_BITS/BYTE;
-        private final int MIN_SIZE = 16;
-
-        int sizeOf(Class src){
-            //
-            // Get the instance fields of src class
-            //
-            List<Field> instanceFields = new LinkedList<Field>();
-            do{
-                if(src == Object.class) return MIN_SIZE;
-                for (Field f : src.getDeclaredFields()) {
-                    if((f.getModifiers() & Modifier.STATIC) == 0){
-                        instanceFields.add(f);
-                    }
-                }
-                src = src.getSuperclass();
-            }while(instanceFields.isEmpty());
-            //
-            // Get the field with the maximum offset
-            //
-            long maxOffset = 0;
-            for (Field f : instanceFields) {
-                long offset = this.UNSAFE.objectFieldOffset(f);
-                if(offset > maxOffset) maxOffset = offset;
-            }
-            return  (((int)maxOffset/WORD) + 1)*WORD;
-        }
-
-        public final sun.misc.Unsafe UNSAFE;
-
-        UtilUnsafe() {
-            Object theUnsafe = null;
-            Exception exception = null;
-            try {
-                Class<?> uc = Class.forName("sun.misc.Unsafe");
-                Field f = uc.getDeclaredField("theUnsafe");
-                f.setAccessible(true);
-                theUnsafe = f.get(uc);
-            } catch (Exception e) { exception = e; }
-            UNSAFE = (sun.misc.Unsafe) theUnsafe;
-            if (UNSAFE == null) throw new Error("Could not obtain access to sun.misc.Unsafe", exception);
-        }
-    }
 }
