e4f90c1d4fbd75818209378e1e2cc9c704ddd356
nem <nem@bob.nem.ninja>
2014-11-16 13:29:37 -0500

    ...

--- nis/src/test/java/org/nem/nis/validators/AggregateBlockValidatorBuilderTest.java
+++ nis/src/test/java/org/nem/nis/validators/AggregateBlockValidatorBuilderTest.java
@@ -1,133 +1,45 @@
 package org.nem.nis.validators;
 
-import org.hamcrest.core.IsEqual;
-import org.junit.*;
 import org.mockito.Mockito;
+import org.mockito.verification.VerificationMode;
 import org.nem.core.model.*;
 
-public class AggregateBlockValidatorBuilderTest {
+public class AggregateBlockValidatorBuilderTest extends AggregateValidatorBuilderTest<AggregateBlockValidatorBuilder, BlockValidator, Block> {
 
-	@Test
-	public void canAddSingleValidator() {
-		// Arrange:
-		final Block block = Mockito.mock(Block.class);
-		final BlockValidator validator = createValidator(ValidationResult.FAILURE_FUTURE_DEADLINE);
-		final AggregateBlockValidatorBuilder builder = new AggregateBlockValidatorBuilder();
-
-		// Act:
-		builder.add(validator);
-		final BlockValidator aggregate = builder.build();
-		final ValidationResult result = aggregate.validate(block);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.FAILURE_FUTURE_DEADLINE));
-		Mockito.verify(validator, Mockito.only()).validate(block);
+	@Override
+	public AggregateBlockValidatorBuilder createBuilder() {
+		return new AggregateBlockValidatorBuilder();
 	}
 
-	@Test
-	public void canAddMultipleValidators() {
-		// Arrange:
-		final ThreeSubValidatorTestContext context = new ThreeSubValidatorTestContext(
-				ValidationResult.SUCCESS,
-				ValidationResult.SUCCESS,
-				ValidationResult.SUCCESS);
-
-		// Act:
-		final BlockValidator aggregate = context.builder.build();
-		final ValidationResult result = aggregate.validate(context.block);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.SUCCESS));
-		context.assertAllValidatorsCalledOnce();
-	}
-
-	@Test
-	public void validationShortCircuitsOnFirstSubValidatorFailure() {
-		// Arrange:
-		final ThreeSubValidatorTestContext context = new ThreeSubValidatorTestContext(
-				ValidationResult.SUCCESS,
-				ValidationResult.FAILURE_CHAIN_INVALID,
-				ValidationResult.SUCCESS);
-
-		// Act:
-		final BlockValidator aggregate = context.builder.build();
-		final ValidationResult result = aggregate.validate(context.block);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.FAILURE_CHAIN_INVALID));
-		context.assertOnlyFirstTwoValidatorsCalledOnce();
-	}
-
-	@Test
-	public void validationDoesNotShortCircuitOnFirstSubValidatorNeutralResult() {
-		// Arrange:
-		final ThreeSubValidatorTestContext context = new ThreeSubValidatorTestContext(
-				ValidationResult.SUCCESS,
-				ValidationResult.NEUTRAL,
-				ValidationResult.SUCCESS);
-
-		// Act:
-		final BlockValidator aggregate = context.builder.build();
-		final ValidationResult result = aggregate.validate(context.block);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.NEUTRAL));
-		context.assertAllValidatorsCalledOnce();
-	}
-
-	@Test
-	public void validationFailureHasHigherPrecedenceThanNeutralResult() {
-		// Arrange:
-		final ThreeSubValidatorTestContext context = new ThreeSubValidatorTestContext(
-				ValidationResult.SUCCESS,
-				ValidationResult.NEUTRAL,
-				ValidationResult.FAILURE_CHAIN_INVALID);
-
-		// Act:
-		final BlockValidator aggregate = context.builder.build();
-		final ValidationResult result = aggregate.validate(context.block);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.FAILURE_CHAIN_INVALID));
-		context.assertAllValidatorsCalledOnce();
-	}
-
-	private static class ThreeSubValidatorTestContext {
-		private final Block block = Mockito.mock(Block.class);
-		private final BlockValidator validator1;
-		private final BlockValidator validator2;
-		private final BlockValidator validator3;
-		private final AggregateBlockValidatorBuilder builder = new AggregateBlockValidatorBuilder();
-
-		private ThreeSubValidatorTestContext(
-				final ValidationResult result1,
-				final ValidationResult result2,
-				final ValidationResult result3) {
-			this.validator1 = createValidator(result1);
-			this.validator2 = createValidator(result2);
-			this.validator3 = createValidator(result3);
-
-			this.builder.add(this.validator1);
-			this.builder.add(this.validator2);
-			this.builder.add(this.validator3);
-		}
-
-		private void assertOnlyFirstTwoValidatorsCalledOnce() {
-			Mockito.verify(this.validator1, Mockito.only()).validate(this.block);
-			Mockito.verify(this.validator2, Mockito.only()).validate(this.block);
-			Mockito.verify(this.validator3, Mockito.never()).validate(this.block);
-		}
-
-		private void assertAllValidatorsCalledOnce() {
-			Mockito.verify(this.validator1, Mockito.only()).validate(this.block);
-			Mockito.verify(this.validator2, Mockito.only()).validate(this.block);
-			Mockito.verify(this.validator3, Mockito.only()).validate(this.block);
-		}
-	}
-
-	private static BlockValidator createValidator(final ValidationResult result) {
+	@Override
+	public BlockValidator createValidator(final ValidationResult result) {
 		final BlockValidator validator = Mockito.mock(BlockValidator.class);
 		Mockito.when(validator.validate(Mockito.any())).thenReturn(result);
 		return validator;
+	}
+
+	@Override
+	public Block createParam() {
+		return Mockito.mock(Block.class);
+	}
+
+	@Override
+	public void add(final AggregateBlockValidatorBuilder builder, final BlockValidator validator) {
+		builder.add(validator);
+	}
+
+	@Override
+	public BlockValidator build(final AggregateBlockValidatorBuilder builder) {
+		return builder.build();
+	}
+
+	@Override
+	public ValidationResult validate(final BlockValidator validator, final Block param) {
+		return validator.validate(param);
+	}
+
+	@Override
+	public void verifyValidate(final BlockValidator validator, final Block param, final VerificationMode verificationMode) {
+		Mockito.verify(validator, verificationMode).validate(param);
 	}
 }
