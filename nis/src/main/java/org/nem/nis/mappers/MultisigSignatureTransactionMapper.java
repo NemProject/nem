c6d5a47f1e986f19ca7279f0058d5f0bebcfa3cc
nem <nem@bob.nem.ninja>
2014-12-05 23:06:34 +0100

    ...

---
+++ nis/src/main/java/org/nem/nis/mappers/MultisigSignatureTransactionMapper.java
@@ -0,0 +1,36 @@
+package org.nem.nis.mappers;
+
+import org.nem.core.crypto.Hash;
+import org.nem.core.crypto.Signature;
+import org.nem.core.model.*;
+import org.nem.core.serialization.AccountLookup;
+import org.nem.core.time.TimeInstant;
+import org.nem.nis.dbmodel.MultisigSignature;
+
+public class MultisigSignatureTransactionMapper {
+	static MultisigSignature toDbModel(org.nem.nis.dbmodel.Account sender, MultisigSignatureTransaction model) {
+		final MultisigSignature dbModel = new MultisigSignature();
+
+		final Hash txHash = HashUtils.calculateHash(model);
+		dbModel.setTransferHash(txHash);
+		dbModel.setVersion(model.getVersion());
+		dbModel.setFee(model.getFee().getNumMicroNem());
+		dbModel.setTimeStamp(model.getTimeStamp().getRawTime());
+		dbModel.setDeadline(model.getDeadline().getRawTime());
+		dbModel.setSender(sender);
+		dbModel.setSenderProof(model.getSignature().getBytes());
+		return dbModel;
+	}
+
+	static MultisigSignatureTransaction toModel(final MultisigSignature multisigSignature, final AccountLookup accountLookup, final Transaction otherTransaction) {
+		final Address cosignerAddress = AccountToAddressMapper.toAddress(multisigSignature.getSender());
+		final Account cosigner = accountLookup.findByAddress(cosignerAddress);
+
+		return new MultisigSignatureTransaction(
+				new TimeInstant(multisigSignature.getTimeStamp()),
+				cosigner,
+				HashUtils.calculateHash(otherTransaction),
+				new Signature(new byte[64])
+		);
+	}
+}
--- nis/src/main/java/org/nem/nis/mappers/MultisigTransactionMapper.java
+++ nis/src/main/java/org/nem/nis/mappers/MultisigTransactionMapper.java
@@ -1,12 +1,12 @@
 package org.nem.nis.mappers;
 
-import org.nem.core.crypto.Hash;
 import org.nem.core.crypto.Signature;
 import org.nem.core.model.*;
+import org.nem.core.model.Account;
 import org.nem.core.model.primitive.Amount;
 import org.nem.core.serialization.AccountLookup;
 import org.nem.core.time.TimeInstant;
-import org.nem.nis.dbmodel.MultisigSignature;
+import org.nem.nis.dbmodel.*;
 import org.nem.nis.dbmodel.MultisigTransaction;
 
 import java.util.HashSet;
@@ -29,27 +29,13 @@ public class MultisigTransactionMapper {
 		final MultisigTransaction multisigTransaction = new MultisigTransaction();
 		AbstractTransferMapper.toDbModel(transaction, sender, blockIndex, orderIndex, multisigTransaction);
 
-		// begin MultisigSignature.toDbModel()
-		// extract to separate mapper
 		final Set<MultisigSignature> multisigSignatures = new HashSet<>();
 		for (final MultisigSignatureTransaction model : transaction.getCosignerSignatures()) {
-			final MultisigSignature dbModel = new MultisigSignature();
-
-			final Hash txHash = HashUtils.calculateHash(model);
-			dbModel.setTransferHash(txHash);
-			dbModel.setVersion(model.getVersion());
-			dbModel.setFee(model.getFee().getNumMicroNem());
-			dbModel.setTimeStamp(model.getTimeStamp().getRawTime());
-			dbModel.setDeadline(model.getDeadline().getRawTime());
-			dbModel.setSender(sender);
-			dbModel.setSenderProof(model.getSignature().getBytes());
-
+			final MultisigSignature dbModel = MultisigSignatureTransactionMapper.toDbModel(sender, model);
 			dbModel.setMultisigTransaction(multisigTransaction);
-
 			multisigSignatures.add(dbModel);
 		}
 		multisigTransaction.setMultisigSignatures(multisigSignatures);
-		// end MultisigSignature.toDbModel()
 
 		// proper multisigTransaction.set*Transfer(); are called from BlockMapper
 
@@ -80,21 +66,9 @@ public class MultisigTransactionMapper {
 		transaction.setDeadline(new TimeInstant(dbTransfer.getDeadline()));
 		transaction.setSignature(new Signature(dbTransfer.getSenderProof()));
 
-		// begin MultisigSignature.toModel()
 		for (final MultisigSignature multisigSignature : dbTransfer.getMultisigSignatures()) {
-			final Address cosignerAddress = AccountToAddressMapper.toAddress(multisigSignature.getSender());
-			final Account cosigner = accountLookup.findByAddress(cosignerAddress);
-
-			transaction.addSignature(
-					new MultisigSignatureTransaction(
-							new TimeInstant(dbTransfer.getTimeStamp()),
-							cosigner,
-							HashUtils.calculateHash(otherTransaction),
-							new Signature(new byte[64])
-					)
-			);
+			transaction.addSignature(MultisigSignatureTransactionMapper.toModel(multisigSignature, accountLookup, otherTransaction));
 		}
-		// end MultisigSignature.toModel()
 
 		return transaction;
 	}
