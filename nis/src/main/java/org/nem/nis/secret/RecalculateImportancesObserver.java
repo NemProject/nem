eab4551187ea3426e43b3402db67e33312e94bf4
nem <nem@bob.nem.ninja>
2014-12-15 17:42:27 -0500

    ...

--- nis/src/main/java/org/nem/nis/secret/RecalculateImportancesObserver.java
+++ nis/src/main/java/org/nem/nis/secret/RecalculateImportancesObserver.java
@@ -9,12 +9,6 @@ import org.nem.nis.poi.GroupedHeight;
 /**
  * An observer that recalculates POI importances.
  */
-// TODO 20141213 G-J: introduction of this, causes, that poi is recalculated also during loading of blockchain
-// earlier recalculation was done after all the blocks have been loaded,
-// (for 70k blocks that is >180 recalculations)
-// TODO 20131214 G-J: ok, so it's not a fraction, for 79k blocks that we currently have:
-// poi recalculation took 190s (3 minutes), while whole load took 373s 6m13s...
-// for some reason webstart version is much slower...
 public class RecalculateImportancesObserver implements BlockTransactionObserver {
 	private final NisCache nisCache;
 
