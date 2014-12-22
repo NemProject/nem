fa3ca473457f68462cf07d74433c07c3ccf9fe2c
nem <nem@bob.nem.ninja>
2014-12-22 22:02:47 +0100

    ...

--- nis/src/main/java/org/nem/nis/harvesting/UnconfirmedTransactions.java
+++ nis/src/main/java/org/nem/nis/harvesting/UnconfirmedTransactions.java
@@ -6,7 +6,8 @@ import org.nem.core.model.*;
 import org.nem.core.model.observers.*;
 import org.nem.core.model.primitive.*;
 import org.nem.core.time.*;
-import org.nem.nis.cache.*;
+import org.nem.nis.cache.ReadOnlyNisCache;
+import org.nem.nis.secret.UnconfirmedBalancesObserver;
 import org.nem.nis.validators.*;
 
 import java.util.*;
@@ -259,8 +260,10 @@ public class UnconfirmedTransactions {
 				for (final Transaction transaction : getReverseTransactions(block)) {
 					this.remove(transaction);
 				}
-			} catch (final Exception e) { // TODO 20141218 J-B: can we at least catch a more specific exception? we can't really recover from every possible failure
-				LOGGER.severe("exception during removal of unconfirmed transactions, rebuilding cache");
+			} catch (final IllegalArgumentException e) {
+				// TODO 20141218 J-B: can we at least catch a more specific exception? we can't really recover from every possible failure
+				// TODO 20141222 BR -> J: I have only seen illegal argument exceptions due to negative amounts so I guess it is enough to catch them.
+				LOGGER.severe("illegal argument exception during removal of unconfirmed transactions, rebuilding cache");
 				this.rebuildCache(this.getAll());
 				return;
 			}
@@ -385,7 +388,9 @@ public class UnconfirmedTransactions {
 		this.transactions.clear();
 		this.pendingTransactions.clear();
 		this.unconfirmedBalances.clearCache();
-		this.addNewBatch(transactions);
+
+		// don't add as batch since this would fail fast and we want to keep as many transactions as possible.
+		transactions.stream().forEach(t -> this.addNew(t));
 	}
 
 	/**
@@ -448,78 +453,6 @@ public class UnconfirmedTransactions {
 							.filter(tx -> tx.getDeadline().compareTo(blockTime) >= 0)
 							.collect(Collectors.toList()),
 					BalanceValidationOptions.ValidateAgainstConfirmedBalance);
-		}
-	}
-
-	// TODO 20141218 J-B: changes look pretty good, nice job; i have a few comments:
-	// > it might make sense to pull UnconfirmedBalancesObserver out into its own class so we can test it more directly
-	// > should definitely add tests for the cases that trigger the cache rebuild (since this is presumably the source of our bugs)
-	private static class UnconfirmedBalancesObserver implements TransferObserver {
-		private final ReadOnlyAccountStateCache accountStateCache;
-		private final Map<Account, Amount> creditedAmounts = new ConcurrentHashMap<>();
-		private final Map<Account, Amount> debitedAmounts = new ConcurrentHashMap<>();
-
-		public UnconfirmedBalancesObserver(final ReadOnlyAccountStateCache accountStateCache) {
-			this.accountStateCache = accountStateCache;
-		}
-
-		public Amount get(final Account account) {
-			return this.getBalance(account).add(this.getCreditedAmount(account)).subtract(this.getDebitedAmount(account));
-		}
-
-		@Override
-		public void notifyTransfer(final Account sender, final Account recipient, final Amount amount) {
-			this.notifyDebit(sender, amount);
-			this.notifyCredit(recipient, amount);
-		}
-
-		@Override
-		public void notifyCredit(final Account account, final Amount amount) {
-			this.addToCache(account);
-			final Amount newCreditedAmount = this.getCreditedAmount(account).add(amount);
-			this.creditedAmounts.replace(account, newCreditedAmount);
-		}
-
-		@Override
-		public void notifyDebit(final Account account, final Amount amount) {
-			this.addToCache(account);
-			final Amount newDebitedAmount = this.getDebitedAmount(account).add(amount);
-			// should not be necessary but do it anyway as check
-			this.getBalance(account).add(this.getCreditedAmount(account)).subtract(newDebitedAmount);
-			this.debitedAmounts.replace(account, newDebitedAmount);
-		}
-
-		private void addToCache(final Account account) {
-			// it's ok to put reference here, thanks to Account being non-mutable
-			this.creditedAmounts.putIfAbsent(account, Amount.ZERO);
-			this.debitedAmounts.putIfAbsent(account, Amount.ZERO);
-		}
-
-		private void clearCache() {
-			this.creditedAmounts.clear();
-			this.debitedAmounts.clear();
-		}
-
-		private boolean unconfirmedBalancesAreValid() {
-			for (final Account account : this.creditedAmounts.keySet()) {
-				if (this.getBalance(account).add(this.getCreditedAmount(account)).compareTo(this.getDebitedAmount(account)) < 0) {
-					return false;
-				}
-			}
-
-			return true;
-		}
-
-		private Amount getBalance(final Account account) {
-			return this.accountStateCache.findStateByAddress(account.getAddress()).getAccountInfo().getBalance();
-		}
-
-		private Amount getCreditedAmount(final Account account) {
-			return this.creditedAmounts.getOrDefault(account, Amount.ZERO);
-		}
-
-		private Amount getDebitedAmount(final Account account) {
-			return this.debitedAmounts.getOrDefault(account, Amount.ZERO);
 		}
 	}
 }
