37d9e997daa05a5dddbce234b5933b168b1b755e
nem <nem@bob.nem.ninja>
2015-01-03 13:25:45 -0500

    ...

--- nis/src/main/java/org/nem/nis/mappers/MultisigSignatureModelToDbModelMapping.java
+++ nis/src/main/java/org/nem/nis/mappers/MultisigSignatureModelToDbModelMapping.java
@@ -1,14 +1,12 @@
 package org.nem.nis.mappers;
 
-import org.nem.core.crypto.Hash;
 import org.nem.core.model.*;
 import org.nem.nis.dbmodel.*;
 
 /**
  * A mapping that is able to map a db multisig signature to a model multisig signature.
  */
-public class MultisigSignatureModelToDbModelMapping implements IMapping<MultisigSignatureTransaction, MultisigSignature> {
-	private final IMapper mapper;
+public class MultisigSignatureModelToDbModelMapping extends AbstractTransferModelToDbModelMapping<MultisigSignatureTransaction, MultisigSignature> {
 
 	/**
 	 * Creates a new mapping.
@@ -16,24 +14,12 @@ public class MultisigSignatureModelToDbModelMapping implements IMapping<Multisig
 	 * @param mapper The mapper.
 	 */
 	public MultisigSignatureModelToDbModelMapping(final IMapper mapper) {
-		this.mapper = mapper;
+		super(mapper);
 	}
 
 	@Override
-	public MultisigSignature map(final MultisigSignatureTransaction source) {
+	protected MultisigSignature mapImpl(final MultisigSignatureTransaction source) {
 		final MultisigSignature dbSignature = new MultisigSignature();
-		final org.nem.nis.dbmodel.Account sender = this.mapper.map(source.getSigner(), org.nem.nis.dbmodel.Account.class);
-
-		final Hash txHash = HashUtils.calculateHash(source);
-		dbSignature.setTransferHash(txHash);
-		dbSignature.setVersion(source.getVersion());
-		dbSignature.setFee(source.getFee().getNumMicroNem());
-		dbSignature.setTimeStamp(source.getTimeStamp().getRawTime());
-		dbSignature.setDeadline(source.getDeadline().getRawTime());
-		dbSignature.setSender(sender);
-		dbSignature.setSenderProof(source.getSignature().getBytes());
-
-		// TODO 20141203 J-G: why do we need this?
 		dbSignature.setMultisigTransaction(null);
 		return dbSignature;
 	}
--- nis/src/test/java/org/nem/nis/mappers/MultisigSignatureModelToDbModelMappingTest.java
+++ nis/src/test/java/org/nem/nis/mappers/MultisigSignatureModelToDbModelMappingTest.java
@@ -6,15 +6,14 @@ import org.mockito.Mockito;
 import org.nem.core.crypto.*;
 import org.nem.core.model.*;
 import org.nem.core.model.Account;
-import org.nem.core.model.primitive.Amount;
 import org.nem.core.test.Utils;
 import org.nem.core.time.TimeInstant;
 import org.nem.nis.dbmodel.*;
 
-public class MultisigSignatureModelToDbModelMappingTest {
+public class MultisigSignatureModelToDbModelMappingTest extends AbstractTransferModelToDbModelMappingTest<MultisigSignatureTransaction, MultisigSignature> {
 
 	@Test
-	public void signatureCanBeMappedToDbModel() {
+	public void otherTransactionHashCanBeMappedToDbModel() {
 		// Arrange:
 		final TestContext context = new TestContext();
 		final MultisigSignatureTransaction signature = context.createModel();
@@ -23,45 +22,35 @@ public class MultisigSignatureModelToDbModelMappingTest {
 		final MultisigSignature dbModel = context.mapping.map(signature);
 
 		// Assert:
-		context.assertDbModel(dbModel, HashUtils.calculateHash(signature));
+		context.assertDbModel(dbModel, signature);
+	}
+
+	@Override
+	protected MultisigSignatureTransaction createModel(final TimeInstant timeStamp, final Account sender) {
+		return new MultisigSignatureTransaction(timeStamp, sender, Utils.generateRandomHash());
+	}
+
+	@Override
+	protected MultisigSignature map(final MultisigSignatureTransaction model, final IMapper mapper) {
+		return new MultisigSignatureModelToDbModelMapping(mapper).map(model);
 	}
 
 	private static class TestContext {
 		private final IMapper mapper = Mockito.mock(IMapper.class);
-		private final org.nem.nis.dbmodel.Account dbSender = Mockito.mock(org.nem.nis.dbmodel.Account.class);
-		private final Account sender = Utils.generateRandomAccount();
-		private final Signature signature = Utils.generateRandomSignature();
 		private final Hash otherTransactionHash = Utils.generateRandomHash();
 		private final MultisigSignatureModelToDbModelMapping mapping = new MultisigSignatureModelToDbModelMapping(this.mapper);
 
-		public TestContext() {
-			Mockito.when(this.mapper.map(this.sender, org.nem.nis.dbmodel.Account.class)).thenReturn(this.dbSender);
-		}
-
 		public MultisigSignatureTransaction createModel() {
-			final MultisigSignatureTransaction transfer = new MultisigSignatureTransaction(
-					new TimeInstant(4444),
-					this.sender,
+			return new MultisigSignatureTransaction(
+					TimeInstant.ZERO,
+					Utils.generateRandomAccount(),
 					this.otherTransactionHash);
-
-			transfer.setFee(Amount.fromMicroNem(98765432L));
-			transfer.setDeadline(new TimeInstant(123));
-			transfer.setSignature(this.signature);
-			return transfer;
 		}
 
-		public void assertDbModel(final MultisigSignature dbModel, final Hash expectedHash) {
-			Assert.assertThat(dbModel.getVersion(), IsEqual.equalTo(1));
+		public void assertDbModel(final MultisigSignature dbModel, final MultisigSignatureTransaction model) {
+			Assert.assertThat(dbModel.getMultisigTransaction(), IsNull.notNullValue()); // TODO clearly failing
 
-			Assert.assertThat(dbModel.getTimeStamp(), IsEqual.equalTo(4444));
-			Assert.assertThat(dbModel.getSender(), IsEqual.equalTo(this.dbSender));
-			Assert.assertThat(dbModel.getMultisigTransaction(), IsEqual.equalTo(null)); // TODO clearly wrong
-
-			Assert.assertThat(dbModel.getFee(), IsEqual.equalTo(98765432L));
-			Assert.assertThat(dbModel.getDeadline(), IsEqual.equalTo(123));
-			Assert.assertThat(dbModel.getSenderProof(), IsEqual.equalTo(this.signature.getBytes()));
-
-			Assert.assertThat(dbModel.getTransferHash(), IsEqual.equalTo(expectedHash));
+			Assert.assertThat(dbModel.getTransferHash(), IsEqual.equalTo(HashUtils.calculateHash(model)));
 		}
 	}
 }
