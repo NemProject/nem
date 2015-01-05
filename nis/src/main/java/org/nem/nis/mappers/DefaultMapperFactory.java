c36b0f479667eb9505264efd303804afe3080b29
nem <nem@bob.nem.ninja>
2015-01-04 19:15:13 -0500

    ...

--- nis/src/main/java/org/nem/nis/mappers/DefaultMapperFactory.java
+++ nis/src/main/java/org/nem/nis/mappers/DefaultMapperFactory.java
@@ -1,24 +1,76 @@
 package org.nem.nis.mappers;
 
 import org.nem.core.model.*;
-import org.nem.core.serialization.AccountLookup;
-import org.nem.nis.dbmodel.MultisigSignature;
+import org.nem.core.model.Account;
+import org.nem.core.model.Block;
+import org.nem.core.serialization.*;
+import org.nem.nis.dbmodel.*;
+
+import java.util.*;
+import java.util.function.*;
 
 /**
  * Factory for creating a mapper.
  */
 public class DefaultMapperFactory implements MapperFactory {
 
-	@Override
+	private static class Entry<TDbModel, TModel> {
+		public final Class<TDbModel> dbModelClass;
+		public final Class<TModel> modelClass;
+
+		private final BiFunction<AccountDaoLookup, IMapper, IMapping<TModel, TDbModel>> createModelToDbModelMapper;
+		private final BiFunction<AccountLookup, IMapper, IMapping<TDbModel, TModel>> createDbModelToModelMapper;
+
+		private Entry(
+				final BiFunction<AccountDaoLookup, IMapper, IMapping<TModel, TDbModel>> createModelToDbModelMapper,
+				final BiFunction<AccountLookup, IMapper, IMapping<TDbModel, TModel>> createDbModelToModelMapper,
+				final Class<TDbModel> dbModelClass,
+				final Class<TModel> modelClass) {
+			this.createModelToDbModelMapper = createModelToDbModelMapper;
+			this.createDbModelToModelMapper = createDbModelToModelMapper;
+			this.dbModelClass = dbModelClass;
+			this.modelClass = modelClass;
+		}
+
+		public void addModelToDbModelMappers(final AccountDaoLookup accountDaoLookup, final MappingRepository repository) {
+			repository.addMapping(this.modelClass, this.dbModelClass, this.createModelToDbModelMapper.apply(accountDaoLookup, repository));
+		}
+
+		public void addDbModelToModelMappers(final AccountLookup accountLookup, final MappingRepository repository) {
+			repository.addMapping(this.dbModelClass, this.modelClass, this.createDbModelToModelMapper.apply(accountLookup, repository));
+		}
+	}
+
+	private static final List<Entry<?, ?>> entries = new ArrayList<Entry<?, ?>>() {
+		{
+			this.add(new Entry<>(
+					(lookup, mapper) -> new AccountModelToDbModelMapping(lookup),
+					(lookup, mapper) -> new AccountDbModelToModelMapping(lookup),
+					org.nem.nis.dbmodel.Account.class,
+					Account.class));
+			this.add(new Entry<>(
+					(lookup, mapper) -> new BlockModelToDbModelMapping(mapper),
+					(lookup, mapper) -> new BlockDbModelToModelMapping(mapper, lookup),
+					org.nem.nis.dbmodel.Block.class,
+					Block.class));
+			this.add(new Entry<>(
+					(lookup, mapper) -> new MultisigSignatureModelToDbModelMapping(mapper),
+					(lookup, mapper) -> new MultisigSignatureDbModelToModelMapping(mapper),
+					MultisigSignature.class,
+					MultisigSignatureTransaction.class));
+		}
+	};
+
 	public MappingRepository createModelToDbModelMapper(final AccountDaoLookup accountDaoLookup) {
 		final MappingRepository mappingRepository = new MappingRepository();
 		for (final TransactionRegistry.Entry<?, ?> entry : TransactionRegistry.iterate()) {
 			entry.addModelToDbModelMappers(mappingRepository);
 		}
 
-		mappingRepository.addMapping(Account.class, org.nem.nis.dbmodel.Account.class, new AccountModelToDbModelMapping(accountDaoLookup));
-		mappingRepository.addMapping(Block.class, org.nem.nis.dbmodel.Block.class, new BlockModelToDbModelMapping(mappingRepository));
-		mappingRepository.addMapping(MultisigSignatureTransaction.class, MultisigSignature.class, new MultisigSignatureModelToDbModelMapping(mappingRepository));
+		for (final Entry<?, ?> entry : entries) {
+			entry.addModelToDbModelMappers(accountDaoLookup, mappingRepository);
+		}
+
 		return mappingRepository;
 	}
 
@@ -29,9 +81,10 @@ public class DefaultMapperFactory implements MapperFactory {
 			entry.addDbModelToModelMappers(mappingRepository);
 		}
 
-		mappingRepository.addMapping(org.nem.nis.dbmodel.Account.class, Account.class, new AccountDbModelToModelMapping(accountLookup));
-		mappingRepository.addMapping(org.nem.nis.dbmodel.Block.class, Block.class, new BlockDbModelToModelMapping(mappingRepository, accountLookup));
-		mappingRepository.addMapping(MultisigSignature.class, MultisigSignatureTransaction.class, new MultisigSignatureDbModelToModelMapping(mappingRepository));
+		for (final Entry<?, ?> entry : entries) {
+			entry.addDbModelToModelMappers(accountLookup, mappingRepository);
+		}
+
 		return mappingRepository;
 	}
 }
