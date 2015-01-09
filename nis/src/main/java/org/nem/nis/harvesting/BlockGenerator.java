1a71c69474fefaf1f2f3fbd3a3b3cd5935c1c15f
nem <nem@bob.nem.ninja>
2015-01-09 19:58:42 +0100

    ...

--- nis/src/main/java/org/nem/nis/harvesting/BlockGenerator.java
+++ nis/src/main/java/org/nem/nis/harvesting/BlockGenerator.java
@@ -100,6 +100,10 @@ public class BlockGenerator {
 		final BlockDifficulty difficulty = this.calculateDifficulty(blockScorer, lastBlock.getHeight());
 
 		// it's the remote harvester that generates a block NOT owner, we won't have owner's key here!
+		// TODO 20150109 G-*: minor micro-optimization:
+		// > this causes the call Block.setPrevious, which in turn calculates hash of lastBlock
+		// > (which causes serialization of the lastBlock every time)
+		// > I think we could avoid that
 		final Block newBlock = new Block(harvesterAccount, lastBlock, blockTime);
 		newBlock.setLessor(ownerAccount);
 		newBlock.setDifficulty(difficulty);
