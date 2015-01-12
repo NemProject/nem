cd938703644a3fc2cdbd58726c4ed1d6b255f57d
nem <nem@bob.nem.ninja>
2015-01-12 18:38:57 -0500

    ...

--- nis/src/main/java/org/nem/nis/controller/requests/AccountId.java
+++ nis/src/main/java/org/nem/nis/controller/requests/AccountId.java
@@ -5,6 +5,13 @@ import org.nem.core.serialization.*;
 
 /**
  * View model that represents an account id information.
+ * TODO 20150112 J-B: i don't see where you need to support SerializableEntity
+ * > afaikt, the only requirement is to be able to deserialize the request
+ * > if so, i would drop 'implements SerializableEntity'
+ * > the reason being that this is the root of a request hierarchy and it would be weird
+ * > and possibly error prone if all of the classes were serializable but only partially
+ * > ok, i see you ran into the generic bounds issue on the SerializableList<>; let's leave this
+ * > for now, but it's probably something we should fix later
  */
 public class AccountId implements SerializableEntity {
 	private final Address address;
@@ -48,7 +55,7 @@ public class AccountId implements SerializableEntity {
 		return this.address;
 	}
 
-	@Override
+	//@Override
 	public void serialize(final Serializer serializer) {
 		Address.writeTo(serializer, "account", this.address);
 	}
