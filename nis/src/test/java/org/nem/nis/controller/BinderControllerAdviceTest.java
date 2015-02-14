4daf7be74e98cb606d0d450a0743376cd2361f4e
nem <nem@bob.nem.ninja>
2015-02-14 13:23:10 -0500

    ...

--- nis/src/test/java/org/nem/nis/controller/BinderControllerAdviceTest.java
+++ nis/src/test/java/org/nem/nis/controller/BinderControllerAdviceTest.java
@@ -1,10 +1,14 @@
 package org.nem.nis.controller;
 
 import org.junit.*;
+import org.mockito.Mockito;
 import org.nem.core.test.IsEquivalent;
+import org.nem.deploy.NisConfiguration;
+import org.nem.nis.cache.ReadOnlyAccountStateCache;
 import org.nem.nis.controller.interceptors.*;
 import org.springframework.web.bind.WebDataBinder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.stream.Collectors;
 
 public class BinderControllerAdviceTest {
@@ -12,11 +16,14 @@ public class BinderControllerAdviceTest {
 	@Test
 	public void addBindersAddsAppropriateValidators() {
 		// Arrange:
-		final BinderControllerAdvice advice = new BinderControllerAdvice(null, null, null);
+		final BinderControllerAdvice advice = new BinderControllerAdvice(
+				Mockito.mock(LocalHostDetector.class),
+				Mockito.mock(ReadOnlyAccountStateCache.class),
+				Mockito.mock(NisConfiguration.class));
 		final WebDataBinder binder = new WebDataBinder(null);
 
 		// Act:
-		advice.addBinders(binder, null);
+		advice.addBinders(binder, Mockito.mock(HttpServletRequest.class));
 
 		// Assert:
 		final Class<?>[] expectedValidatorClasses = new Class<?>[] {
