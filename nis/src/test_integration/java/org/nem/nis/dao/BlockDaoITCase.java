fc315b5afd4fcb1cb06501d0ae7d1d33a76a807e
nem <nem@bob.nem.ninja>
2015-02-20 18:26:43 -0500

    ...

--- nis/src/test_integration/java/org/nem/nis/dao/BlockDaoITCase.java
+++ nis/src/test_integration/java/org/nem/nis/dao/BlockDaoITCase.java
@@ -1,6 +1,7 @@
 package org.nem.nis.dao;
 
-import org.junit.Test;
+import org.hamcrest.core.IsEqual;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.nem.core.model.primitive.BlockHeight;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,19 +24,13 @@ public class BlockDaoITCase {
 
 	@Test
 	public void getBlocksAfterItCase() {
+		// Act:
 		System.gc();
-		final long originalTime = measureTime(this.blockDao::getBlocksAfterOriginal);
+		final long elapsedTime = measureTime(this.blockDao::getBlocksAfter);
+		LOGGER.warning(String.format("getBlocksAfter needed %dms", elapsedTime));
 
-		System.gc();
-		final long newTime = measureTime(this.blockDao::getBlocksAfter);
-
-		// TODO 20150217 J-B: on my machine, i'm seeing times like:
-		// WARNING: getBlocksAfter needed 6410 ms(orig) and 9625 ms(new)
-
-		// TODO 20150217 J-B: the difference is also pronounced when processing just transfers:
-		// WARNING: getBlocksAfter needed 4557ms (orig) and 6802ms
-		// i will leave you with that
-		LOGGER.warning(String.format("getBlocksAfter needed %dms (orig) and %dms (new)", originalTime, newTime));
+		// Assert:
+		Assert.assertThat(elapsedTime < 7500, IsEqual.equalTo(true));
 	}
 
 	private static long measureTime(final BiConsumer<BlockHeight, Integer> getBlocks) {
