6976c40d9cd355b5196bbf5b9fdd4e72be84a265
nem <nem@bob.nem.ninja>
2015-02-19 22:11:22 -0500

    ...

--- nis/src/test/java/org/nem/nis/dbmodel/DbAccountTest.java
+++ nis/src/test/java/org/nem/nis/dbmodel/DbAccountTest.java
@@ -11,30 +11,30 @@ public class DbAccountTest {
 	@Test
 	public void hashCodesAreEqualForEquivalentObjects() {
 		// Arrange:
-		final int hashCode = DESC_TO_DBACCOUNT_MAP.get("default").hashCode();
+		final int hashCode = DESC_TO_DB_ACCOUNT_MAP.get("default").hashCode();
 
 		// Assert:
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("copy").hashCode(), IsEqual.equalTo(hashCode));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-id").hashCode(), IsNot.not(IsEqual.equalTo(hashCode)));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-address").hashCode(), IsNot.not(IsEqual.equalTo(hashCode)));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-address-and-pubKey").hashCode(), IsNot.not(IsEqual.equalTo(hashCode)));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-address").hashCode(), IsEqual.equalTo(DESC_TO_DBACCOUNT_MAP.get("diff-address-and-pubKey").hashCode()));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("copy").hashCode(), IsEqual.equalTo(hashCode));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-id").hashCode(), IsNot.not(IsEqual.equalTo(hashCode)));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-address").hashCode(), IsNot.not(IsEqual.equalTo(hashCode)));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-address-and-pubKey").hashCode(), IsNot.not(IsEqual.equalTo(hashCode)));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-address").hashCode(), IsEqual.equalTo(DESC_TO_DB_ACCOUNT_MAP.get("diff-address-and-pubKey").hashCode()));
 	}
 
 	@Test
 	public void equalsOnlyReturnsTrueForEquivalentObjects() {
 		// Arrange:
-		final DbAccount dbAccount = DESC_TO_DBACCOUNT_MAP.get("default");
+		final DbAccount dbAccount = DESC_TO_DB_ACCOUNT_MAP.get("default");
 
 		// Assert:
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("copy"), IsEqual.equalTo(dbAccount));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-id"), IsNot.not(IsEqual.equalTo(dbAccount)));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-address"), IsNot.not(IsEqual.equalTo(dbAccount)));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-address-and-pubKey"), IsNot.not(IsEqual.equalTo(dbAccount)));
-		Assert.assertThat(DESC_TO_DBACCOUNT_MAP.get("diff-address"), IsEqual.equalTo(DESC_TO_DBACCOUNT_MAP.get("diff-address-and-pubKey")));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("copy"), IsEqual.equalTo(dbAccount));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-id"), IsNot.not(IsEqual.equalTo(dbAccount)));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-address"), IsNot.not(IsEqual.equalTo(dbAccount)));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-address-and-pubKey"), IsNot.not(IsEqual.equalTo(dbAccount)));
+		Assert.assertThat(DESC_TO_DB_ACCOUNT_MAP.get("diff-address"), IsEqual.equalTo(DESC_TO_DB_ACCOUNT_MAP.get("diff-address-and-pubKey")));
 	}
 
-	private static final Map<String, DbAccount> DESC_TO_DBACCOUNT_MAP = new HashMap<String, DbAccount>() {
+	private static final Map<String, DbAccount> DESC_TO_DB_ACCOUNT_MAP = new HashMap<String, DbAccount>() {
 		{
 			this.put("default", NisUtils.createDbAccount(1L));
 			this.put("copy", NisUtils.createDbAccount(1L));
