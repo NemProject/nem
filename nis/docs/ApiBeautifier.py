9554aa2b0823cf59fa38fb08cb4142507c624cef
nem <nem@bob.nem.ninja>
2015-02-25 20:48:13 +0100

    ...

---
+++ nis/docs/ApiBeautifier.py
@@ -0,0 +1,81 @@
+#
+# helper script to generate TOC, and add links in proper h2 elements
+#
+
+from datetime import datetime
+import re
+from collections import OrderedDict
+from bs4 import BeautifulSoup
+
+def uncapitalize(s):
+	if (s.upper() == s):
+		return s
+	return s[:1].lower() + s[1:]
+
+def prettyName(t):
+	return '-'.join(map(uncapitalize, t.split()))
+
+r = re.compile(r'^(\s*)', re.MULTILINE)
+def soupTab(s, encoding=None, formatter="minimal"):
+	return s.encode(encoding)
+#	return r.sub(lambda mobj: '\t'*len(mobj.group(1)), s.prettify(encoding, formatter))
+
+def printToc(toc, indent=0):
+	r = "\t"*indent + "<ol>\n";
+	indent += 1 
+	for k,v in toc.iteritems():
+		r += ("\t" * (indent+1)) + "<li>"
+		if len(k) > 0:
+			r += "<a href='#%s'>%s</a>" % (prettyName(k), k)
+		if v != {}:
+			r += "\n" + printToc(v, indent+2)
+			r += ("\t" * (indent+1)) + "</li>\n"
+		else:
+			r += "</li>\n"
+	indent -= 1 
+	r += "\t"*indent + "</ol>\n"
+	return r
+
+
+def main():
+	o = OrderedDict()
+	toc = OrderedDict({'root':o})
+	parents = [o]
+
+	def addObj(name):
+		o = OrderedDict()
+		parents[-1][name] = o
+		parents.append(o)
+		
+	def adjust(level):
+		while len(parents) > level:
+			parents.pop()
+		#while len(parents) != level:
+		#	addObj('')
+
+	soup = BeautifulSoup(open('NisApi.src.html'))
+	for header in soup.findAll(['h1', 'h2', 'h3']):
+		c = header.contents[0]
+		header['id'] = prettyName(c)
+		level = int(header.name[1])
+		adjust(level)
+		addObj(c)
+	
+	for header in soup.findAll(['h4']):
+		if len(header.contents):
+			c = header.contents[0]
+			header['id'] = prettyName(c)
+
+	e=soup.find(id='toc')
+	e.clear()
+	newtoc = BeautifulSoup(printToc(toc['root']))
+	for elem in newtoc.html.body.contents:
+		e.append(elem)
+
+	d=soup.find(id='timedate')
+	d.clear()
+	d.append( datetime.utcnow().strftime('%H:%M, %B %d, %Y') )
+	open('index.html', 'w').write(soupTab(soup, 'utf-8'))
+
+if __name__ == "__main__":
+    main()
