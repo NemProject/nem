5da615af1402298df9746cac9bf0f18db0f473a5
nem <nem@bob.nem.ninja>
2015-03-03 18:36:38 -0500

    ...

---
+++ nis/src/test/java/org/nem/nis/test/validation/AbstractTransactionValidationTest.java
@@ -0,0 +1,243 @@
+package org.nem.nis.test.validation;
+
+import org.junit.*;
+import org.nem.core.model.*;
+import org.nem.core.model.primitive.*;
+import org.nem.core.test.*;
+import org.nem.core.time.TimeInstant;
+import org.nem.nis.cache.*;
+import org.nem.nis.state.AccountState;
+import org.nem.nis.test.NisCacheFactory;
+
+import java.util.*;
+
+public abstract class AbstractTransactionValidationTest {
+
+	//region real validators
+
+	//region transfers
+
+	@Test
+	public void getBlockTransactionsAllowsEarlierBlockTransfersToBeSpentLater() {
+		// Arrange:
+		final TestContext context = new TestContext();
+		final Account sender = context.addAccount(Amount.fromNem(20));
+		final Account recipient = context.addAccount(Amount.fromNem(10));
+		final TimeInstant currentTime = new TimeInstant(11);
+
+		// - T(O) - S: 20 | R: 10
+		// - T(1) - S -15-> R | S: 03 | R: 25
+		// - T(2) - R -12-> S | S: 15 | R: 10 # this transfer is allowed even though it wouldn't be allowed in reverse order
+		final Transaction t1 = createTransferTransaction(currentTime, sender, recipient, Amount.fromNem(15));
+		t1.setFee(Amount.fromNem(2));
+		t1.sign();
+		final Transaction t2 = createTransferTransaction(currentTime, recipient, sender, Amount.fromNem(12));
+		t2.setFee(Amount.fromNem(3));
+		t2.sign();
+
+		// Act / Assert:
+		this.assertTransactions(
+				context.nisCache,
+				Arrays.asList(t1, t2),
+				Arrays.asList(t1, t2),
+				ValidationResult.SUCCESS);
+	}
+
+	@Test
+	public void getBlockTransactionsExcludesTransactionFromNextBlockIfConfirmedBalanceIsInsufficient() {
+		// Arrange:
+		final TestContext context = new TestContext();
+		final Account sender = context.addAccount(Amount.fromNem(20));
+		final Account recipient = context.addAccount(Amount.fromNem(10));
+		final TimeInstant currentTime = new TimeInstant(11);
+
+		// - T(O) - S: 20 | R: 10
+		// - T(1) - R -12-> S | XXX
+		// - T(2) - S -15-> R | S: 03 | R: 25
+		final Transaction t1 = createTransferTransaction(currentTime, recipient, sender, Amount.fromNem(12));
+		t1.setFee(Amount.fromNem(3));
+		t1.sign();
+		final Transaction t2 = createTransferTransaction(currentTime, sender, recipient, Amount.fromNem(15));
+		t2.setFee(Amount.fromNem(2));
+		t2.sign();
+
+		// Act / Assert:
+		this.assertTransactions(
+				context.nisCache,
+				Arrays.asList(t1, t2),
+				Arrays.asList(t2),
+				ValidationResult.FAILURE_INSUFFICIENT_BALANCE);
+	}
+
+	//endregion
+
+	//region importance transfer
+
+	@Test
+	public void getBlockTransactionsDoesNotAllowConflictingImportanceTransfersToBeInSingleBlock() {
+		// Arrange:
+		final TestContext context = new TestContext();
+		final Account sender = context.addAccount(Amount.fromNem(50000));
+		final Account remote = context.addAccount(Amount.ZERO);
+
+		final Transaction t1 = createActivateImportanceTransfer(sender, remote);
+		final Transaction t2 = createActivateImportanceTransfer(sender, remote);
+
+		// Act / Assert:
+		this.assertTransactions(
+				context.nisCache,
+				Arrays.asList(t1, t2),
+				Arrays.asList(t1),
+				ValidationResult.FAILURE_INSUFFICIENT_BALANCE);
+	}
+
+	private static Transaction createActivateImportanceTransfer(final Account sender, final Account remote) {
+		final Transaction transaction = new ImportanceTransferTransaction(TimeInstant.ZERO, sender, ImportanceTransferTransaction.Mode.Activate, remote);
+		transaction.setDeadline(transaction.getTimeStamp().addMinutes(1));
+		return transaction;
+	}
+
+	//endregion
+
+	//region multisig modification
+
+	@Test
+	public void getBlockTransactionsDoesNotAllowMultipleMultisigModificationsForSameAccountToBeInSingleBlock() {
+		// Arrange:
+		final MultisigTestContext context = new MultisigTestContext();
+		final Account multisig = context.addAccount(Amount.fromNem(2000));
+		final Account cosigner1 = context.addAccount(Amount.ZERO);
+		final Account cosigner2 = context.addAccount(Amount.ZERO);
+		final Account cosigner3 = context.addAccount(Amount.ZERO);
+		context.makeCosignatory(cosigner1, multisig);
+
+		final Transaction t1 = createModification(cosigner1, multisig, cosigner2);
+		final Transaction t2 = createModification(cosigner1, multisig, cosigner3);
+
+		// Act / Assert:
+		this.assertTransactions(
+				context.nisCache,
+				Arrays.asList(t1, t2),
+				Arrays.asList(t1),
+				ValidationResult.FAILURE_INSUFFICIENT_BALANCE);
+	}
+
+	private static Transaction createModification(
+			final Account cosigner,
+			final Account multisig,
+			final Account newCosigner) {
+		final Transaction transaction = new MultisigAggregateModificationTransaction(
+				TimeInstant.ZERO,
+				multisig,
+				Arrays.asList(new MultisigModification(MultisigModificationType.Add, newCosigner)));
+		transaction.setDeadline(TimeInstant.ZERO.addMinutes(1));
+		return createMultisig(cosigner, transaction);
+	}
+
+	//endregion
+
+	//region multisig
+
+	@Test
+	public void getBlockTransactionsDoesNotReturnMultisigTransactionIfMultisigSignaturesAreNotPresent() {
+		// Arrange:
+		final MultisigTestContext context = new MultisigTestContext();
+		final Account multisig = context.addAccount(Amount.fromNem(2000));
+		final Account cosigner1 = context.addAccount(Amount.ZERO);
+		final Account cosigner2 = context.addAccount(Amount.ZERO);
+		final Account recipient = context.addAccount(Amount.ZERO);
+
+		context.makeCosignatory(cosigner1, multisig);
+		context.makeCosignatory(cosigner2, multisig);
+
+		final Transaction t1 = createTransferTransaction(TimeInstant.ZERO, multisig, recipient, Amount.fromNem(7));
+		final MultisigTransaction mt1 = createMultisig(cosigner1, t1);
+
+		// Act / Assert:
+		this.assertTransactions(
+				context.nisCache,
+				Arrays.asList(mt1),
+				Arrays.asList(),
+				ValidationResult.FAILURE_INSUFFICIENT_BALANCE);
+	}
+
+	@Test
+	public void getBlockTransactionsReturnsMultisigTransactionIfMultisigSignaturesArePresent() {
+		// Arrange:
+		final MultisigTestContext context = new MultisigTestContext();
+		final Account multisig = context.addAccount(Amount.fromNem(2000));
+		final Account cosigner1 = context.addAccount(Amount.ZERO);
+		final Account cosigner2 = context.addAccount(Amount.ZERO);
+		final Account recipient = context.addAccount(Amount.ZERO);
+
+		context.makeCosignatory(cosigner1, multisig);
+		context.makeCosignatory(cosigner2, multisig);
+
+		final Transaction t1 = createTransferTransaction(TimeInstant.ZERO, multisig, recipient, Amount.fromNem(7));
+		final MultisigTransaction mt1 = createMultisig(cosigner1, t1);
+		mt1.addSignature(createSignature(cosigner2, multisig, t1));
+
+		// Act / Assert:
+		this.assertTransactions(
+				context.nisCache,
+				Arrays.asList(mt1),
+				Arrays.asList(mt1),
+				ValidationResult.SUCCESS);
+	}
+
+	private static MultisigTransaction createMultisig(final Account cosigner, final Transaction innerTransaction) {
+		final MultisigTransaction transaction = new MultisigTransaction(TimeInstant.ZERO, cosigner, innerTransaction);
+		transaction.setDeadline(TimeInstant.ZERO.addMinutes(1));
+		return transaction;
+	}
+
+	private static MultisigSignatureTransaction createSignature(final Account cosigner, final Account multisig, final Transaction innerTransaction) {
+		final MultisigSignatureTransaction transaction = new MultisigSignatureTransaction(TimeInstant.ZERO, cosigner, multisig, innerTransaction);
+		transaction.setDeadline(TimeInstant.ZERO.addMinutes(1));
+		return transaction;
+	}
+
+	private static class MultisigTestContext extends TestContext {
+
+		public void makeCosignatory(final Account cosigner, final Account multisig) {
+			final NisCache copyCache = this.nisCache.copy();
+			final AccountStateCache accountStateCache = copyCache.getAccountStateCache();
+			accountStateCache.findStateByAddress(cosigner.getAddress()).getMultisigLinks().addCosignatoryOf(multisig.getAddress());
+			accountStateCache.findStateByAddress(multisig.getAddress()).getMultisigLinks().addCosignatory(cosigner.getAddress());
+			copyCache.commit();
+		}
+	}
+
+	//endregion
+
+	//endregion
+
+	protected abstract void assertTransactions(
+			final ReadOnlyNisCache nisCache,
+			final List<Transaction> all,
+			final List<Transaction> expectedFiltered,
+			final ValidationResult expectedResult);
+
+	public static TransferTransaction createTransferTransaction(final TimeInstant timeStamp, final Account sender, final Account recipient, final Amount amount) {
+		final TransferTransaction transferTransaction = new TransferTransaction(timeStamp, sender, recipient, amount, null);
+		transferTransaction.setDeadline(timeStamp.addSeconds(1));
+		return transferTransaction;
+	}
+
+	public static class TestContext {
+		public final ReadOnlyNisCache nisCache = NisCacheFactory.createReal();
+
+		public Account addAccount(final Amount amount) {
+			return this.prepareAccount(Utils.generateRandomAccount(), amount);
+		}
+
+		public Account prepareAccount(final Account account, final Amount amount) {
+			final NisCache copyCache = this.nisCache.copy();
+			final AccountState accountState = copyCache.getAccountStateCache().findStateByAddress(account.getAddress());
+			accountState.getAccountInfo().incrementBalance(amount);
+			accountState.getWeightedBalances().addFullyVested(BlockHeight.ONE, amount);
+			copyCache.commit();
+			return account;
+		}
+	}
+}
---
+++ nis/src/test/java/org/nem/nis/test/validation/DefaultNewBlockTransactionsProviderIntegrationTest.java
@@ -0,0 +1,64 @@
+package org.nem.nis.test.validation;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.mockito.Mockito;
+import org.nem.core.model.*;
+import org.nem.core.model.primitive.BlockHeight;
+import org.nem.core.test.*;
+import org.nem.core.time.TimeInstant;
+import org.nem.nis.cache.*;
+import org.nem.nis.harvesting.*;
+import org.nem.nis.secret.BlockTransactionObserverFactory;
+import org.nem.nis.test.NisUtils;
+
+import java.util.*;
+
+public class DefaultNewBlockTransactionsProviderIntegrationTest extends AbstractTransactionValidationTest {
+
+	@Override
+	protected void assertTransactions(
+			final ReadOnlyNisCache nisCache,
+			final List<Transaction> all,
+			final List<Transaction> expectedFiltered,
+			final ValidationResult expectedResult) {
+		final TestContext context = new TestContext(nisCache);
+		context.addTransactions(all);
+
+		// Act:
+		final List<Transaction> blockTransactions = context.getBlockTransactions();
+
+		// Assert:
+		Assert.assertThat(blockTransactions.size(), IsEqual.equalTo(expectedFiltered.size()));
+		Assert.assertThat(blockTransactions, IsEquivalent.equivalentTo(expectedFiltered));
+	}
+
+	private static class TestContext {
+		private final UnconfirmedTransactionsFilter unconfirmedTransactions = Mockito.mock(UnconfirmedTransactionsFilter.class);
+		private final List<Transaction> transactions = new ArrayList<>();
+		private final NewBlockTransactionsProvider provider;
+
+		private TestContext(final ReadOnlyNisCache nisCache) {
+			Mockito.when(this.unconfirmedTransactions.getTransactionsBefore(Mockito.any()))
+					.thenReturn(this.transactions);
+
+			this.provider = new DefaultNewBlockTransactionsProvider(
+					nisCache,
+					NisUtils.createTransactionValidatorFactory(),
+					NisUtils.createBlockValidatorFactory(),
+					new BlockTransactionObserverFactory(),
+					this.unconfirmedTransactions);
+		}
+
+		public List<Transaction> getBlockTransactions() {
+			return this.provider.getBlockTransactions(
+					Utils.generateRandomAccount().getAddress(),
+					TimeInstant.ZERO,
+					new BlockHeight(10));
+		}
+
+		public void addTransactions(final Collection<? extends Transaction> transactions) {
+			this.transactions.addAll(transactions);
+		}
+	}
+}
