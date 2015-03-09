3521f0210d8088f548d9b0af364854ff9b73239e
nem <nem@bob.nem.ninja>
2015-03-09 17:44:03 +0100

    ...

--- nis/src/main/java/org/nem/nis/secret/WeightedBalancesObserver.java
+++ nis/src/main/java/org/nem/nis/secret/WeightedBalancesObserver.java
@@ -24,16 +24,25 @@ public class WeightedBalancesObserver implements BlockTransferObserver {
 	// once with amount and once with fee
 	@Override
 	public void notifySend(final BlockHeight height, final Account account, final Amount amount) {
+		if (Amount.ZERO.equals(amount)) {
+			return;
+		}
+
 		this.getWeightedBalances(account).addSend(height, amount);
 	}
 
 	@Override
 	public void notifyReceive(final BlockHeight height, final Account account, final Amount amount) {
+		// TODO 20141215 J-G: is there any reason we don't filter out Amount.ZERO?
+		// TODO 20150309 BR -> J: not sure where the best place is to make the check (probably the observer should have the intelligence).
+		if (Amount.ZERO.equals(amount)) {
+			return;
+		}
+
 		final WeightedBalances weightedBalances = this.getWeightedBalances(account);
 		weightedBalances.addReceive(height, amount);
 
 		// fully vest all transactions coming out of the nemesis block
-		// TODO 20141215 J-G: is there any reason we don't filter out Amount.ZERO?
 		if (BlockHeight.ONE.equals(height) && !NemesisBlock.ADDRESS.equals(account.getAddress())) {
 			weightedBalances.convertToFullyVested();
 		}
@@ -43,11 +52,19 @@ public class WeightedBalancesObserver implements BlockTransferObserver {
 	// once with amount and once with fee
 	@Override
 	public void notifySendUndo(final BlockHeight height, final Account account, final Amount amount) {
+		if (Amount.ZERO.equals(amount)) {
+			return;
+		}
+
 		this.getWeightedBalances(account).undoSend(height, amount);
 	}
 
 	@Override
 	public void notifyReceiveUndo(final BlockHeight height, final Account account, final Amount amount) {
+		if (Amount.ZERO.equals(amount)) {
+			return;
+		}
+
 		this.getWeightedBalances(account).undoReceive(height, amount);
 	}
 
