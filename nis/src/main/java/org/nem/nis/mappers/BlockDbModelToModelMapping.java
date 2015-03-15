7c55f724530c436b122a4e3b69968fc961f5e8ed
nem <nem@bob.nem.ninja>
2015-03-15 18:45:43 -0400

    ...

--- nis/src/main/java/org/nem/nis/mappers/BlockDbModelToModelMapping.java
+++ nis/src/main/java/org/nem/nis/mappers/BlockDbModelToModelMapping.java
@@ -3,7 +3,6 @@ package org.nem.nis.mappers;
 import org.nem.core.crypto.Signature;
 import org.nem.core.model.*;
 import org.nem.core.model.primitive.*;
-import org.nem.core.serialization.AccountLookup;
 import org.nem.core.time.TimeInstant;
 import org.nem.nis.dbmodel.*;
 
@@ -15,31 +14,18 @@ import java.util.stream.*;
  */
 public class BlockDbModelToModelMapping implements IMapping<DbBlock, Block> {
 	private final IMapper mapper;
-	private final AccountLookup accountLookup;
 
 	/**
 	 * Creates a new mapping.
 	 *
 	 * @param mapper The mapper.
-	 * @param accountLookup The account lookup.
 	 */
-	public BlockDbModelToModelMapping(
-			final IMapper mapper,
-			final AccountLookup accountLookup) {
+	public BlockDbModelToModelMapping(final IMapper mapper) {
 		this.mapper = mapper;
-		this.accountLookup = accountLookup;
 	}
 
 	@Override
 	public Block map(final DbBlock dbBlock) {
-		// TODO 20141226 J-G: do you remember why we have this special case / do we still need it after separating the account and account state?
-		// TODO 20150309 BR -> J: works perfectly without this block of code.
-		// TODO 20150309 J -> B: after removing this line there is an issue syncing from scratch (i didn't have time to debug)
-		// TODO 20150310 BR -> J: it is the block type/block class, I should have tested syncing from scratch :/ There is an easy fix but now
-		// > the nemesis block is not mapped to a NemesisBlock object which makes BlockDbModelToModelMappingTest.nemesisDbModelCanBeMappedToNemesisModel() fail.
-		// > Is it really important that the nemesis block is mapped to its own class?
-		// > Anyway, i created a new branch in order not to pollute the todo20150301 branch.
-
 		final Account harvester = this.mapper.map(dbBlock.getHarvester(), Account.class);
 		final Account lessor = dbBlock.getLessor() != null ? this.mapper.map(dbBlock.getLessor(), Account.class) : null;
 
