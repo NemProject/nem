92795bbfd0b827685ebde94bd9f5234a0e96431b
nem <nem@bob.nem.ninja>
2015-03-22 16:57:40 +0100

    ...

--- nis/src/test_integration/java/org/nem/nis/dao/H2Test.java
+++ nis/src/test_integration/java/org/nem/nis/dao/H2Test.java
@@ -1,7 +1,7 @@
 package org.nem.nis.dao;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Test;
+import org.junit.*;
 
 import java.security.SecureRandom;
 import java.sql.*;
@@ -11,16 +11,17 @@ import java.util.stream.*;
 public class H2Test {
 	private static final Logger LOGGER = Logger.getLogger(H2Test.class.getName());
 
+	@Ignore
 	@Test
 	public void h2MemoryTest() {
 		final H2Database db = new H2Database();
 		final int range = 1000;
-		for (int j = 0; j < 10000; j++) {
+		for (int j = 0; j < 1000; j++) {
 			if (j % 100 == 0) {
 				LOGGER.info(String.format("round %d", j + 1));
 			}
 			final int randomInt = new SecureRandom().nextInt(range);
-			final String ids = StringUtils.join(IntStream.range(1, 19000 + randomInt).mapToObj(i -> new Long(i)).collect(Collectors.toList()), ",");
+			final String ids = StringUtils.join(IntStream.range(1, 19000 + randomInt).mapToObj(Long::new).collect(Collectors.toList()), ",");
 			final String sql = "SELECT a.* FROM accounts a WHERE a.id in (" + ids + ")";
 			final ResultSet rs = db.executeQuery(sql);
 
@@ -35,6 +36,8 @@ public class H2Test {
 
 			// TODO 20150320 J-B: should we have an assert here? e.g. the delta in used memory is less than some value?
 			// TODO 20150320 J-B: also can we cut down on the iterations on the dao tests, in general?
+			// TODO 20150322 BR -> J: not sure if we can make a meaningful assert here. If you look at how successful a full garbage collection is
+			// > you see that it varies a lot.
 		}
 	}
 }
