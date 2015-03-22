c32aa339b3b53e0b678c6376e73f3b986de82fba
nem <nem@bob.nem.ninja>
2015-03-22 21:48:19 +0100

    ...

--- nis/src/main/java/org/nem/deploy/appconfig/NisAppConfig.java
+++ nis/src/main/java/org/nem/deploy/appconfig/NisAppConfig.java
@@ -3,6 +3,7 @@ package org.nem.deploy.appconfig;
 import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
 import org.nem.core.deploy.*;
+import org.nem.core.model.NetworkInfos;
 import org.nem.core.model.primitive.*;
 import org.nem.core.time.TimeProvider;
 import org.nem.deploy.*;
@@ -289,6 +290,8 @@ public class NisAppConfig {
 
 	@Bean
 	public NisMain nisMain() {
+		// initialize network info
+		NetworkInfos.setDefault(this.nisConfiguration().getNetworkInfo());
 		return new NisMain(
 				this.blockDao,
 				this.nisCache(),
