b47cf1e2a197767762f7dc90cec8aaaf6b0262be
nem <nem@bob.nem.ninja>
2015-06-09 22:27:08 +0200

    ...

--- nis/src/main/java/org/nem/nis/BlockMarkerConstants.java
+++ nis/src/main/java/org/nem/nis/BlockMarkerConstants.java
@@ -9,5 +9,5 @@ public class BlockMarkerConstants {
 	 * Hard fork due to:
 	 * Switch from multisig aggregate modification transaction V1 to V2
 	 */
-	public static final long MULTISIG_M_OF_N_FORK = 90000;
+	public static final long MULTISIG_M_OF_N_FORK = 93000;
 }
