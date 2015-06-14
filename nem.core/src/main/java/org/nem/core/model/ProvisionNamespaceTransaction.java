d8e3f92bc97ce939e465f0a78cf55b03f1ba2bb5
BloodyRookie <nemproject@gmx.de>
2015-06-14 11:50:06 +0200

    provision namespace transaction tests.

--- nem.core/src/main/java/org/nem/core/model/ProvisionNamespaceTransaction.java
+++ nem.core/src/main/java/org/nem/core/model/ProvisionNamespaceTransaction.java
@@ -1,8 +1,8 @@
 package org.nem.core.model;
 
 import org.nem.core.model.namespace.*;
-import org.nem.core.model.observers.TransactionObserver;
-import org.nem.core.serialization.Serializer;
+import org.nem.core.model.observers.*;
+import org.nem.core.serialization.*;
 import org.nem.core.time.TimeInstant;
 
 import java.util.*;
@@ -14,6 +14,15 @@ public class ProvisionNamespaceTransaction extends Transaction {
 	private final NamespaceIdPart newPart;
 	private final NamespaceId parent;
 
+	/**
+	 * Creates a new provision namespace transaction.
+	 * The parent parameter is allowed to be null.
+	 *
+	 * @param timeStamp The timestamp.
+	 * @param sender The sender.
+	 * @param newPart The new namespace id part.
+	 * @param parent The parent namespace id.
+	 */
 	public ProvisionNamespaceTransaction(
 			final TimeInstant timeStamp,
 			final Account sender,
@@ -24,16 +33,48 @@ public class ProvisionNamespaceTransaction extends Transaction {
 		this.parent = parent;
 	}
 
+	/**
+	 * Deserializes a multisig signature transaction.
+	 *
+	 * @param options The deserialization options.
+	 * @param deserializer The deserializer.
+	 */
+	public ProvisionNamespaceTransaction(final DeserializationOptions options, final Deserializer deserializer) {
+		super(TransactionTypes.PROVISION_NAMESPACE, options, deserializer);
+		this.newPart = deserializer.readObject("newPart", NamespaceIdPart::new);
+		this.parent = deserializer.readObject("parent", NamespaceId::new);
+	}
+
+	/**
+	 * Gets the new part for the namespace id.
+	 *
+	 * @return The new namespace id part.
+	 */
 	public NamespaceIdPart getNewPart() {
 		return this.newPart;
 	}
 
-	public NamespaceId getResultingNamespace() {
+	/**
+	 * Gets the parent namespace id.
+	 *
+	 * @return The parent namespace id.
+	 */
+	public NamespaceId getParent() {
+		return this.parent;
+	}
+
+	/**
+	 * Gets the resulting namespace id after appending the new part to the parent
+	 * .
+	 * @return The resulting namespace id.
+	 */
+	public NamespaceId getResultingNamespaceId() {
 		return null == this.parent ? this.newPart.toNamespaceId() : this.parent.concat(this.newPart);
 	}
 
-	public NamespaceId getParent() {
-		return this.parent;
+	@Override
+	protected Collection<Account> getOtherAccounts() {
+		return Collections.emptyList();
 	}
 
 	@Override
@@ -45,11 +86,6 @@ public class ProvisionNamespaceTransaction extends Transaction {
 
 	@Override
 	protected void transfer(final TransactionObserver observer) {
-
-	}
-
-	@Override
-	protected Collection<Account> getOtherAccounts() {
-		return Collections.emptyList();
+		observer.notify(new BalanceAdjustmentNotification(NotificationType.BalanceDebit, this.getDebtor(), this.getFee()));
 	}
 }
---
+++ nem.core/src/test/java/org/nem/core/model/ProvisionNamespaceTransactionTest.java
@@ -0,0 +1,177 @@
+package org.nem.core.model;
+
+import org.hamcrest.core.*;
+import org.junit.*;
+import org.mockito.*;
+import org.nem.core.model.namespace.*;
+import org.nem.core.model.observers.*;
+import org.nem.core.model.primitive.Amount;
+import org.nem.core.serialization.Deserializer;
+import org.nem.core.test.*;
+import org.nem.core.time.TimeInstant;
+
+import java.util.Collection;
+
+public class ProvisionNamespaceTransactionTest {
+	private static final Account SIGNER = Utils.generateRandomAccount();
+	private static final TimeInstant TIME_INSTANT = new TimeInstant((123));
+
+	// region ctor
+
+	@Test
+	public void canCreateTransactionWithNonNullParentParameter() {
+		assertCanCreateTransaction("bar", "foo");
+	}
+
+	@Test
+	public void canCreateTransactionWithNullParentParameter() {
+		assertCanCreateTransaction("bar", null);
+	}
+
+	private static void assertCanCreateTransaction(final String newPart, final String parent) {
+		// Act:
+		final ProvisionNamespaceTransaction transaction = createTransaction(newPart, parent);
+
+		// Assert:
+		Assert.assertThat(transaction.getType(), IsEqual.equalTo(TransactionTypes.PROVISION_NAMESPACE));
+		Assert.assertThat(transaction.getVersion(), IsEqual.equalTo(VerifiableEntityUtils.VERSION_ONE));
+		Assert.assertThat(transaction.getTimeStamp(), IsEqual.equalTo(TIME_INSTANT));
+		Assert.assertThat(transaction.getSigner(), IsEqual.equalTo(SIGNER));
+		Assert.assertThat(transaction.getNewPart(), IsEqual.equalTo(new NamespaceIdPart(newPart)));
+		Assert.assertThat(transaction.getParent(), null == parent ? IsNull.nullValue() : IsEqual.equalTo(new NamespaceId(parent)));
+	}
+
+	// endregion
+
+	// region getResultingNamespace
+
+	@Test
+	public void getResultingNamespaceIdReturnsConcatenatedNamespaceIdIfParentIsNotNull() {
+		// Arrange:
+		final ProvisionNamespaceTransaction transaction = createTransaction("bar", "foo");
+
+		// Act:
+		final NamespaceId result = transaction.getResultingNamespaceId();
+
+		// Assert:
+		Assert.assertThat(result, IsEqual.equalTo(new NamespaceId("foo.bar")));
+	}
+
+	@Test
+	public void getResultingNamespaceIdReturnsNamespaceIdOfNewPartIfParentIsNull() {
+		// Arrange:
+		final ProvisionNamespaceTransaction transaction = createTransaction("bar", null);
+
+		// Act:
+		final NamespaceId result = transaction.getResultingNamespaceId();
+
+		// Assert:
+		Assert.assertThat(result, IsEqual.equalTo(new NamespaceId("bar")));
+	}
+
+	// endregion
+
+	// region getOtherAccounts
+
+	@Test
+	public void getOtherAccountsReturnsEmptyList() {
+		// Arrange:
+		final ProvisionNamespaceTransaction transaction = createTransaction("bar", "foo");
+
+		// Act:
+		final Collection<Account> accounts = transaction.getOtherAccounts();
+
+		// Assert:
+		Assert.assertThat(accounts.isEmpty(), IsEqual.equalTo(true));
+	}
+
+	// endregion
+
+	// region getAccounts
+
+	@Test
+	public void getAccountsIncludesOnlySigner() {
+		// Arrange:
+		final ProvisionNamespaceTransaction transaction = createTransaction("bar", "foo");
+
+		// Act:
+		final Collection<Account> accounts = transaction.getAccounts();
+
+		// Assert:
+		Assert.assertThat(accounts, IsEquivalent.equivalentTo(SIGNER));
+	}
+
+	// endregion
+
+	//region execute / undo
+
+	@Test
+	public void executeRaisesAppropriateNotifications() {
+		// Arrange:
+		final Transaction transaction = createTransaction("bar", "foo");
+		transaction.setFee(Amount.fromNem(25000));
+
+		// Act:
+		final TransactionObserver observer = Mockito.mock(TransactionObserver.class);
+		transaction.execute(observer);
+
+		// Assert:
+		final ArgumentCaptor<Notification> notificationCaptor = ArgumentCaptor.forClass(Notification.class);
+		Mockito.verify(observer, Mockito.only()).notify(notificationCaptor.capture());
+		NotificationUtils.assertBalanceDebitNotification(notificationCaptor.getValue(), SIGNER, Amount.fromNem(25000));
+	}
+
+	@Test
+	public void undoRaisesAppropriateNotifications() {
+		// Arrange:
+		final Transaction transaction = createTransaction("bar", "foo");
+		transaction.setFee(Amount.fromNem(25000));
+
+		// Act:
+		final TransactionObserver observer = Mockito.mock(TransactionObserver.class);
+		transaction.undo(observer);
+
+		// Assert:
+		final ArgumentCaptor<Notification> notificationCaptor = ArgumentCaptor.forClass(Notification.class);
+		Mockito.verify(observer, Mockito.only()).notify(notificationCaptor.capture());
+		NotificationUtils.assertBalanceCreditNotification(notificationCaptor.getValue(), SIGNER, Amount.fromNem(25000));
+	}
+
+	// endregion
+
+	// region round trip
+
+	@Test
+	public void canRoundtripTransaction() {
+		// Arrange:
+		final ProvisionNamespaceTransaction original = createTransaction("bar", "foo");
+
+		// Act:
+		final ProvisionNamespaceTransaction transaction = createRoundTrippedTransaction(original);
+
+		// Assert:
+		Assert.assertThat(transaction.getType(), IsEqual.equalTo(TransactionTypes.PROVISION_NAMESPACE));
+		Assert.assertThat(transaction.getVersion(), IsEqual.equalTo(VerifiableEntityUtils.VERSION_ONE));
+		Assert.assertThat(transaction.getTimeStamp(), IsEqual.equalTo(TIME_INSTANT));
+		Assert.assertThat(transaction.getSigner(), IsEqual.equalTo(SIGNER));
+		Assert.assertThat(transaction.getNewPart(), IsEqual.equalTo(new NamespaceIdPart("bar")));
+		Assert.assertThat(transaction.getParent(), IsEqual.equalTo(new NamespaceId("foo")));
+	}
+
+	private static ProvisionNamespaceTransaction createRoundTrippedTransaction(final Transaction originalTransaction) {
+		// Act:
+		final Deserializer deserializer = Utils.roundtripSerializableEntity(originalTransaction.asNonVerifiable(), new MockAccountLookup());
+		deserializer.readInt("type");
+		return new ProvisionNamespaceTransaction(VerifiableEntity.DeserializationOptions.NON_VERIFIABLE, deserializer);
+	}
+
+	// endregion
+
+	private static ProvisionNamespaceTransaction createTransaction(final String newPart, final String parent) {
+		return new ProvisionNamespaceTransaction(
+				TIME_INSTANT,
+				SIGNER,
+				new NamespaceIdPart(newPart),
+				null == parent ? null : new NamespaceId(parent));
+	}
+}
