5ce43d9f594fbf7b9ed3fe27dd041cf101777946
nem <nem@bob.nem.ninja>
2015-06-20 12:02:07 -0400

    ...

---
+++ nis/src/it/java/org/nem/nis/dbmodel/DbProvisionNamespaceTransactionTest.java
@@ -0,0 +1,23 @@
+package org.nem.nis.dbmodel;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.*;
+
+public class DbProvisionNamespaceTransactionTest {
+
+	@Test
+	public void setBlockSetsHeightInNamespace() {
+		// Arrange:
+		final DbProvisionNamespaceTransaction transaction = new DbProvisionNamespaceTransaction();
+		transaction.setNamespace(new DbNamespace());
+		final DbBlock block = new DbBlock();
+		block.setHeight(20L);
+
+		// Act:
+		transaction.setBlock(block);
+
+		// Assert:
+		Assert.assertThat(transaction.getBlock(), IsEqual.equalTo(block));
+		Assert.assertThat(transaction.getNamespace().getExpiryHeight(), IsEqual.equalTo(20L));
+	}
+}
