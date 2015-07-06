81ffea3e2600dd9f96683c6ee423bb22f7a6d380
nem <nem@bob.nem.ninja>
2015-07-06 17:48:43 +0200

    ...

--- nis/src/main/resources/db/h2/V1.0.4__mosaic_tables.sql
+++ nis/src/main/resources/db/h2/V1.0.4__mosaic_tables.sql
@@ -1,47 +1,12 @@
-CREATE TABLE IF NOT EXISTS `mosaiccreationtransactions` (
-  `blockId` BIGINT NOT NULL,
-
-  `id` BIGINT NOT NULL DEFAULT transaction_id_seq.nextval,
-  `transferHash` VARBINARY(34) NOT NULL,
-
-  `version` INT NOT NULL,
-  `fee` BIGINT NOT NULL,
-  `timestamp` INT NOT NULL,
-  `deadline` INT NOT NULL,
-
-  `senderId` BIGINT NOT NULL, -- reference to accounts
-  `senderProof` VARBINARY(66), -- can be null for multisig TXes
-
-  `blkIndex` INT NOT NULL, -- index inside block
-  `referencedTransaction` BIGINT NOT NULL, -- do we want this?
-
-  PRIMARY KEY (`id`)
-);
-
-ALTER TABLE public.mosaiccreationtransactions ADD
-  FOREIGN KEY (blockId)
-  REFERENCES public.blocks(id);
-
-ALTER TABLE public.mosaiccreationtransactions ADD
-  FOREIGN KEY (senderId)
-  REFERENCES public.accounts(id);
-
 CREATE TABLE IF NOT EXISTS `mosaics` (
-  `mosaicCreationTransactionId` BIGINT NOT NULL,
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `creatorId` BIGINT NOT NULL, -- reference to accounts
-  `mosaicId` VARCHAR(34) NOT NULL,
+  `name` VARCHAR(34) NOT NULL,
   `description` VARCHAR(514) NOT NULL,
   `namespaceId` VARCHAR(148) NOT NULL,
-  `amount` BIGINT NOT NULL,
-  `position` INTEGER NOT NULL,
 
   PRIMARY KEY (`id`)
 );
-
-ALTER TABLE public.mosaics ADD
-  FOREIGN KEY (mosaicCreationTransactionId)
-  REFERENCES public.mosaicCreationTransactions(id);
 
 ALTER TABLE public.mosaics ADD
   FOREIGN KEY (creatorId)
@@ -60,6 +25,39 @@ ALTER TABLE public.mosaicproperties ADD
   FOREIGN KEY (mosaicId)
   REFERENCES public.mosaics(id);
 
+CREATE TABLE IF NOT EXISTS `mosaiccreationtransactions` (
+  `blockId` BIGINT NOT NULL,
+
+  `id` BIGINT NOT NULL DEFAULT transaction_id_seq.nextval,
+  `transferHash` VARBINARY(34) NOT NULL,
+
+  `version` INT NOT NULL,
+  `fee` BIGINT NOT NULL,
+  `timestamp` INT NOT NULL,
+  `deadline` INT NOT NULL,
+
+  `senderId` BIGINT NOT NULL, -- reference to accounts
+  `senderProof` VARBINARY(66), -- can be null for multisig TXes
+  `mosaicId` BIGINT NOT NULL, -- reference to mosaics
+
+  `blkIndex` INT NOT NULL, -- index inside block
+  `referencedTransaction` BIGINT NOT NULL, -- do we want this?
+
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE public.mosaiccreationtransactions ADD
+  FOREIGN KEY (blockId)
+  REFERENCES public.blocks(id);
+
+ALTER TABLE public.mosaiccreationtransactions ADD
+  FOREIGN KEY (senderId)
+  REFERENCES public.accounts(id);
+
+ALTER TABLE public.mosaiccreationtransactions ADD
+  FOREIGN KEY (mosaicId)
+  REFERENCES public.mosaics(id);
+
 ALTER TABLE public.multisigtransactions ADD
   COLUMN `mosaicCreationId` BIGINT;
 
@@ -69,13 +67,13 @@ ALTER TABLE public.multisigtransactions ADD
 
 CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_BLOCKID_ASC ON `mosaiccreationtransactions` (blockId ASC);
 CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_TIMESTAMP ON `mosaiccreationtransactions` (timeStamp);
+CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_MOSAICID ON `mosaiccreationtransactions` (mosaicId);
 CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_SENDERID ON `mosaiccreationtransactions` (senderId);
 CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_SENDERID_ID ON `mosaiccreationtransactions` (senderId, id DESC);
 
-CREATE INDEX IDX_MOSAICS_MOSAICCREATIONTRANSACTIONID ON `mosaics` (mosaicCreationTransactionId);
-CREATE INDEX IDX_MOSAICS_MOSAICID ON `mosaics` (mosaicId);
+CREATE INDEX IDX_MOSAICS_NAME ON `mosaics` (name);
 CREATE INDEX IDX_MOSAICS_NAMESPACEID ON `mosaics` (namespaceId);
-CREATE INDEX IDX_MOSAICS_NAMESPACEID_MOSAICID ON `mosaics` (namespaceId, mosaicId);
+CREATE INDEX IDX_MOSAICS_NAMESPACEID_NAME ON `mosaics` (namespaceId, name);
 CREATE INDEX IDX_MOSAICS_CREATORID ON `mosaics` (creatorId);
 CREATE INDEX IDX_MOSAICS_CREATORID_ID ON `mosaics` (creatorId, id DESC);
 
