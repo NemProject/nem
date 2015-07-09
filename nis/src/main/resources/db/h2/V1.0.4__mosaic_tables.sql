55de6c9dda4ce76d23f129adc077da8d7b424201
nem <nem@bob.nem.ninja>
2015-07-09 13:48:10 +0200

    ...

--- nis/src/main/resources/db/h2/V1.0.4__mosaic_tables.sql
+++ nis/src/main/resources/db/h2/V1.0.4__mosaic_tables.sql
@@ -58,6 +58,38 @@ ALTER TABLE public.mosaiccreationtransactions ADD
   FOREIGN KEY (mosaicId)
   REFERENCES public.mosaics(id);
 
+CREATE TABLE IF NOT EXISTS `smarttilesupplychanges` (
+  `blockId` BIGINT NOT NULL,
+
+  `id` BIGINT NOT NULL DEFAULT transaction_id_seq.nextval,
+  `transferHash` VARBINARY(34) NOT NULL,
+
+  `version` INT NOT NULL,
+  `fee` BIGINT NOT NULL,
+  `timestamp` INT NOT NULL,
+  `deadline` INT NOT NULL,
+
+  `senderId` BIGINT NOT NULL, -- reference to accounts
+  `senderProof` VARBINARY(66), -- can be null for multisig TXes
+  `namespaceId` VARCHAR(148) NOT NULL,
+  `mosaicName` VARCHAR(34) NOT NULL,
+  `supplyType` INT NOT NULL,
+  `quantity` BIGINT NOT NULL,
+
+  `blkIndex` INT NOT NULL, -- index inside block
+  `referencedTransaction` BIGINT NOT NULL, -- do we want this?
+
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE public.smarttilesupplychanges ADD
+  FOREIGN KEY (blockId)
+  REFERENCES public.blocks(id);
+
+ALTER TABLE public.smarttilesupplychanges ADD
+  FOREIGN KEY (senderId)
+  REFERENCES public.accounts(id);
+
 ALTER TABLE public.multisigtransactions ADD
   COLUMN `mosaicCreationId` BIGINT;
 
@@ -70,6 +102,14 @@ CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_TIMESTAMP ON `mosaiccreationtransact
 CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_MOSAICID ON `mosaiccreationtransactions` (mosaicId);
 CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_SENDERID ON `mosaiccreationtransactions` (senderId);
 CREATE INDEX IDX_MOSAICCREATIONTRANSACTIONS_SENDERID_ID ON `mosaiccreationtransactions` (senderId, id DESC);
+
+CREATE INDEX IDX_SMARTTILESUPPLYCHANGES_BLOCKID_ASC ON `smarttilesupplychanges` (blockId ASC);
+CREATE INDEX IDX_SMARTTILESUPPLYCHANGES_TIMESTAMP ON `smarttilesupplychanges` (timeStamp);
+CREATE INDEX IDX_SMARTTILESUPPLYCHANGES_SUPPLYTYPE ON `smarttilesupplychanges` (supplyType);
+CREATE INDEX IDX_SMARTTILESUPPLYCHANGES_NAMESPACEID ON `smarttilesupplychanges` (namespaceId);
+CREATE INDEX IDX_SMARTTILESUPPLYCHANGES_MOSAICNAME ON `smarttilesupplychanges` (mosaicName);
+CREATE INDEX IDX_SMARTTILESUPPLYCHANGES_SENDERID ON `smarttilesupplychanges` (senderId);
+CREATE INDEX IDX_SMARTTILESUPPLYCHANGES_SENDERID_ID ON `smarttilesupplychanges` (senderId, id DESC);
 
 CREATE INDEX IDX_MOSAICS_NAME ON `mosaics` (name);
 CREATE INDEX IDX_MOSAICS_NAMESPACEID ON `mosaics` (namespaceId);
