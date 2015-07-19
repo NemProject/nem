9cd5c355764549f31f3d42e9f6c04ae16862656d
Jaguar0625 <jaguar0625@127.0.0.1>
2015-07-18 20:09:03 -0400

    pair refactoring part 2

--- nem.core/src/test/java/org/nem/core/model/ncc/UnconfirmedTransactionMetaDataPairTest.java
+++ nem.core/src/test/java/org/nem/core/model/ncc/UnconfirmedTransactionMetaDataPairTest.java
@@ -1,65 +1,22 @@
 package org.nem.core.model.ncc;
 
-import org.hamcrest.core.*;
-import org.junit.*;
 import org.nem.core.crypto.Hash;
 import org.nem.core.model.*;
-import org.nem.core.model.primitive.Amount;
 import org.nem.core.test.*;
-import org.nem.core.time.TimeInstant;
 
-public class UnconfirmedTransactionMetaDataPairTest {
+public class UnconfirmedTransactionMetaDataPairTest extends AbstractMetaDataPairTest<Transaction, UnconfirmedTransactionMetaData> {
 
-	@Test
-	public void canCreateUnconfirmedTransactionMetaDataPair() {
-		// Arrange:
-		final Account signer = Utils.generateRandomAccount();
-		final MockTransaction transaction = new MockTransaction(signer, 6);
-		transaction.sign();
-		final UnconfirmedTransactionMetaData metaData = new UnconfirmedTransactionMetaData(Utils.generateRandomHash());
-
-		// Act:
-		final UnconfirmedTransactionMetaDataPair entity = new UnconfirmedTransactionMetaDataPair(transaction, metaData);
-
-		// Assert:
-		Assert.assertThat(entity.getTransaction(), IsSame.sameInstance(transaction));
-		Assert.assertThat(entity.getMetaData(), IsSame.sameInstance(metaData));
-	}
-
-	@Test
-	public void canRoundTripUnconfirmedTransactionMetaDataPair() {
-		// Arrange:
-		final Account signer = Utils.generateRandomAccount();
-		final Hash hash = Utils.generateRandomHash();
-
-		// Act:
-		final UnconfirmedTransactionMetaDataPair metaDataPair = createRoundTrippedPair(signer, 123, hash);
-
-		// Assert:
-		Assert.assertThat(metaDataPair.getTransaction().getType(), IsEqual.equalTo(TransactionTypes.TRANSFER));
-		final TransferTransaction transaction = (TransferTransaction)metaDataPair.getTransaction();
-		Assert.assertThat(transaction.getAmount(), IsEqual.equalTo(Amount.fromNem(123)));
-		Assert.assertThat(transaction.getSigner(), IsEqual.equalTo(signer));
-
-		Assert.assertThat(metaDataPair.getMetaData().getInnerTransactionHash(), IsEqual.equalTo(hash));
-	}
-
-	private static UnconfirmedTransactionMetaDataPair createRoundTrippedPair(
-			final Account signer,
-			final long amount,
-			final Hash innerTransactionHash) {
-		// Arrange:
-		final Transaction transaction = new TransferTransaction(
-				TimeInstant.ZERO,
-				signer,
-				signer,
-				Amount.fromNem(amount),
-				null);
-		transaction.sign();
-		final UnconfirmedTransactionMetaData metaData = new UnconfirmedTransactionMetaData(innerTransactionHash);
-		final UnconfirmedTransactionMetaDataPair metaDataPair = new UnconfirmedTransactionMetaDataPair(transaction, metaData);
-
-		// Act:
-		return new UnconfirmedTransactionMetaDataPair(Utils.roundtripSerializableEntity(metaDataPair, new MockAccountLookup()));
+	public UnconfirmedTransactionMetaDataPairTest() {
+		super(
+				address -> {
+					final Transaction transfer = RandomTransactionFactory.createTransfer(new Account(address));
+					transfer.sign();
+					return transfer;
+				},
+				id -> new UnconfirmedTransactionMetaData(Hash.fromHexString(Integer.toHexString(id))),
+				UnconfirmedTransactionMetaDataPair::new,
+				UnconfirmedTransactionMetaDataPair::new,
+				transaction -> transaction.getSigner().getAddress(),
+				metaData -> Integer.parseInt(metaData.getInnerTransactionHash().toString(), 16));
 	}
 }
