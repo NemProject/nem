9228a0c61f387c93559aafabbd6a87fded1b63b8
nem <nem@bob.nem.ninja>
2015-08-16 11:58:13 -0400

    ...

--- nis/src/test/java/org/nem/nis/validators/integration/AbstractBlockChainValidatorTransactionValidationTest.java
+++ nis/src/test/java/org/nem/nis/validators/integration/AbstractBlockChainValidatorTransactionValidationTest.java
@@ -22,51 +22,6 @@ import java.util.*;
 
 public abstract class AbstractBlockChainValidatorTransactionValidationTest extends AbstractTransactionValidationTest {
 
-	// TODO 20150814 J-B: i think all the tests in this class that call NisUtils.createBlockList
-	// > can / should be moved to MultiBlockBlockChainValidatorTransactionValidationTest
-	// > thoughts?
-
-	@Test
-	public void allBlocksInChainMustHaveValidTimestamp() {
-		// Arrange:
-		final TestContext context = new TestContext();
-		final Block parentBlock = NisUtils.createParentBlock(Utils.generateRandomAccount(), 11);
-		parentBlock.sign();
-
-		final List<Block> blocks = NisUtils.createBlockList(parentBlock, 3);
-		final Block block = createFutureBlock(blocks.get(2));
-		blocks.add(block);
-
-		// Act:
-		final BlockChainValidator validator = new BlockChainValidatorFactory().create(context.nisCache.copy());
-		final ValidationResult result = validator.isValid(parentBlock, blocks);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.FAILURE_TIMESTAMP_TOO_FAR_IN_FUTURE));
-	}
-
-	@Test
-	public void chainIsInvalidIfAnyTransactionInBlockIsSignedByBlockHarvester() {
-		// Arrange:
-		final TestContext context = new TestContext();
-		final Block parentBlock = NisUtils.createParentBlock(Utils.generateRandomAccount(), 11);
-		parentBlock.sign();
-
-		final List<Block> blocks = NisUtils.createBlockList(parentBlock, 2);
-		final Block block = blocks.get(1);
-		block.addTransaction(context.createValidSignedTransaction());
-		block.addTransaction(context.createValidSignedTransaction(block.getSigner()));
-		block.addTransaction(context.createValidSignedTransaction());
-		block.sign();
-
-		// Act:
-		final BlockChainValidator validator = new BlockChainValidatorFactory().create(context.nisCache.copy());
-		final ValidationResult result = validator.isValid(parentBlock, blocks);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.FAILURE_SELF_SIGNED_TRANSACTION));
-	}
-
 	@Test
 	public void multisigTransferHasFeesAndAmountsDeductedFromMultisigAccount() {
 		// Arrange:
@@ -138,68 +93,6 @@ public abstract class AbstractBlockChainValidatorTransactionValidationTest exten
 		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.FAILURE_ENTITY_INVALID_VERSION));
 	}
 
-	@Test
-	public void mosaicTransfersInLaterBlocksAreAllowed() {
-		// Arrange:
-		final TestContext context = new TestContext();
-		final Block parentBlock = NisUtils.createParentBlock(
-				Utils.generateRandomAccount(),
-				BlockMarkerConstants.MOSAICS_FORK(NetworkInfos.getDefault().getVersion()));
-		parentBlock.sign();
-
-		final List<Block> blocks = NisUtils.createBlockList(parentBlock, 3);
-
-		// - add a namespace creation transaction to block index 0
-		final Account mosaicOwner = context.addAccount(Amount.fromNem(200000));
-		final MosaicDefinition mosaicDefinition = Utils.createMosaicDefinition(
-				mosaicOwner,
-				Utils.createMosaicId("alice", "tokens"),
-				Utils.createMosaicProperties(100000L, 1, null, null));
-		final ProvisionNamespaceTransaction namespaceTransaction = new ProvisionNamespaceTransaction(
-				TimeInstant.ZERO,
-				mosaicOwner,
-				MosaicConstants.NAMESPACE_OWNER_NEM,
-				Amount.fromNem(50000),
-				mosaicDefinition.getId().getNamespaceId().getLastPart(),
-				null);
-		blocks.get(0).addTransaction(fixUp(namespaceTransaction));
-
-		// - add a mosaic definition creation transaction to block index 1
-		final MosaicDefinitionCreationTransaction creationTransaction = new MosaicDefinitionCreationTransaction(
-				TimeInstant.ZERO,
-				mosaicOwner,
-				mosaicDefinition,
-				MosaicConstants.MOSAIC_CREATION_FEE_SINK,
-				Amount.fromNem(50000));
-		blocks.get(1).addTransaction(fixUp(creationTransaction));
-
-		// - add a mosaic transfer transaction to block index 2
-		final TransferTransactionAttachment attachment = new TransferTransactionAttachment();
-		attachment.addMosaic(mosaicDefinition.getId(), new Quantity(1000));
-		final Transaction transferTransaction = new TransferTransaction(
-				TimeInstant.ZERO,
-				mosaicOwner,
-				Utils.generateRandomAccount(),
-				Amount.fromNem(1),
-				attachment);
-		blocks.get(2).addTransaction(fixUp(transferTransaction));
-		NisUtils.signAllBlocks(blocks);
-
-		// Act:
-		final BlockChainValidator validator = new BlockChainValidatorFactory().create(context.nisCache.copy());
-		final ValidationResult result = validator.isValid(parentBlock, blocks);
-
-		// Assert:
-		Assert.assertThat(result, IsEqual.equalTo(ValidationResult.SUCCESS));
-	}
-
-	private static Transaction fixUp(final Transaction transaction) {
-		transaction.setDeadline(transaction.getTimeStamp().addSeconds(1));
-		transaction.setFee(Amount.fromNem(10000));
-		transaction.sign();
-		return transaction;
-	}
-
 	@Override
 	protected void assertTransactions(
 			final BlockHeight chainHeight,
@@ -240,13 +133,6 @@ public abstract class AbstractBlockChainValidatorTransactionValidationTest exten
 		return nisCache.getAccountStateCache().findStateByAddress(account.getAddress()).getAccountInfo();
 	}
 
-	private static Block createFutureBlock(final Block parentBlock) {
-		final TimeInstant currentTime = NisMain.TIME_PROVIDER.getCurrentTime();
-		final Block block = new Block(Utils.generateRandomAccount(), parentBlock, currentTime.addMinutes(2));
-		block.sign();
-		return block;
-	}
-
 	private static Block createBlockWithVersion(final Block parentBlock, final int version) {
 		final TimeInstant currentTime = NisMain.TIME_PROVIDER.getCurrentTime();
 		final Block blockTemplate = new Block(Utils.generateRandomAccount(), parentBlock, currentTime);
@@ -262,7 +148,7 @@ public abstract class AbstractBlockChainValidatorTransactionValidationTest exten
 		return block;
 	}
 
-	private static class BlockChainValidatorFactory {
+	protected static class BlockChainValidatorFactory {
 		public final BlockScorer scorer = Mockito.mock(BlockScorer.class);
 		public final int maxChainSize = 21;
 
