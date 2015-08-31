016ed2aa6a03bd73bfe0854acd9ad68d202f1f9a
nem <nem@bob.nem.ninja>
2015-08-31 12:25:14 +0200

    ...

--- nis/src/test/java/org/nem/nis/controller/AccountNamespaceInfoControllerTest.java
+++ nis/src/test/java/org/nem/nis/controller/AccountNamespaceInfoControllerTest.java
@@ -1,18 +1,14 @@
 package org.nem.nis.controller;
 
-import org.hamcrest.core.IsEqual;
-import org.junit.*;
+import org.junit.Test;
 import org.mockito.Mockito;
 import org.nem.core.model.Address;
 import org.nem.core.model.mosaic.*;
-import org.nem.core.model.namespace.NamespaceId;
-import org.nem.core.model.ncc.*;
+import org.nem.core.model.ncc.AccountId;
 import org.nem.core.model.primitive.Quantity;
 import org.nem.core.serialization.SerializableList;
-import org.nem.core.test.*;
-import org.nem.nis.cache.*;
-import org.nem.nis.state.*;
-import org.nem.nis.test.NisUtils;
+import org.nem.core.test.Utils;
+import org.nem.nis.test.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -48,7 +44,9 @@ public class AccountNamespaceInfoControllerTest {
 		// Assert:
 		Mockito.verify(context.accountStateCache, Mockito.times(1)).findStateByAddress(context.address);
 		Mockito.verify(context.accountStateCache, Mockito.times(1)).findStateByAddress(context.another);
-		context.assertMosaicDefinitionsOwned(returnedMosaicDefinitions.asCollection(), Arrays.asList(context.mosaicId1, context.mosaicId2, context.mosaicId3));
+		context.assertMosaicDefinitionsOwned(
+				returnedMosaicDefinitions.asCollection(),
+				Arrays.asList(MosaicConstants.MOSAIC_ID_XEM, context.mosaicId1, context.mosaicId2, context.mosaicId3));
 	}
 
 	@Test
@@ -79,11 +77,11 @@ public class AccountNamespaceInfoControllerTest {
 	}
 
 	private SerializableList<Mosaic> getOwnedMosaics(final TestContext context, final Address address) {
-		return context.controller.accountGetOwnedMosaics(context.getBuilder(address));
+		return context.controller.accountGetOwnedMosaics(context.getAccountIdBuilder(address));
 	}
 
 	private SerializableList<MosaicDefinition> getAccountMosaicDefinitions(final TestContext context, final Address address) {
-		return context.controller.accountGetMosaicDefinitions(context.getBuilder(address));
+		return context.controller.accountGetMosaicDefinitions(context.getAccountIdBuilder(address));
 	}
 
 	private SerializableList<MosaicDefinition> getAccountMosaicDefinitionsBatch(final TestContext context, final List<Address> addresses) {
@@ -98,84 +96,20 @@ public class AccountNamespaceInfoControllerTest {
 		private final Address another = Utils.generateRandomAddressWithPublicKey();
 
 		public ThreeMosaicsTestContext() {
+			this.mosaicDefinitions.put(MosaicConstants.MOSAIC_ID_XEM, MosaicConstants.MOSAIC_DEFINITION_XEM);
 			this.prepareMosaics(Arrays.asList(this.mosaicId1, this.mosaicId2, this.mosaicId3));
 			this.ownsMosaic(this.address, Arrays.asList(this.mosaicId1, this.mosaicId2));
 			this.ownsMosaic(this.another, Arrays.asList(this.mosaicId2, this.mosaicId3));
 		}
 	}
 
-	private static class TestContext {
-		public final Address address = Utils.generateRandomAddressWithPublicKey();
-
+	private static class TestContext extends MosaicTestContext {
 		private final AccountNamespaceInfoController controller;
-		public final ReadOnlyAccountStateCache accountStateCache = Mockito.mock(ReadOnlyAccountStateCache.class);
-		private final NamespaceCache namespaceCache = Mockito.mock(NamespaceCache.class);
-		private final HashMap<MosaicId, MosaicDefinition> mosaicDefinitions = new HashMap<>();
 
 		public TestContext() {
 			this.controller = new AccountNamespaceInfoController(
 					this.accountStateCache,
 					this.namespaceCache);
-		}
-
-		private AccountIdBuilder getBuilder(final Address address) {
-			final AccountIdBuilder builder = new AccountIdBuilder();
-			builder.setAddress(address.getEncoded());
-			return builder;
-		}
-
-		public MosaicId createMosaicId(final String namespaceName, final String mosaicName) {
-			final MosaicId mosaicId = Utils.createMosaicId(namespaceName, mosaicName);
-			final MosaicDefinition mosaicDefinition = new MosaicDefinition(
-					Utils.generateRandomAccount(),
-					mosaicId,
-					new MosaicDescriptor("descriptor"),
-					Utils.createMosaicProperties(),
-					null);
-			this.mosaicDefinitions.put(mosaicId, mosaicDefinition);
-			return mosaicId;
-		}
-
-		public void prepareMosaics(final List<MosaicId> mosaicIds) {
-			final Set<NamespaceId> uniqueNamespaces = mosaicIds.stream().map(MosaicId::getNamespaceId).collect(Collectors.toSet());
-			for (final NamespaceId namespaceId : uniqueNamespaces) {
-				final NamespaceEntry namespaceEntry = Mockito.mock(NamespaceEntry.class);
-				Mockito.when(this.namespaceCache.get(namespaceId)).thenReturn(namespaceEntry);
-
-				final Mosaics mosaics = Mockito.mock(Mosaics.class);
-				Mockito.when(namespaceEntry.getMosaics()).thenReturn(mosaics);
-			}
-
-			for (final MosaicId mosaicId : mosaicIds) {
-				final ReadOnlyMosaics mosaics = this.namespaceCache.get(mosaicId.getNamespaceId()).getMosaics();
-
-				final MosaicDefinition mosaicDefinition = this.mosaicDefinitions.get(mosaicId);
-				final MosaicEntry mosaicEntry = new MosaicEntry(mosaicDefinition);
-				Mockito.when(mosaics.get(mosaicId)).thenReturn(mosaicEntry);
-			}
-		}
-
-		public void ownsMosaic(final Address address, final List<MosaicId> mosaicIds) {
-			final AccountState accountState = new AccountState(address);
-			mosaicIds.forEach(id -> accountState.getAccountInfo().addMosaicId(id));
-			Mockito.when(this.accountStateCache.findStateByAddress(address)).thenReturn(accountState);
-		}
-
-		public void setBalance(final MosaicId mosaicId, final Address address, final Quantity balance) {
-			final MosaicEntry mosaicEntry = this.namespaceCache.get(mosaicId.getNamespaceId()).getMosaics().get(mosaicId);
-			mosaicEntry.getBalances().incrementBalance(address, balance);
-		}
-
-		public void assertMosaicDefinitionsOwned(final Collection<MosaicDefinition> returnedMosaicDefinitions, final List<MosaicId> expected) {
-			Assert.assertThat(returnedMosaicDefinitions.size(), IsEqual.equalTo(expected.size()));
-
-			final Set<MosaicDefinition> definitions = expected.stream().map(this.mosaicDefinitions::get).collect(Collectors.toSet());
-			Assert.assertThat(returnedMosaicDefinitions, IsEquivalent.equivalentTo(definitions));
-		}
-
-		public void assertMosaicsOwned(final Collection<Mosaic> returnedMosaics, final List<Mosaic> expectedMosaics) {
-			Assert.assertThat(returnedMosaics.size(), IsEqual.equalTo(expectedMosaics.size()));
-			Assert.assertThat(returnedMosaics, IsEquivalent.equivalentTo(expectedMosaics));
 		}
 	}
 }
