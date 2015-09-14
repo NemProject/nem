6c0985689f54224c009f175f44a051e8338c95e8
nem <nem@bob.nem.ninja>
2015-09-13 23:09:53 -0400

    ...

--- nis/src/main/java/org/nem/nis/state/AccountState.java
+++ nis/src/main/java/org/nem/nis/state/AccountState.java
@@ -26,7 +26,7 @@ public class AccountState implements ReadOnlyAccountState {
 				address,
 				new AccountImportance(),
 				new HistoricalImportances(),
-				new TimeBasedVestingWeightedBalances(),
+				NemStateGlobals.createWeightedBalances(),
 				new RemoteLinks(),
 				new MultisigLinks(),
 				new AccountInfo(),
