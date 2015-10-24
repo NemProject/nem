995bcdb4254da34b5341018a5a781e3b4c503e1e
Jaguar0625 <jaguar0625@127.0.0.1>
2015-10-24 12:05:08 -0400

    close NemNodeType todo; revert NodeVersion

--- nem.core/src/main/java/org/nem/core/model/NemNodeType.java
+++ nem.core/src/main/java/org/nem/core/model/NemNodeType.java
@@ -2,7 +2,6 @@ package org.nem.core.model;
 
 /**
  * Types of NEM nodes.
- * TODO 20151017 J-B: this enum doesn't seem to be used in core, can probably move to node rewards branch
  */
 public enum NemNodeType {
 
