92a4d16c8e2876d7d7fb4618e53137cffdf1c0af
BloodyRookie <nemproject@gmx.de>
2016-07-25 21:02:22 +0200

    new fee structure, no tests yet

--- nem.core/src/main/java/org/nem/core/model/TransactionFeeCalculatorAfterFork.java
+++ nem.core/src/main/java/org/nem/core/model/TransactionFeeCalculatorAfterFork.java
@@ -1,7 +1,9 @@
 package org.nem.core.model;
 
-import org.nem.core.model.mosaic.MosaicFeeInformationLookup;
+import org.nem.core.model.mosaic.*;
 import org.nem.core.model.primitive.*;
+
+import java.math.BigInteger;
 
 /**
  * Implementation for calculating and validating transaction fees since the fee fork.
@@ -56,18 +58,56 @@ public class TransactionFeeCalculatorAfterFork implements TransactionFeeCalculat
 	private Amount calculateMinimumFee(final TransferTransaction transaction) {
 		final long messageFee = null == transaction.getMessage()
 				? 0
-				: Math.max(1, transaction.getMessageLength() / 16) * FEE_UNIT_NUM_NEM;
+				: Math.max(1, transaction.getMessageLength() / 32);
 		if (transaction.getAttachment().getMosaics().isEmpty()) {
 			final long numXem = transaction.getAmount().getNumNem();
 			final long transferFee = calculateXemTransferFee(numXem);
 			return Amount.fromNem(messageFee + transferFee);
 		}
 
-		return Amount.fromNem(25);
+		final long transferFee = transaction.getAttachment().getMosaics().stream()
+				.map(m -> {
+					final MosaicFeeInformation information = this.mosaicFeeInformationLookup.findById(m.getMosaicId());
+					if (null == information) {
+						throw new IllegalArgumentException(String.format("unable to find fee information for '%s'", m.getMosaicId()));
+					}
+
+					return calculateMosaicTransferFee(transaction.getAmount(), m, information);
+				})
+				.reduce(0L, Long::sum);
+		return Amount.fromNem(messageFee + transferFee);
 	}
 
 	private static long calculateXemTransferFee(final long numXem) {
-		return Math.max(1L, numXem / 10_000L);
+		return Math.min(25, Math.max(1L, numXem / 10_000L));
+	}
+
+	private static long calculateMosaicTransferFee(
+			final Amount amount,
+			final Mosaic mosaic,
+			final MosaicFeeInformation information) {
+		if (0 == information.getDivisibility() && 10_000 >= information.getSupply().getRaw()) {
+			return 1L;
+		}
+
+		final long xemEquivalent = calculateXemEquivalent(amount, mosaic, information.getSupply(), information.getDivisibility());
+		final long xemFee = calculateXemTransferFee(xemEquivalent);
+		final long mosaicQuantity = MosaicUtils.toQuantity(information.getSupply(), information.getDivisibility()).getRaw();
+		final long supplyRelatedAdjustment = (long)(0.8 * Math.log(9_000_000_000_000_000L / mosaicQuantity));
+		return Math.min(1L, xemFee - supplyRelatedAdjustment);
+	}
+
+	private static long calculateXemEquivalent(final Amount amount, final Mosaic mosaic, final Supply supply, final int divisibility) {
+		if (Supply.ZERO.equals(supply)) {
+			return 0;
+		}
+
+		return BigInteger.valueOf(MosaicConstants.MOSAIC_DEFINITION_XEM.getProperties().getInitialSupply())
+				.multiply(BigInteger.valueOf(mosaic.getQuantity().getRaw()))
+				.multiply(BigInteger.valueOf(amount.getNumMicroNem()))
+				.divide(BigInteger.valueOf(supply.getRaw()))
+				.divide(BigInteger.TEN.pow(divisibility + 6))
+				.longValue();
 	}
 
 	private static Amount calculateMinimumFee(final MultisigAggregateModificationTransaction transaction) {
