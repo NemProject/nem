d09aa0dd831782cba489ecce6c0a69f9326e6d14
nem <nem@bob.nem.ninja>
2017-02-26 15:42:53 +0100

    ...

--- nis/src/main/java/org/nem/nis/validators/transaction/ImportanceTransferTransactionValidator.java
+++ nis/src/main/java/org/nem/nis/validators/transaction/ImportanceTransferTransactionValidator.java
@@ -6,6 +6,8 @@ import org.nem.nis.cache.ReadOnlyAccountStateCache;
 import org.nem.nis.state.*;
 import org.nem.nis.validators.ValidationContext;
 
+import static org.nem.core.model.ImportanceTransferMode.Deactivate;
+
 /**
  * A TransferTransactionValidator implementation that applies to importance transfer transactions.
  */
@@ -36,7 +38,7 @@ public class ImportanceTransferTransactionValidator implements TSingleTransactio
 	}
 
 	private static boolean isRemoteDeactivated(final ReadOnlyRemoteLinks remoteLinks) {
-		return remoteLinks.isEmpty() || ImportanceTransferMode.Deactivate == remoteLinks.getCurrent().getMode();
+		return remoteLinks.isEmpty() || Deactivate == remoteLinks.getCurrent().getMode();
 	}
 
 	private static boolean isRemoteChangeWithinLimit(final ReadOnlyRemoteLinks remoteLinks, final BlockHeight height) {
@@ -65,9 +67,28 @@ public class ImportanceTransferTransactionValidator implements TSingleTransactio
 				// second attack vector, user X announces account Y as his remote
 				// EVIL also announces Y as his remote... (handled by this.validateRemote and by BlockImportanceTransferValidator)
 				// again this cuts off X from harvesting
-				final Amount remoteBalance = this.accountStateCache.findStateByAddress(transaction.getRemote().getAddress()).getAccountInfo().getBalance();
-				if (0 != remoteBalance.compareTo(Amount.ZERO)) {
+				//
+				// We also have to check that the remote account
+				// - does not own any mosaic
+				// - does not own any namespace
+				// - is not a multisig account
+				// - is not a cosignatory of any multsig account
+				final ReadOnlyAccountState remoteAccountState = this.accountStateCache.findStateByAddress(transaction.getRemote().getAddress());
+				final ReadOnlyAccountInfo remoteAccountInfo = remoteAccountState.getAccountInfo();
+				if (0 != remoteAccountInfo.getBalance().compareTo(Amount.ZERO)) {
 					return ValidationResult.FAILURE_DESTINATION_ACCOUNT_HAS_PREEXISTING_BALANCE_TRANSFER;
+				}
+
+				if (!remoteAccountInfo.getMosaicIds().isEmpty()) {
+					return ValidationResult.FAILURE_DESTINATION_ACCOUNT_OWNS_MOSAIC;
+				}
+
+				if (!remoteAccountState.getMultisigLinks().isMultisig()) {
+					return ValidationResult.FAILURE_DESTINATION_ACCOUNT_IS_MULTISIG;
+				}
+
+				if (!remoteAccountState.getMultisigLinks().isCosignatory()) {
+					return ValidationResult.FAILURE_DESTINATION_ACCOUNT_IS_COSIGNER;
 				}
 
 				// if a remote is already activated, it needs to be deactivated first
