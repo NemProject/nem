b226149bd85ed4d75f571cb3d31a6710e6ef5a77
nem <nem@bob.nem.ninja>
2017-12-13 12:29:05 +0100

    ...

--- nis/src/main/java/org/nem/nis/cache/NamespaceCacheUtils.java
+++ nis/src/main/java/org/nem/nis/cache/NamespaceCacheUtils.java
@@ -1,7 +1,11 @@
 package org.nem.nis.cache;
 
+import org.nem.core.model.Address;
 import org.nem.core.model.mosaic.*;
+import org.nem.core.model.namespace.NamespaceId;
 import org.nem.nis.state.*;
+
+import java.util.*;
 
 /**
  * Static class containing namespace cache helper functions.
@@ -34,5 +38,42 @@ public class NamespaceCacheUtils {
 		}
 
 		return namespaceEntry.getMosaics().get(mosaicId);
+	}
+
+	/**
+	 * Get the collection of owners for a mosaic.
+	 *
+	 * @param cache The namespace cache.
+	 * @param mosaicId The mosaic id.
+	 * @return The collection of owners.
+	 */
+	public static Collection<Address> getMosaicOwners(final ReadOnlyNamespaceCache cache, final MosaicId mosaicId) {
+		final ReadOnlyNamespaceEntry namespaceEntry = cache.get(mosaicId.getNamespaceId());
+		if (null == namespaceEntry) {
+			return Collections.emptyList();
+		}
+
+		final ReadOnlyMosaicEntry mosaicEntry = namespaceEntry.getMosaics().get(mosaicId);
+		if (null == mosaicEntry) {
+			return Collections.emptyList();
+		}
+
+		return mosaicEntry.getBalances().getOwners();
+	}
+
+	/**
+	 * Gets all mosaic ids of for a given namespace id.
+	 *
+	 * @param cache The namespace cache.
+	 * @param namespaceId The namespace id.
+	 * @return The collection of mosaic ids.
+	 */
+	public static Collection<MosaicId> getMosaicIds(final ReadOnlyNamespaceCache cache, final NamespaceId namespaceId) {
+		final ReadOnlyNamespaceEntry namespaceEntry = cache.get(namespaceId);
+		if (null == namespaceEntry) {
+			return Collections.emptyList();
+		}
+
+		return namespaceEntry.getMosaics().getMosaicIds();
 	}
 }
