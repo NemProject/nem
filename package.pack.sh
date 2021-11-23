3e63aea4cc1377084f94921189eba0d1f60123b4
gimre <g.imre@protonmail.com>
2021-11-23 20:56:34 +0100

    drop zip, drop fork properties from package config file, fix dir name

--- package.pack.sh
+++ package.pack.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-packageName="nis-`ls package/nis/nem-infra* | sed 's/.*-\(.*\)-BETA.*/\1/'`"
-
-echo " [+] CREATING zip"
-zip -r -9 $packageName.zip package > /dev/null
-sha256sum $packageName.zip
+packageName="nis-$(ls package/nis/nem-infra* | sed 's/.*-\(.*\)\jar*/\1/')"
 
 echo " [+] CREATING tgz"
 tar -czpf $packageName.tgz package
