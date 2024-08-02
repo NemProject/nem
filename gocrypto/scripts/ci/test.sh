879ee8e2c888ef275466cde5dc2390f65660d2a5
wayonb <wayon@symbol.dev>
2024-08-02 13:05:51 -0400

    [gocrypto]: add code coverage

--- gocrypto/scripts/ci/test.sh
+++ gocrypto/scripts/ci/test.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
-TEST_RUNNER="go test -v ed25519.go ed25519_test.go"
+TEST_RUNNER="go test -v -skip TestVectors"
 if [ "$1" = "code-coverage" ]; then
   TEST_RUNNER+=" -race -cover -covermode=atomic -args -test.gocoverdir=.coverage"
+  export CGO_ENABLED=1
+  mkdir -p .coverage
 fi
 
 ${TEST_RUNNER}
