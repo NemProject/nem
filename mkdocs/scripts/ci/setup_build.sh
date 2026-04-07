43a57fcad89f1a190767c760aa5f57147ac36187
Xavi Artigas <xavierartigas@yahoo.es>
2026-04-07 18:50:05 +0200

    [mkdocs] fix: patch mkdoxy plugin
    Doxygen creates some methods called [static initializer],
    but the mkdocs wrapper does not expect method names
    to begin with a [.
    
    This patch includes the open bracket as a valid first letter
    for a method.

--- mkdocs/scripts/ci/setup_build.sh
+++ mkdocs/scripts/ci/setup_build.sh
@@ -4,3 +4,8 @@ set -ex
 ez_root=$(pip show mkdocs-ezglossary-plugin | sed -n 's/Location: \(.*\)/\1/p')
 ez_plugin=$ez_root/mkdocs_ezglossary_plugin/plugin.py
 patch $ez_plugin scripts/ci/ezglossary.patch --force --ignore-whitespace --fuzz 0 || true
+
+# Patch the mkdoxy plugin, ignoring errors if it was already patched
+mkdoxy_root=$(pip show mkdoxy | sed -n 's/Location: \(.*\)/\1/p')
+mkdoxy_plugin=$mkdoxy_root/mkdoxy/generatorBase.py
+patch $mkdoxy_plugin scripts/ci/mkdoxy.patch --force --ignore-whitespace --fuzz 0 || true
