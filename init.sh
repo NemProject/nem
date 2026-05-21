073c377e069f078ef3181c7cb22cd2bceef679a9
Xavi Artigas <xavi@symbolsyndicate.us>
2026-05-21 16:14:11 +0200

    [jenkins] Include sdk/ in Symbol submodule sparse checkout

--- init.sh
+++ init.sh
@@ -7,4 +7,5 @@ git -C _symbol config core.sparseCheckout true
 echo 'jenkins/*' >> .git/modules/_symbol/info/sparse-checkout
 echo 'linters/*' >> .git/modules/_symbol/info/sparse-checkout
 echo 'tests/*' >> .git/modules/_symbol/info/sparse-checkout
+echo 'sdk/*' >> .git/modules/_symbol/info/sparse-checkout
 git submodule update --force --checkout _symbol
